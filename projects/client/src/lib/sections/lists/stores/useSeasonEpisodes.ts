2341f14d66f06b4a2f57176a930e904c84a88534
Vlad Jerca <vlad.jerca@gmail.com>
2024-12-25 12:44:32 +0100

    feat(summary): add season episode list in show page

---
+++ projects/client/src/lib/sections/lists/stores/useSeasonEpisodes.ts
@@ -0,0 +1,20 @@
+import { showSeasonEpisodesQuery } from '$lib/requests/queries/shows/showSeasonEpisodesQuery.ts';
+import { time } from '$lib/utils/timing/time.ts';
+import { createQuery } from '@tanstack/svelte-query';
+import { derived } from 'svelte/store';
+
+export const useSeasonEpisodes = (slug: string, season: number) => {
+  console.log('slug:', slug, 'season:', season);
+  const query = createQuery({
+    ...showSeasonEpisodesQuery({
+      slug,
+      season,
+    }),
+    staleTime: time.hours(1),
+  });
+
+  return {
+    list: derived(query, ($query) => $query.data ?? []),
+    isLoading: derived(query, ($query) => $query.isLoading),
+  };
+};
