d392e79a6285220f6f3d45fda8a9e84e611cebc6
Vlad Jerca <vlad.jerca@gmail.com>
2025-01-23 20:43:29 +0100

    fix: disable spoilers until we figure out how to compute state

--- projects/client/src/lib/features/spoilers/useSpoiler.ts
+++ projects/client/src/lib/features/spoilers/useSpoiler.ts
@@ -1,17 +1,13 @@
 import { useAuth } from '$lib/features/auth/stores/useAuth';
-import { useUser } from '$lib/features/auth/stores/useUser';
 import { derived } from 'svelte/store';
 
 export function useSpoiler() {
   const { isAuthorized } = useAuth();
 
   return {
-    isSpoilerHidden: derived(isAuthorized, ($isAuthorized) => {
-      if (!$isAuthorized) {
-        return false;
-      }
-
-      return Boolean(useUser().current().preferences.isSpoilerHidden);
+    // FIXME: calcualte isSpoilerHidden based on user settings
+    isSpoilerHidden: derived(isAuthorized, () => {
+      return false;
     }),
   };
 }
