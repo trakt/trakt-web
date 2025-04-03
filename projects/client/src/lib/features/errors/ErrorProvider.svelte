3ff5c5958be89a281ebebcf482c043dae5569790
seferturan <seferturan85@gmail.com>
2025-04-03 19:57:29 +0200

    feat(errors): handle well known errors

---
+++ projects/client/src/lib/features/errors/ErrorProvider.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { afterNavigate } from "$app/navigation";
+  import ErrorServicePage from "$lib/pages/errors/ErrorServicePage.svelte";
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
+  import { mapToWellKnownError } from "./_internal/mapToWellKnownError";
+  import { FETCH_ERROR_EVENT } from "./constants";
+  import { WellKnownError } from "./models/WellKnownErrors";
+
+  const { children }: ChildrenProps = $props();
+  const fetchError = writable<WellKnownError | undefined>(undefined);
+
+  onMount(() => {
+    const handler = (event: Event) => {
+      const errorEvent = event as CustomEvent<number>;
+      fetchError.set(mapToWellKnownError(errorEvent.detail));
+    };
+
+    globalThis.window.addEventListener(FETCH_ERROR_EVENT, handler);
+
+    return () => {
+      globalThis.window.removeEventListener(FETCH_ERROR_EVENT, handler);
+    };
+  });
+
+  afterNavigate((_) => fetchError.set(undefined));
+</script>
+
+{#if $fetchError === WellKnownError.ServerError}
+  <ErrorServicePage />
+{/if}
+
+{#if !$fetchError}
+  {@render children()}
+{/if}
