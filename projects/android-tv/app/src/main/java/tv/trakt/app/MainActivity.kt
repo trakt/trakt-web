0e0c73b470767f7d16b695eb656726f5adda0f74
seferturan <seferturan85@gmail.com>
2025-04-04 22:16:39 +0200

    feat(android-tv): enable back navigation

--- projects/android-tv/app/src/main/java/tv/trakt/app/MainActivity.kt
+++ projects/android-tv/app/src/main/java/tv/trakt/app/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("SetJavaScriptEnabled")
@@ -25,6 +26,23 @@ class MainActivity : ComponentActivity() {
         // 10.0.2.2 is the alias to host machine's loopback interface
         webView.loadUrl("http://10.0.2.2:5173/")
 
+        enableWebViewBackNavigation(webView)
         setContentView(webView)
+    }
+
+    private fun enableWebViewBackNavigation(webView: WebView) {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                    return
+                }
+
+                remove()
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(this, callback)
     }
 }
