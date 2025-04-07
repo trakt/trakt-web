0a3e596dd8217e8e657ba0c2ef3cfad66e431726
seferturan <seferturan85@gmail.com>
2025-04-07 12:53:03 +0200

    feat(requests): add request for add item to lists

---
+++ projects/client/src/lib/requests/queries/users/addToListRequest.ts
@@ -0,0 +1,25 @@
+import { api, type ApiParams } from '$lib/requests/api.ts';
+import type { WatchlistRequest } from '@trakt/api';
+
+type AddToListParams = {
+  body: WatchlistRequest;
+  listId: string;
+  userId?: string | Nil;
+} & ApiParams;
+
+export function addToListRequest(
+  { body, fetch, listId, userId }: AddToListParams,
+): Promise<boolean> {
+  return api({ fetch })
+    .users
+    .lists
+    .list
+    .add({
+      params: {
+        id: userId ?? 'me',
+        list_id: listId,
+      },
+      body,
+    })
+    .then(({ status }) => status === 201);
+}
