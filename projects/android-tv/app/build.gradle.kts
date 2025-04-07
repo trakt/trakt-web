8cbfd492be0c55df6cc9074bd07d8da1cb9bf4f4
seferturan <seferturan85@gmail.com>
2025-04-07 15:08:31 +0200

    feat(android-tv): add support for unsigned release builds

--- projects/android-tv/app/build.gradle.kts
+++ projects/android-tv/app/build.gradle.kts
@@ -1,3 +1,8 @@
+object ManifestConstants {
+    const val DEV_LOOPBACK_URL = "http://10.0.2.2:5173/"
+    const val PROD_URL = "https://app.trakt.tv/"
+}
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -14,25 +19,35 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
     }
 
     buildTypes {
         release {
+            // FIXME: proper signing for release builds
+            signingConfig = signingConfigs.getByName("debug")
+            manifestPlaceholders["IS_CLEAR_TEXT_TRAFFIC"] = "false"
+            manifestPlaceholders["BASE_URL"] = ManifestConstants.PROD_URL
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+        debug {
+            manifestPlaceholders["IS_CLEAR_TEXT_TRAFFIC"] = "true"
+            manifestPlaceholders["BASE_URL"] = ManifestConstants.DEV_LOOPBACK_URL
+        }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
         viewBinding = true
@@ -40,7 +55,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(platform(libs.androidx.compose.bom))
