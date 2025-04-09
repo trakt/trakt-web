d949d071314ed7c51959083fb8a66fd2cdd41ee4
seferturan <seferturan85@gmail.com>
2025-04-09 15:17:41 +0200

    fix(auth): refresh on wrong token

--- projects/client/src/lib/requests/_internal/createAuthenticatedFetch.ts
+++ projects/client/src/lib/requests/_internal/createAuthenticatedFetch.ts
@@ -1,5 +1,17 @@
 import { getToken } from '$lib/features/auth/token/index.ts';
+
 import { error } from '$lib/utils/console/print.ts';
+
+const RELOAD_STATE_KEY = 'has-reloaded';
+
+function handle401(token: string | Nil) {
+  if (!token || sessionStorage.getItem(RELOAD_STATE_KEY)) {
+    return;
+  }
+
+  sessionStorage.setItem(RELOAD_STATE_KEY, 'true');
+  globalThis.window.location.reload();
+}
 
 export function createAuthenticatedFetch<
   T extends typeof fetch,
@@ -24,7 +36,16 @@ export function createAuthenticatedFetch<
           ...modifiedInit,
           headers,
         } as Parameters<T>[1],
-      );
+      ).then((response) => {
+        if (response.status === 401) {
+          handle401(token);
+          return Promise.reject(new Error('Unauthorized request'));
+        }
+
+        return response;
+      }).finally(() => {
+        sessionStorage.removeItem(RELOAD_STATE_KEY);
+      });
     } catch (e) {
       error('Fetch interceptor error:', e);
       return baseFetch(input, init);
