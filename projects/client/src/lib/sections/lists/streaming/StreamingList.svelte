62f568c396aac81ce7ab57bfcf230e47a8080866
seferturan <seferturan85@gmail.com>
2025-04-10 11:19:36 +0200

    fix(list): only show streaming list when user has favorite services

--- projects/client/src/lib/sections/lists/streaming/StreamingList.svelte
+++ projects/client/src/lib/sections/lists/streaming/StreamingList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { useUser } from "$lib/features/auth/stores/useUser";
   import type { MediaType } from "$lib/requests/models/MediaType";
   import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
   import DrillableMediaList from "../drilldown/DrillableMediaList.svelte";
@@ -12,17 +13,24 @@
   };
 
   const { title, drilldownLabel, type }: TrendingListProps = $props();
+  const { current } = useUser();
+
+  const hasFavorites = $derived(
+    (current().services.favorites ?? []).length > 0,
+  );
 </script>
 
-<DrillableMediaList
-  id="streaming-list-{type}"
-  {title}
-  {drilldownLabel}
-  {type}
-  useList={useStreamingList}
-  urlBuilder={UrlBuilder.streaming}
->
-  {#snippet item(media)}
-    <StreamingListItem {type} {media} />
-  {/snippet}
-</DrillableMediaList>
+{#if hasFavorites}
+  <DrillableMediaList
+    id="streaming-list-{type}"
+    {title}
+    {drilldownLabel}
+    {type}
+    useList={useStreamingList}
+    urlBuilder={UrlBuilder.streaming}
+  >
+    {#snippet item(media)}
+      <StreamingListItem {type} {media} />
+    {/snippet}
+  </DrillableMediaList>
+{/if}
