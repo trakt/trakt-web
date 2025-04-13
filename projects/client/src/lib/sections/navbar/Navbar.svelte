52660e26318932bc26fd3a1047216e7de38f4bdb
seferturan <seferturan85@gmail.com>
2025-04-13 16:49:55 +0200

    feat(filter): add filter button to navbar

--- projects/client/src/lib/sections/navbar/Navbar.svelte
+++ projects/client/src/lib/sections/navbar/Navbar.svelte
@@ -14,6 +14,7 @@
   import { navigateToTraktOg } from "$lib/utils/url/navigateToTraktOg";
   import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
   import { onMount } from "svelte";
+  import FilterButton from "./components/filter/FilterButton.svelte";
   import JoinTraktButton from "./components/JoinTraktButton.svelte";
   import SearchInput from "./components/search/SearchInput.svelte";
   import ProfileButton from "./ProfileButton.svelte";
@@ -134,6 +135,7 @@
         <JoinTraktButton />
       </RenderFor>
       <RenderFor audience="authenticated">
+        <FilterButton />
         <ProfileButton />
       </RenderFor>
     </div>
---
+++ projects/client/src/lib/sections/navbar/components/filter/FilterButton.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import DropdownItem from "$lib/components/dropdown/DropdownItem.svelte";
+  import DropdownList from "$lib/components/dropdown/DropdownList.svelte";
+  import FilterIcon from "$lib/components/icons/FilterIcon.svelte";
+  import { useFilter } from "$lib/features/filters/useFilter";
+  import * as m from "$lib/features/i18n/messages.ts";
+  import RenderFor from "$lib/guards/RenderFor.svelte";
+
+  const { filter, setFilter } = useFilter();
+
+  const currentOption = $derived(
+    $filter.options.find((option) => option.value === $filter.value),
+  );
+  const isFiltering = $derived(currentOption !== undefined);
+
+  const color = $derived(isFiltering ? "blue" : "default");
+  const state = $derived(isFiltering ? "filtered" : "unfiltered");
+  const currentLabel = $derived(currentOption?.label ?? m.filter_label());
+</script>
+
+<trakt-filter>
+  <DropdownList
+    label={m.filter_label()}
+    variant="secondary"
+    text="capitalize"
+    size="small"
+    style="flat"
+    {color}
+  >
+    <RenderFor audience="authenticated" device={["desktop"]}>
+      {currentLabel}
+    </RenderFor>
+    {#snippet icon()}
+      <FilterIcon {state} />
+    {/snippet}
+    {#snippet items()}
+      <DropdownItem color="red" onclick={() => setFilter(null)}>
+        {m.filter_reset()}
+      </DropdownItem>
+      {#each $filter.options as option}
+        <DropdownItem
+          color="blue"
+          disabled={option.value === currentOption?.value}
+          onclick={() => setFilter(option.value)}
+        >
+          {option.label}
+        </DropdownItem>
+      {/each}
+    {/snippet}
+  </DropdownList>
+</trakt-filter>
+
+<style>
+  trakt-filter {
+    :global(.trakt-list) {
+      min-width: var(--ni-176);
+    }
+  }
+</style>
