89a70e44d2c938757401151a2c577bf5155da423
seferturan <seferturan85@gmail.com>
2025-04-14 08:59:03 +0200

    chore(rating): tweaked ratings mapping

--- projects/client/src/lib/sections/summary/components/rating/mapRatingToSimpleRating.spec.ts
+++ projects/client/src/lib/sections/summary/components/rating/mapRatingToSimpleRating.spec.ts
@@ -8,16 +8,16 @@ describe('mapRatingToSimpleRating', () => {
     expect(mapRatingToSimpleRating(3)).toBe('bad');
     expect(mapRatingToSimpleRating(4)).toBe('bad');
     expect(mapRatingToSimpleRating(5)).toBe('bad');
-    expect(mapRatingToSimpleRating(6)).toBe('bad');
   });
 
   it('should return good', () => {
+    expect(mapRatingToSimpleRating(6)).toBe('good');
     expect(mapRatingToSimpleRating(7)).toBe('good');
     expect(mapRatingToSimpleRating(8)).toBe('good');
-    expect(mapRatingToSimpleRating(9)).toBe('good');
   });
 
   it('should return great', () => {
+    expect(mapRatingToSimpleRating(9)).toBe('great');
     expect(mapRatingToSimpleRating(10)).toBe('great');
   });
 });
--- projects/client/src/lib/sections/summary/components/rating/mapRatingToSimpleRating.ts
+++ projects/client/src/lib/sections/summary/components/rating/mapRatingToSimpleRating.ts
@@ -2,7 +2,7 @@ import { SimpleRating } from '$lib/models/SimpleRating.ts';
 import { SIMPLE_RATINGS } from './constants.ts';
 
 export function mapRatingToSimpleRating(value: number): SimpleRating {
-  if (value === SIMPLE_RATINGS[SimpleRating.Great]) {
+  if (value >= SIMPLE_RATINGS[SimpleRating.Great]) {
     return SimpleRating.Great;
   }
 
