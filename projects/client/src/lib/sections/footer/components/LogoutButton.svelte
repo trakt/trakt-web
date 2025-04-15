9db737bfc8668027c917086fc956d44c6e203f98
seferturan <seferturan85@gmail.com>
2025-04-15 11:00:42 +0200

    feat(footer): add logout to footer on profile page

---
+++ projects/client/src/lib/sections/footer/components/LogoutButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Button from "$lib/components/buttons/Button.svelte";
+  import { useAuth } from "$lib/features/auth/stores/useAuth";
+  import * as m from "$lib/features/i18n/messages.ts";
+
+  const { size }: { size: "small" | "tag" } = $props();
+
+  const { logout } = useAuth();
+</script>
+
+<Button
+  {size}
+  onclick={logout}
+  label={m.logout()}
+  color="red"
+  style="flat"
+  variant="secondary"
+>
+  {m.logout()}
+</Button>
