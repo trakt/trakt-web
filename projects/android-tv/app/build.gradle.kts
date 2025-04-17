69b07e1f18c676cd59213213bddeb43bf130e1ff
seferturan <seferturan85@gmail.com>
2025-04-17 17:43:10 +0200

    chore(android-tv): set version

--- projects/android-tv/app/build.gradle.kts
+++ projects/android-tv/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "tv.trakt.app"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1"
     }
 
     buildTypes {
