d398e35c9fec260c9c9957fea9f98671f1d0835f
seferturan <seferturan85@gmail.com>
2025-04-22 11:24:41 +0200

    chore: add navigation provider to testprovider

--- projects/client/test/beds/_internal/TestProvider.svelte
+++ projects/client/test/beds/_internal/TestProvider.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import AnalyticsProvider from "$lib/features/analytics/AnalyticsProvider.svelte";
   import AuthProvider from "$lib/features/auth/components/AuthProvider.svelte";
+  import NavigationProvider from "$lib/features/navigation/NavigationProvider.svelte";
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import type { Snippet } from "svelte";
   import { isAuthorized } from "./isAuthorized.ts";
@@ -11,8 +12,10 @@
 <!-- TODO: add more providers here as we expand test suite -->
 <AuthProvider isAuthorized={$isAuthorized} url="http://localhost:3000">
   <QueryClientProvider client={new QueryClient()}>
-    <AnalyticsProvider>
-      {@render children()}
-    </AnalyticsProvider>
+    <NavigationProvider device="unknown">
+      <AnalyticsProvider>
+        {@render children()}
+      </AnalyticsProvider>
+    </NavigationProvider>
   </QueryClientProvider>
 </AuthProvider>
