04e2ba51de9636da1512b5b700cfd55f62306b9e
seferturan <seferturan85@gmail.com>
2025-04-22 11:24:51 +0200

    chore: add component test bed

---
+++ projects/client/test/beds/component/ComponentTestBed.svelte
@@ -0,0 +1,9 @@
+<script>
+  import TestProvider from "../_internal/TestProvider.svelte";
+
+  const input = $props();
+</script>
+
+<TestProvider>
+  <input.component {...input.props} />
+</TestProvider>
