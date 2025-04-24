427229be92810c8051664ef67b69ab38ffd08a9c
seferturan <seferturan85@gmail.com>
2025-04-24 10:03:31 +0200

    feat(auth): use activation link login when d-pad navigation is enabled

--- projects/client/src/lib/sections/landing/components/JoinForFreeButton.svelte
+++ projects/client/src/lib/sections/landing/components/JoinForFreeButton.svelte
@@ -4,19 +4,31 @@
   import { useAuth } from "$lib/features/auth/stores/useAuth";
   import * as m from "$lib/features/i18n/messages";
   import { DpadNavigationType } from "$lib/features/navigation/models/DpadNavigationType";
+  import RenderFor from "$lib/guards/RenderFor.svelte";
+  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
 
   const { url } = useAuth();
 </script>
 
-<Button
-  color="purple"
-  href={$url}
-  label={m.join_trakt_button_label()}
-  style="flat"
-  navigationType={DpadNavigationType.Item}
->
-  {m.join_trakt_for_free_button()}
-  {#snippet icon()}
-    <CaretRightIcon />
-  {/snippet}
-</Button>
+{#snippet joinButton(url: string)}
+  <Button
+    color="purple"
+    href={url}
+    label={m.join_trakt_button_label()}
+    style="flat"
+    navigationType={DpadNavigationType.Item}
+  >
+    {m.join_trakt_for_free_button()}
+    {#snippet icon()}
+      <CaretRightIcon />
+    {/snippet}
+  </Button>
+{/snippet}
+
+<RenderFor audience="public" navigation="default">
+  {@render joinButton($url)}
+</RenderFor>
+
+<RenderFor audience="public" navigation="dpad">
+  {@render joinButton(UrlBuilder.login.activate())}
+</RenderFor>
--- projects/client/src/lib/sections/navbar/components/JoinTraktButton.svelte
+++ projects/client/src/lib/sections/navbar/components/JoinTraktButton.svelte
@@ -3,18 +3,30 @@
   import { useAuth } from "$lib/features/auth/stores/useAuth";
   import * as m from "$lib/features/i18n/messages";
   import { DpadNavigationType } from "$lib/features/navigation/models/DpadNavigationType";
+  import RenderFor from "$lib/guards/RenderFor.svelte";
+  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
 
   const { url } = useAuth();
 </script>
 
-<Button
-  href={$url}
-  label={m.join_trakt_button_label()}
-  style="flat"
-  size="small"
-  color="purple"
-  variant="primary"
-  navigationType={DpadNavigationType.Item}
->
-  {m.join_trakt_button()}
-</Button>
+{#snippet joinButton(url: string)}
+  <Button
+    href={url}
+    label={m.join_trakt_button_label()}
+    style="flat"
+    size="small"
+    color="purple"
+    variant="primary"
+    navigationType={DpadNavigationType.Item}
+  >
+    {m.join_trakt_button()}
+  </Button>
+{/snippet}
+
+<RenderFor audience="public" navigation="default">
+  {@render joinButton($url)}
+</RenderFor>
+
+<RenderFor audience="public" navigation="dpad">
+  {@render joinButton(UrlBuilder.login.activate())}
+</RenderFor>
