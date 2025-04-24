935559c002e9fbad59d97ffd891dd308b0505a06
seferturan <seferturan85@gmail.com>
2025-04-24 18:13:09 +0200

    refactor(navbar): extract logo

---
+++ projects/client/src/lib/sections/navbar/components/TraktLogo.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Link from "$lib/components/link/Link.svelte";
+  import Logo from "$lib/components/logo/Logo.svelte";
+  import LogoMark from "$lib/components/logo/LogoMark.svelte";
+  import RenderFor from "$lib/guards/RenderFor.svelte";
+  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+</script>
+
+<RenderFor
+  audience="authenticated"
+  device={["tablet-sm", "tablet-lg", "desktop"]}
+>
+  <div class="trakt-logo">
+    <Link href={UrlBuilder.home()}>
+      <LogoMark />
+    </Link>
+  </div>
+</RenderFor>
+<RenderFor audience="public">
+  <div class="trakt-logo">
+    <Logo />
+  </div>
+</RenderFor>
+
+<style>
+  .trakt-logo {
+    height: var(--ni-32);
+    display: flex;
+    justify-content: center;
+
+    :global(svg) {
+      /* Safari 🥲 */
+      height: var(--ni-32);
+    }
+  }
+</style>
