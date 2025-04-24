2022126d3f1e0b9038366b2301a3ed386630b998
seferturan <seferturan85@gmail.com>
2025-04-24 09:43:19 +0200

    feat(auth): add support for device token polling

--- projects/client/src/lib/features/auth/handle.ts
+++ projects/client/src/lib/features/auth/handle.ts
@@ -1,12 +1,15 @@
+import { requestDeviceCode } from '$lib/features/auth/requests/requestDeviceCode.ts';
 import { time } from '$lib/utils/timing/time.ts';
 import { prependHttpOrHttps } from '$lib/utils/url/prependHttpOrHttps.ts';
 import { setCacheBuster } from '$lib/utils/url/setCacheBuster.ts';
 import { type Handle, type RequestEvent } from '@sveltejs/kit';
+import { AuthDeviceEndpoint } from './AuthDeviceEndpoint.ts';
 import { AuthEndpoint } from './AuthEndpoint.ts';
 import type {
   SerializedAuthResponse,
 } from './models/SerializedAuthResponse.ts';
 import { authorize } from './requests/authorize.ts';
+import { authorizeDeviceCode } from './requests/authorizeDeviceCode.ts';
 
 export const AUTH_COOKIE_NAME = 'trakt-auth';
 const REFRESH_THRESHOLD_MINUTES = 15;
@@ -20,6 +23,7 @@ function getAuth(event: RequestEvent): SerializedAuthResponse | null {
   }
 }
 
+// FIXME: split up this file
 export const handle: Handle = async ({ event, resolve }) => {
   const setAuth = (auth: SerializedAuthResponse | Nil) => {
     event.locals.auth = auth;
@@ -28,45 +32,16 @@ export const handle: Handle = async ({ event, resolve }) => {
   const getReferrer = () =>
     prependHttpOrHttps(event.request.headers.get('host')) ?? '';
 
-  const isLogout = event.url.pathname.startsWith(AuthEndpoint.Logout);
-
-  if (isLogout) {
-    setAuth(null);
-    return new Response(null, {
-      headers: {
-        'Set-Cookie': event.cookies.serialize(AUTH_COOKIE_NAME, '', {
-          httpOnly: true,
-          secure: true,
-          maxAge: 0,
-          path: '/',
-        }),
-      },
-    });
-  }
-
-  const code = event.url.searchParams.get('code');
-  const isExchange = code != null;
-
-  if (isExchange) {
-    const result = await authorize({
-      token: {
-        type: 'exchange',
-        code,
-      },
-      referrer: getReferrer(),
-    });
-    const { isAuthorized } = result;
-    setAuth(result);
-
-    const url = new URL(event.url);
-    url.searchParams.delete('code');
+  const authorizedResponse = (response: SerializedAuthResponse, url: URL) => {
+    const { isAuthorized } = response;
+    setAuth(response);
 
     const headers = new Headers();
 
     if (isAuthorized) {
       const cookie = event.cookies.serialize(
         AUTH_COOKIE_NAME,
-        JSON.stringify(result),
+        JSON.stringify(response),
         {
           httpOnly: true,
           secure: true,
@@ -85,6 +60,72 @@ export const handle: Handle = async ({ event, resolve }) => {
         Location: setCacheBuster(url).toString(),
       },
     });
+  };
+
+  const isLogout = event.url.pathname.startsWith(AuthEndpoint.Logout);
+
+  if (isLogout) {
+    setAuth(null);
+    return new Response(null, {
+      headers: {
+        'Set-Cookie': event.cookies.serialize(AUTH_COOKIE_NAME, '', {
+          httpOnly: true,
+          secure: true,
+          maxAge: 0,
+          path: '/',
+        }),
+      },
+    });
+  }
+
+  const isDeviceAuth = event.url.pathname.startsWith(AuthDeviceEndpoint.Get);
+
+  if (isDeviceAuth) {
+    const response = await requestDeviceCode();
+
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const isDeviceAuthPoll = event.url.pathname.startsWith(
+    AuthDeviceEndpoint.Poll,
+  );
+
+  if (isDeviceAuthPoll) {
+    const { code } = await event.request.json() as { code: string };
+    const response = await authorizeDeviceCode(code);
+
+    if ('state' in response) {
+      return new Response(JSON.stringify(response), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    return authorizedResponse(response, new URL(event.url.origin));
+  }
+
+  const code = event.url.searchParams.get('code');
+  const isExchange = code != null;
+
+  if (isExchange) {
+    const result = await authorize({
+      token: {
+        type: 'exchange',
+        code,
+      },
+      referrer: getReferrer(),
+    });
+
+    const url = new URL(event.url);
+    url.searchParams.delete('code');
+    return authorizedResponse(result, url);
   }
 
   const authResponse = getAuth(event);
