adf891315b8369dfb1d213ed69f6754936d9d47f
Vlad Jerca <vlad.jerca@gmail.com>
2025-04-25 16:42:54 +0200

    feat(up-next): define mark as watch swipe left gesture

--- projects/client/src/lib/sections/lists/progress/EpisodeProgressItem.svelte
+++ projects/client/src/lib/sections/lists/progress/EpisodeProgressItem.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import SwipeX from "$lib/components/gestures/SwipeX.svelte";
   import RenderFor from "$lib/guards/RenderFor.svelte";
   import type { EpisodeProgressEntry } from "$lib/requests/models/EpisodeProgressEntry";
   import type { ShowEntry } from "$lib/requests/models/ShowEntry";
+  import MarkAsWatchedSwipeIndicator from "$lib/sections/media-actions/mark-as-watched/MarkAsWatchedSwipeIndicator.svelte";
+  import { useMarkAsWatched } from "$lib/sections/media-actions/mark-as-watched/useMarkAsWatched";
   import RestoreAction from "$lib/sections/media-actions/restore/RestoreAction.svelte";
   import DropAction from "../../media-actions/drop/DropAction.svelte";
   import MarkAsWatchedAction from "../../media-actions/mark-as-watched/MarkAsWatchedAction.svelte";
@@ -20,27 +23,58 @@
   const { episode, show, status, style }: UpNextEpisodeProps = $props();
 
   const isHidden = $derived(status === "hidden");
+
+  const { markAsWatched } = $derived(
+    useMarkAsWatched({
+      type: "episode",
+      media: [episode],
+      show: show,
+      episode: episode,
+    }),
+  );
 </script>
 
-<EpisodeCard {episode} {show} {status} {style} variant="next">
-  {#snippet popupActions()}
-    <RenderFor audience="authenticated">
-      <MarkAsWatchedAction
-        style="dropdown-item"
-        type="episode"
-        allowRewatch
-        title={episode.title}
-        {show}
-        {episode}
-        media={episode}
-      />
-      {#if $isNitroEnabled}
-        <DropAction style="dropdown-item" title={show.title} id={show.id} />
-      {/if}
+{#snippet card()}
+  <EpisodeCard {episode} {show} {status} {style} variant="next">
+    {#snippet popupActions()}
+      <RenderFor audience="authenticated">
+        <MarkAsWatchedAction
+          style="dropdown-item"
+          type="episode"
+          allowRewatch
+          title={episode.title}
+          {show}
+          {episode}
+          media={episode}
+        />
+        {#if $isNitroEnabled}
+          <DropAction style="dropdown-item" title={show.title} id={show.id} />
+        {/if}
 
-      {#if $isNitroEnabled && isHidden}
-        <RestoreAction style="dropdown-item" title={show.title} id={show.id} />
-      {/if}
-    </RenderFor>
-  {/snippet}
-</EpisodeCard>
+        {#if $isNitroEnabled && isHidden}
+          <RestoreAction
+            style="dropdown-item"
+            title={show.title}
+            id={show.id}
+          />
+        {/if}
+      </RenderFor>
+    {/snippet}
+  </EpisodeCard>
+{/snippet}
+
+{#if style === "summary"}
+  <SwipeX
+    children={card}
+    directions={["left"]}
+    classList="trakt-up-next-episode"
+    onSwipe={markAsWatched}
+    --indicator-height="var(--height-summary-card-cover)"
+  >
+    {#snippet indicator({ isActive })}
+      <MarkAsWatchedSwipeIndicator {isActive} />
+    {/snippet}
+  </SwipeX>
+{:else}
+  {@render card()}
+{/if}
