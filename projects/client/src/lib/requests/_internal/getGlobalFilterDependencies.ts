77ea96891f103e7742b760e8d14e491c4d5d172e
seferturan <seferturan85@gmail.com>
2025-04-29 14:48:23 +0200

    refactor(requests): automatically determine filter dependencies

--- projects/client/src/lib/requests/_internal/getGlobalFilterDependencies.ts
+++ projects/client/src/lib/requests/_internal/getGlobalFilterDependencies.ts
@@ -1,9 +1,10 @@
+import { FilterKey } from '$lib/features/filters/models/Filter.ts';
 import type { FilterParams } from '$lib/requests/models/FilterParams.ts';
 
 export function getGlobalFilterDependencies(params: FilterParams) {
-  return [
-    params.filter?.genres,
-    params.filter?.ignore_watched,
-    params.filter?.ignore_watchlisted,
-  ];
+  const filterKeys = Object.values(FilterKey);
+
+  return filterKeys
+    .filter((key) => key in (params.filter || {}))
+    .map((key) => params.filter?.[key as keyof typeof params.filter]);
 }
