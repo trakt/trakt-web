3ae66aaf96fb7e7cac831a3fca76e80ad28b9c62
seferturan <seferturan85@gmail.com>
2025-05-02 14:53:22 +0200

    fix(navigation): omit disabled elements

--- projects/client/src/lib/features/navigation/_internal/getSelectableItems.ts
+++ projects/client/src/lib/features/navigation/_internal/getSelectableItems.ts
@@ -3,7 +3,7 @@ import { DpadNavigationType } from '../models/DpadNavigationType.ts';
 export const getSelectableItems = (list: Element) => {
   return Array.from(
     list.querySelectorAll(
-      `[data-dpad-navigation="${DpadNavigationType.Item}"]`,
+      `[data-dpad-navigation="${DpadNavigationType.Item}"]:not([disabled]):not([disabled="true"])`,
     ),
   );
 };
