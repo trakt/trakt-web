3074c3211f0ddaa444abc37a59d4508ecd2aa73f
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-02 14:14:40 +0200

    feat(show): add extra video list

---
+++ projects/client/src/lib/requests/_internal/mapToMediaVideo.ts
@@ -0,0 +1,21 @@
+import { type MediaVideo } from '$lib/requests/models/MediaVideo.ts';
+import { checksum } from '$lib/utils/string/checksum.ts';
+import type { VideoResponse } from '@trakt/api';
+export function mapToMediaVideo(
+  video: VideoResponse,
+): MediaVideo {
+  // Extract YouTube video ID from the URL if it's a YouTube video
+  const [_, youtubeId] = video.url.match(
+    /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/?|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/,
+  ) ?? [];
+
+  const thumbnail = `https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`;
+
+  return {
+    id: checksum(video.url),
+    title: video.title,
+    type: video.type,
+    url: video.url,
+    thumbnail,
+  };
+}
