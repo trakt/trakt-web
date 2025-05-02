40f5edb0674a6ac5e1eaff79361da21ad872c589
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-02 13:53:58 +0200

    test(movie): verify video query

---
+++ projects/client/src/lib/requests/queries/movies/movieVideosQuery.spec.ts
@@ -0,0 +1,20 @@
+import { MovieHereticVideoMappedMock } from '$mocks/data/summary/movies/heretic/mapped/MovieHereticVideoMappedMock.ts';
+import { MovieHereticResponseMock } from '$mocks/data/summary/movies/heretic/response/MovieHereticResponseMock.ts';
+import { runQuery } from '$test/beds/query/runQuery.ts';
+import { createQuery } from '@tanstack/svelte-query';
+import { describe, expect, it } from 'vitest';
+import { movieVideosQuery } from './movieVideosQuery.ts';
+
+describe('movieVideosQuery', () => {
+  it('should query videos for Heretic (2024)', async () => {
+    const result = await runQuery({
+      factory: () =>
+        createQuery(
+          movieVideosQuery({ slug: MovieHereticResponseMock.ids.slug }),
+        ),
+      mapper: (response) => response?.data,
+    });
+
+    expect(result).to.deep.equal(MovieHereticVideoMappedMock);
+  });
+});
