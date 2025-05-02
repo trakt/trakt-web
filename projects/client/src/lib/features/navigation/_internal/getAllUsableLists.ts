76a6d64e9db039030d80c728e51b69dceb4db432
seferturan <seferturan85@gmail.com>
2025-05-02 14:53:01 +0200

    feat(navigation): add support for navigation traps

--- projects/client/src/lib/features/navigation/_internal/getAllUsableLists.ts
+++ projects/client/src/lib/features/navigation/_internal/getAllUsableLists.ts
@@ -1,13 +1,18 @@
 import { DpadNavigationType } from '$lib/features/navigation/models/DpadNavigationType.ts';
+import { getNavigationScope } from './getNavigationScope.ts';
+import { getSelectableItems } from './getSelectableItems.ts';
 
 export function getAllUsableLists() {
+  const scope = getNavigationScope();
+
   const lists = Array.from(
-    document.querySelectorAll(
+    scope.querySelectorAll(
       `[data-dpad-navigation="${DpadNavigationType.List}"]`,
     ),
   );
 
-  return lists.filter((list) =>
-    list.querySelector(`[data-dpad-navigation="${DpadNavigationType.Item}"]`)
-  );
+  return lists.filter((list) => {
+    const selectableItems = getSelectableItems(list);
+    return selectableItems.length > 0;
+  });
 }
---
+++ projects/client/src/lib/features/navigation/useNavigationTrap.ts
@@ -0,0 +1,30 @@
+import { onMount } from 'svelte';
+import { get, writable } from 'svelte/store';
+import { focusAndScrollIntoView } from './_internal/focusAndScrollIntoView.ts';
+import { DpadNavigationType } from './models/DpadNavigationType.ts';
+
+export function useNavigationTrap(
+  element: HTMLElement,
+  itemParentSelector: string,
+) {
+  const currentElement = writable<Element | null>(null);
+
+  onMount(() => {
+    currentElement.set(document.activeElement);
+
+    element.setAttribute(
+      'data-dpad-navigation',
+      DpadNavigationType.Trap,
+    );
+
+    const selectableElement = element.querySelector(
+      `${itemParentSelector} [data-dpad-navigation='${DpadNavigationType.Item}']`,
+    );
+    focusAndScrollIntoView(selectableElement);
+
+    return () => {
+      element.removeAttribute('data-dpad-navigation');
+      focusAndScrollIntoView(get(currentElement));
+    };
+  });
+}
