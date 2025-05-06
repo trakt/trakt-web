1a4e58461a8db99d916ef4f00e7e69e8d9c52c21
seferturan <seferturan85@gmail.com>
2025-05-06 15:29:27 +0200

    test(i18n): setting default locale

--- projects/client/src/lib/features/i18n/handle.spec.ts
+++ projects/client/src/lib/features/i18n/handle.spec.ts
@@ -1,6 +1,7 @@
 import { mockRequestEvent } from '$test/request/mockRequestEvent.ts';
 import { interceptHandleResolveOptions } from '$test/resolve/interceptHandleResolveOptions.ts';
 import { describe, expect, it, vi } from 'vitest';
+import { LOCALE_COOKIE_NAME } from './constants.ts';
 import { DIR_PLACEHOLDER, handle, LANG_PLACEHOLDER } from './handle.ts';
 
 describe('handle: i18n', () => {
@@ -40,5 +41,53 @@ describe('handle: i18n', () => {
     expect(response.headers.get('Set-Cookie')).toEqual(
       'trakt-locale=en;httpOnly=true;secure=true;maxAge=157680000;path=/',
     );
+  });
+
+  it('should use the accept-language by default', async () => {
+    const html = `<html
+                  lang="${LANG_PLACEHOLDER}"
+                  dir="${DIR_PLACEHOLDER}">
+                ></html>`;
+
+    const { transformPageChunk } = await interceptHandleResolveOptions(
+      handle,
+      new Request('http://localhost', {
+        headers: new Headers({
+          'Accept-Language': 'nl-nl',
+        }),
+      }),
+    );
+
+    const transformed = transformPageChunk?.({ html, done: true });
+
+    expect(transformed).toContain('lang="nl-nl"');
+  });
+
+  it('should prefer the cookie over the accept-language header', async () => {
+    const cookieLocale = 'en';
+
+    const html = `<html
+                  lang="${LANG_PLACEHOLDER}"
+                  dir="${DIR_PLACEHOLDER}">
+                ></html>`;
+
+    const { transformPageChunk } = await interceptHandleResolveOptions(
+      handle,
+      new Request('http://localhost', {
+        headers: new Headers({
+          'Accept-Language': 'nl-nl',
+        }),
+      }),
+      (key) => {
+        if (key === LOCALE_COOKIE_NAME) {
+          return cookieLocale;
+        }
+        return null;
+      },
+    );
+
+    const transformed = transformPageChunk?.({ html, done: true });
+
+    expect(transformed).toContain(`lang="${cookieLocale}"`);
   });
 });
