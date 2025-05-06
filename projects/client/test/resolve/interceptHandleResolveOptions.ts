62281f8acb521d6eb47652ad0e3d3bacdf53a66b
seferturan <seferturan85@gmail.com>
2025-05-06 15:29:27 +0200

    test(i18n): setting default locale

--- projects/client/test/resolve/interceptHandleResolveOptions.ts
+++ projects/client/test/resolve/interceptHandleResolveOptions.ts
@@ -4,12 +4,14 @@ import type { Handle, ResolveOptions } from '@sveltejs/kit';
 export function interceptHandleResolveOptions(
   handle: Handle,
   request?: Request,
+  cookieHandler?: (key: string) => string | Nil,
 ) {
   return new Promise<ResolveOptions>((resolve) => {
     handle({
       event: mockRequestEvent({
         url: 'http://localhost',
         request,
+        cookieHandler,
       }),
       resolve: (_, opts?: ResolveOptions) => {
         resolve(opts ?? {});
