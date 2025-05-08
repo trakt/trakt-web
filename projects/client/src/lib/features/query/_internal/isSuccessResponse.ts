9fad8901bb92f74c687aecda6dc736d72b2b25b9
seferturan <seferturan85@gmail.com>
2025-05-08 16:00:18 +0200

    revert: "fix(query): accept 204 also as a valid response status"
    This reverts commit 166374ce95c5e07f672531de9a73905563f9e654.

--- projects/client/src/lib/features/query/_internal/isSuccessResponse.ts
+++ projects/client/src/lib/features/query/_internal/isSuccessResponse.ts
@@ -3,12 +3,10 @@ import type {
   SuccessResponse,
 } from '../models/ResponseDefinitions.ts';
 
-const SUCCESSFUL_STATUSES: number[] = [200, 204] as const;
-
 export function isSuccessResponse<TInput>(
   response: RequestResponse<TInput>,
 ): response is SuccessResponse<TInput> {
   return Array.isArray(response)
-    ? response.every((item) => SUCCESSFUL_STATUSES.includes(item?.status))
-    : SUCCESSFUL_STATUSES.includes(response.status);
+    ? response.every((item) => item?.status === 200)
+    : response.status === 200;
 }
