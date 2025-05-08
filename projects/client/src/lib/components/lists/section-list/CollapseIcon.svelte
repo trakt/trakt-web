13d9820e054d11f6bf0d0413a9ca6d614db3336c
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-08 09:00:21 +0200

    feat(list): update collapse / expand icons

--- projects/client/src/lib/components/lists/section-list/CollapseIcon.svelte
+++ projects/client/src/lib/components/lists/section-list/CollapseIcon.svelte
@@ -1,9 +1,13 @@
 <svg
+  width="17"
+  height="16"
+  viewBox="0 0 17 16"
+  fill="none"
   xmlns="http://www.w3.org/2000/svg"
-  height="24px"
-  viewBox="0 -960 960 960"
-  width="24px"
-  fill="currentColor"
 >
-  <path d="m280-400 200-200 200 200H280Z" />
+  <path
+    d="M15.5 11.5L8.5 4.5L1.5 11.5"
+    stroke="currentColor"
+    stroke-width="2"
+  />
 </svg>
