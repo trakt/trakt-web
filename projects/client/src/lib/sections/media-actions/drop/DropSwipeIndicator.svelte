349c0586ca5542f7f33d9b66a8fe5899b10e5867
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-12 13:12:08 +0200

    refactor: extram stem swipe indicator

--- projects/client/src/lib/sections/media-actions/drop/DropSwipeIndicator.svelte
+++ projects/client/src/lib/sections/media-actions/drop/DropSwipeIndicator.svelte
@@ -1,42 +1,10 @@
 <script lang="ts">
   import DropIcon from "$lib/components/icons/DropIcon.svelte";
+  import StemSwipeIndicator from "../_internal/StemSwipeIndicator.svelte";
 
   const { isActive }: { isActive: boolean } = $props();
 </script>
 
-<div class="trakt-gesture-indicator" class:trakt-gesture-active={isActive}>
+<StemSwipeIndicator {isActive} --color-gesture-active="var(--color-foreground)">
   <DropIcon />
-</div>
-
-<style>
-  .trakt-gesture-indicator {
-    --color-inactive-gesture: color-mix(
-      in srgb,
-      var(--color-foreground) 35%,
-      var(--color-background)
-    );
-    border-radius: var(--border-radius-m);
-
-    width: calc(var(--indicator-width, 0) + calc(var(--border-radius-m) * 2));
-
-    backdrop-filter: blur(var(--ni-8)) brightness(0.85);
-    color: var(--color-inactive-gesture);
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    outline: var(--color-inactive-gesture) var(--ni-1) solid;
-    outline-offset: var(--ni-neg-2);
-
-    &.trakt-gesture-active {
-      outline-color: var(--color-foreground);
-      color: var(--color-foreground);
-    }
-
-    :global(svg) {
-      opacity: var(--swipe-progress, 0);
-      transition: color var(--transition-increment) ease-in-out;
-    }
-  }
-</style>
+</StemSwipeIndicator>
--- projects/client/src/lib/sections/media-actions/mark-as-watched/MarkAsWatchedSwipeIndicator.svelte
+++ projects/client/src/lib/sections/media-actions/mark-as-watched/MarkAsWatchedSwipeIndicator.svelte
@@ -1,37 +1,13 @@
 <script lang="ts">
   import MarkAsWatchedIcon from "$lib/components/icons/MarkAsWatchedIcon.svelte";
+  import StemSwipeIndicator from "../_internal/StemSwipeIndicator.svelte";
 
   const { isActive }: { isActive: boolean } = $props();
 </script>
 
-<div class="trakt-gesture-indicator" class:trakt-gesture-active={isActive}>
+<StemSwipeIndicator
+  {isActive}
+  --color-gesture-active="var(--color-background-purple)"
+>
   <MarkAsWatchedIcon size="normal" state="unwatched" />
-</div>
-
-<style>
-  .trakt-gesture-indicator {
-    border-radius: var(--border-radius-m);
-
-    width: calc(var(--indicator-width, 0) + calc(var(--border-radius-m) * 2));
-
-    backdrop-filter: blur(var(--ni-8)) brightness(0.85);
-    color: var(--color-foreground);
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    outline: var(--color-foreground) var(--ni-1) solid;
-    outline-offset: var(--ni-neg-2);
-
-    &.trakt-gesture-active {
-      outline-color: var(--color-background-purple);
-      color: var(--color-background-purple);
-    }
-
-    :global(svg) {
-      opacity: var(--swipe-progress, 0);
-      transition: color var(--transition-increment) ease-in-out;
-    }
-  }
-</style>
+</StemSwipeIndicator>
