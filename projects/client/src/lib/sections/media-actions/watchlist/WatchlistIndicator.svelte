cc7bb87c78c239dd03740e8d5bcb3f673f2e847c
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-12 15:21:16 +0200

    feat: define media swipe actions for content lists

---
+++ projects/client/src/lib/sections/media-actions/watchlist/WatchlistIndicator.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import WatchlistIcon from "$lib/components/icons/WatchlistIcon.svelte";
+  import StemSwipeIndicator from "../_internal/StemSwipeIndicator.svelte";
+
+  const {
+    isActive,
+    isWatchlisted,
+  }: { isActive: boolean; isWatchlisted: boolean } = $props();
+
+  const state = $derived(isWatchlisted ? "added" : "missing");
+</script>
+
+<StemSwipeIndicator
+  {isActive}
+  --color-gesture-active={state === "missing"
+    ? "var(--color-background-blue)"
+    : "var(--color-background-red)"}
+>
+  <WatchlistIcon {state} />
+</StemSwipeIndicator>
