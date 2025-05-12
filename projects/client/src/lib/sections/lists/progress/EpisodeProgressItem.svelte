9accf1787704a5c2f8202208c6f3b0f3d528f2d5
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-12 12:47:42 +0200

    refactor(up-next): extract swipe component parent

--- projects/client/src/lib/sections/lists/progress/EpisodeProgressItem.svelte
+++ projects/client/src/lib/sections/lists/progress/EpisodeProgressItem.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-  import SwipeX from "$lib/components/gestures/SwipeX.svelte";
   import RenderFor from "$lib/guards/RenderFor.svelte";
   import type { EpisodeProgressEntry } from "$lib/requests/models/EpisodeProgressEntry";
   import type { ShowEntry } from "$lib/requests/models/ShowEntry";
-  import MarkAsWatchedSwipeIndicator from "$lib/sections/media-actions/mark-as-watched/MarkAsWatchedSwipeIndicator.svelte";
-  import { useMarkAsWatched } from "$lib/sections/media-actions/mark-as-watched/useMarkAsWatched";
   import RestoreAction from "$lib/sections/media-actions/restore/RestoreAction.svelte";
   import DropAction from "../../media-actions/drop/DropAction.svelte";
   import MarkAsWatchedAction from "../../media-actions/mark-as-watched/MarkAsWatchedAction.svelte";
   import EpisodeItem from "../components/EpisodeItem.svelte";
+  import EpisodeProgressSwipe from "./EpisodeProgressSwipe.svelte";
   import { useUpNextExperiment } from "./useUpNextExperiment";
 
   type UpNextEpisodeProps = {
@@ -23,18 +21,9 @@
   const { episode, show, status, style }: UpNextEpisodeProps = $props();
 
   const isHidden = $derived(status === "hidden");
-
-  const { markAsWatched } = $derived(
-    useMarkAsWatched({
-      type: "episode",
-      media: [episode],
-      show: show,
-      episode: episode,
-    }),
-  );
 </script>
 
-{#snippet card()}
+<EpisodeProgressSwipe {episode} {show} {style}>
   <EpisodeItem {episode} {show} {status} {style} variant="next">
     {#snippet popupActions()}
       <RenderFor audience="authenticated">
@@ -61,20 +50,4 @@
       </RenderFor>
     {/snippet}
   </EpisodeItem>
-{/snippet}
-
-{#if style === "summary"}
-  <SwipeX
-    children={card}
-    directions={["left"]}
-    classList="trakt-up-next-episode"
-    onSwipe={markAsWatched}
-    --indicator-height="var(--height-summary-card-cover)"
-  >
-    {#snippet indicator({ isActive })}
-      <MarkAsWatchedSwipeIndicator {isActive} />
-    {/snippet}
-  </SwipeX>
-{:else}
-  {@render card()}
-{/if}
+</EpisodeProgressSwipe>
