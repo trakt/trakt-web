01b53c04815edd8ef6e36bdeb64bd438ed9365d7
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-12 13:12:08 +0200

    refactor: extram stem swipe indicator

---
+++ projects/client/src/lib/sections/media-actions/_internal/StemSwipeIndicator.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  const { isActive, children }: { isActive: boolean } & ChildrenProps =
+    $props();
+</script>
+
+<div class="trakt-gesture-indicator" class:trakt-gesture-active={isActive}>
+  {@render children()}
+</div>
+
+<style>
+  .trakt-gesture-indicator {
+    --color-gesture-inactive: color-mix(
+      in srgb,
+      var(--color-foreground) 35%,
+      var(--color-background)
+    );
+    border-radius: var(--border-radius-m);
+
+    width: calc(var(--indicator-width, 0) + calc(var(--border-radius-m) * 2));
+
+    backdrop-filter: blur(var(--ni-8)) brightness(0.85);
+    color: var(--color-inactive-gesture);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    outline: var(--color-gesture-inactive) var(--ni-1) solid;
+    outline-offset: var(--ni-neg-2);
+
+    &.trakt-gesture-active {
+      outline-color: var(--color-gesture-active);
+      color: var(--color-gesture-active);
+    }
+
+    :global(svg) {
+      opacity: var(--swipe-progress, 0);
+      transition: color var(--transition-increment) ease-in-out;
+    }
+  }
+</style>
