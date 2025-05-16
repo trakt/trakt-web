f5f444c41d0cef7f0a2b9922ad78c8d603cbfc64
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-16 17:16:20 +0200

    feat(up-next): route director requests to hd instance

--- projects/client/src/lib/features/auth/token/index.ts
+++ projects/client/src/lib/features/auth/token/index.ts
@@ -1,11 +1,13 @@
 type Token = {
   value: string | Nil;
   expiresAt: number | Nil;
+  isDirector?: boolean;
 };
 
 const token: Token = {
   value: null,
   expiresAt: null,
+  isDirector: false,
 };
 
 export function getToken() {
@@ -13,6 +15,8 @@ export function getToken() {
 }
 
 export function setToken(newToken: Token | Nil) {
+  token.isDirector = newToken?.isDirector ?? false;
+
   if (!newToken) {
     token.value = null;
     token.expiresAt = null;
