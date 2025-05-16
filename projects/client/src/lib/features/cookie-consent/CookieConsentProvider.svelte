e898897ba89bab8618cfd7bcb1735d75758b14c9
seferturan <seferturan85@gmail.com>
2025-05-16 14:51:32 +0200

    fix(tv): do not flash cookie notice

--- projects/client/src/lib/features/cookie-consent/CookieConsentProvider.svelte
+++ projects/client/src/lib/features/cookie-consent/CookieConsentProvider.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
+  import type { DeviceType } from "$lib/models/DeviceType";
   import CookieNotice from "$lib/sections/cookie/CookieNotice.svelte";
-  import { getDeviceType } from "$lib/utils/devices/getDeviceType";
   import { useCookieConsent } from "./useCookieConsent";
 
-  const { hasConsent, children }: { hasConsent: boolean } & ChildrenProps =
-    $props();
+  const {
+    hasConsent,
+    device,
+    children,
+  }: { hasConsent: boolean; device: DeviceType } & ChildrenProps = $props();
 
   const { setConsent } = useCookieConsent();
   setConsent(hasConsent);
-
-  const isTV = $derived(getDeviceType(navigator.userAgent) === "tv");
 </script>
 
 {@render children()}
 
-{#if !isTV}
+{#if device !== "tv"}
   <CookieNotice />
 {/if}
