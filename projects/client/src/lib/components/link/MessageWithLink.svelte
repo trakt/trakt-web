9a50855181e48128bc6bc456f55cc99fe94693b2
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-20 15:50:54 +0200

    feat: define better cookie message

---
+++ projects/client/src/lib/components/link/MessageWithLink.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Link from "./Link.svelte";
+
+  const { message, href, target } = $props();
+
+  const [beforeText, linkText, afterText] = message.split(/<a[^>]*>|<\/a>/);
+</script>
+
+{beforeText}<Link {href} {target}>{linkText}</Link>{afterText}
