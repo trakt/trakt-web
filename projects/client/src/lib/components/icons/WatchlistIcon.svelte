8b20f4497fa0e1064d2c6adfee901cc34377c771
Vlad Jerca <vlad.jerca@gmail.com>
2025-06-04 02:32:34 +0200

    fix(watchlist): add missing data-size property

--- projects/client/src/lib/components/icons/WatchlistIcon.svelte
+++ projects/client/src/lib/components/icons/WatchlistIcon.svelte
@@ -12,6 +12,7 @@
   viewBox="0 0 24 24"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
+  data-size={size}
 >
   {#if state === "missing"}
     <path d="M12 2L12 22" stroke="currentColor" stroke-width="2" />
