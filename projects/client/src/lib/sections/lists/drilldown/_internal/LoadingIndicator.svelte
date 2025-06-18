3227909c1a0801ec727e2b79c8bdec65d78f158c
seferturan <seferturan85@gmail.com>
2025-06-18 11:57:38 +0200

    feat(list): use icon for loading indicator

--- projects/client/src/lib/sections/lists/drilldown/_internal/LoadingIndicator.svelte
+++ projects/client/src/lib/sections/lists/drilldown/_internal/LoadingIndicator.svelte
@@ -1,28 +1,23 @@
 <script>
+  import LoaderIcon from "$lib/components/icons/LoaderIcon.svelte";
   import { fade } from "svelte/transition";
 </script>
 
 <div class="loading-indicator" out:fade={{ duration: 150 }}>
-  <div class="spinner"></div>
+  <LoaderIcon />
 </div>
 
 <style>
   .loading-indicator {
     display: flex;
     justify-content: center;
-  }
 
-  .spinner {
-    width: var(--ni-56);
-    height: var(--ni-56);
-    border: var(--ni-16) solid;
+    :global(svg) {
+      width: var(--ni-56);
+      height: var(--ni-56);
 
-    box-sizing: border-box;
-    border-radius: 50%;
-    border-color: var(--purple-100);
-    border-right-color: var(--purple-500);
-
-    animation: spin 1s infinite linear;
+      animation: spin 2s infinite linear;
+    }
   }
 
   @keyframes spin {
