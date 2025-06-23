ce9f86032f77196693d907b57e9c5fdceb95f731
Vlad Jerca <vlad.jerca@gmail.com>
2025-06-23 21:20:56 +0200

    feat(formatter): improve human-readable ETA
    Refines the date formatting logic in `toHumanETA` to provide more accurate and user-friendly estimated time arrivals.
    
    It now considers fractional days and hours, rounding them up to the nearest whole unit, providing a more intuitive representation of time differences. It also handles times after midnight correctly.
    
    Adds comprehensive test cases to cover various scenarios, including fractional days, hours, and different timezones.

--- projects/client/src/lib/utils/formatting/date/toHumanETA.ts
+++ projects/client/src/lib/utils/formatting/date/toHumanETA.ts
@@ -5,17 +5,30 @@ export function toHumanETA(
   targetDate: Date,
   locale: AvailableLocale = 'en',
 ): string {
-  const MS_PER_DAY = 1000 * 60 * 60 * 24;
-  const days = Math.round(
-    (targetDate.getTime() - today.getTime()) / MS_PER_DAY,
-  );
+  const MS_PER_HOUR = 1000 * 60 * 60;
+  const MS_PER_DAY = MS_PER_HOUR * 24;
 
-  const isPastDate = days < 0;
+  const timeDiff = targetDate.getTime() - today.getTime();
+  const days = Math.ceil(timeDiff / MS_PER_DAY);
+  const hours = Math.ceil(timeDiff / MS_PER_HOUR);
+
+  const isPastDate = timeDiff < 0;
   const rtf = new Intl.RelativeTimeFormat(locale, { numeric: 'auto' });
   const year = targetDate.getFullYear().toString();
 
   if (isPastDate) {
     return year;
+  }
+
+  const isTargetAfterMidnight = targetDate.getUTCHours() >= 0 &&
+    targetDate.getUTCHours() < 6;
+
+  const isSameDay = today.getUTCFullYear() === targetDate.getUTCFullYear() &&
+    today.getUTCMonth() === targetDate.getUTCMonth() &&
+    today.getUTCDate() === targetDate.getUTCDate();
+
+  if (isSameDay && !isTargetAfterMidnight) {
+    return rtf.format(hours, 'hour');
   }
 
   if (days <= 6) {
