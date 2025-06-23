trakt/trakt-lite

refactor: split up monolith files

projects/client/i18n/generator/factory/GeneratorFactory.spec.ts
@@ -1,0 +1,157 @@
+/**
+ * Tests for GeneratorFactory
+ */
+
+import { describe, expect, it } from 'vitest';
+import type { MetaMessages } from '../model/MetaMessages.ts';
+import { Platform } from '../model/Platform.ts';
+import { AndroidGenerator } from '../platform/AndroidGenerator.ts';
+import { IOSGenerator } from '../platform/IOSGenerator.ts';
+import { WebGenerator } from '../platform/WebGenerator.ts';
+import { GeneratorFactory } from './GeneratorFactory.ts';
+
+describe('GeneratorFactory', () => {
+  describe('create', () => {
+    it('should create WebGenerator for WEB platform', () => {
+      const generator = GeneratorFactory.create(Platform.WEB);
+      expect(generator).toBeInstanceOf(WebGenerator);
+    });
+
+    it('should create AndroidGenerator for ANDROID platform', () => {
+      const generator = GeneratorFactory.create(Platform.ANDROID);
+      expect(generator).toBeInstanceOf(AndroidGenerator);
+    });
+
+    it('should create IOSGenerator for IOS platform', () => {
+      const generator = GeneratorFactory.create(Platform.IOS);
+      expect(generator).toBeInstanceOf(IOSGenerator);
+    });
+
+    it('should throw error for unsupported platform', () => {
+      expect(() => {
+        GeneratorFactory.create('unknown' as Platform);
+      }).toThrow('Unsupported platform: unknown');
+    });
+  });
+
+  describe('getEnabledPlatforms', () => {
+    it('should return all enabled platforms', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+          generator: {
+            inlang: {
+              enabled: true,
+              outputPath: './messages/{locale}.json',
+            },
+            android: {
+              enabled: true,
+              outputPath: './android/values-{locale}/strings.xml',
+              resourceName: 'strings',
+            },
+            ios: {
+              enabled: true,
+              outputPath: './ios/Localizable.xcstrings',
+            },
+          },
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(3);
+      expect(platforms).toContain(Platform.WEB);
+      expect(platforms).toContain(Platform.ANDROID);
+      expect(platforms).toContain(Platform.IOS);
+    });
+
+    it('should return only enabled platforms', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+          generator: {
+            inlang: {
+              enabled: true,
+              outputPath: './messages/{locale}.json',
+            },
+            android: {
+              enabled: false,
+              outputPath: './android/values-{locale}/strings.xml',
+              resourceName: 'strings',
+            },
+            ios: {
+              enabled: true,
+              outputPath: './ios/Localizable.xcstrings',
+            },
+          },
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(2);
+      expect(platforms).toContain(Platform.WEB);
+      expect(platforms).toContain(Platform.IOS);
+      expect(platforms).not.toContain(Platform.ANDROID);
+    });
+
+    it('should return empty array when no platforms are enabled', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+          generator: {
+            inlang: {
+              enabled: false,
+              outputPath: './messages/{locale}.json',
+            },
+            android: {
+              enabled: false,
+              outputPath: './android/values-{locale}/strings.xml',
+              resourceName: 'strings',
+            },
+            ios: {
+              enabled: false,
+              outputPath: './ios/Localizable.xcstrings',
+            },
+          },
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(0);
+    });
+
+    it('should return empty array when no generator config exists', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(0);
+    });
+
+    it('should handle partial generator configuration', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+          generator: {
+            inlang: {
+              enabled: true,
+              outputPath: './messages/{locale}.json',
+            },
+            // Missing android and ios configs
+          },
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(1);
+      expect(platforms).toContain(Platform.WEB);
+    });
+  });
+});

projects/client/i18n/generator/platform/WebGenerator.spec.ts
@@ -1,0 +1,137 @@
+/**
+ * Tests for WebGenerator
+ */
+
+import * as fs from 'node:fs';
+import * as os from 'node:os';
+import * as path from 'node:path';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import type { MetaMessages } from '../model/MetaMessages.ts';
+import { Platform } from '../model/Platform.ts';
+import { WebGenerator } from './WebGenerator.ts';
+
+describe('WebGenerator', () => {
+  let tempDir: string;
+  let generator: WebGenerator;
+
+  beforeEach(async () => {
+    tempDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'i18n-test-'));
+    generator = new WebGenerator();
+  });
+
+  afterEach(async () => {
+    if (tempDir) {
+      await fs.promises.rm(tempDir, { recursive: true, force: true });
+    }
+  });
+
+  it('should generate correct Inlang JSON format', async () => {
+    const testMetaMessages: MetaMessages = {
+      meta: {
+        locale: 'en',
+        direction: 'ltr',
+        generator: {
+          inlang: {
+            enabled: true,
+            outputPath: './messages/{locale}.json',
+          },
+        },
+      },
+      messages: {
+        simple_message: 'Hello World',
+        greeting: {
+          default: 'Hello, {name}!',
+          description: 'Greeting message with user name',
+          variables: {
+            name: {
+              type: 'string',
+              description: 'User display name',
+            },
+          },
+        },
+      },
+    };
+
+    const results = await generator.generate([testMetaMessages], tempDir);
+    expect(results).toHaveLength(1);
+    expect(results[0]!.platform).toBe('web');
+
+    const content = JSON.parse(results[0]!.content);
+    expect(content.$schema).toBe(
+      'https://inlang.com/schema/inlang-message-format',
+    );
+    expect(content.simple_message).toBe('Hello World');
+    expect(content.greeting).toBe('Hello, {name}!');
+  });
+
+  it('should exclude messages marked for web exclusion', async () => {
+    const testMetaMessages: MetaMessages = {
+      meta: {
+        locale: 'en',
+        direction: 'ltr',
+        generator: {
+          inlang: {
+            enabled: true,
+            outputPath: './messages/{locale}.json',
+          },
+        },
+      },
+      messages: {
+        included_message: 'This should be included',
+        excluded_message: {
+          default: 'This should be excluded',
+          exclude: ['web' as Platform],
+        },
+      },
+    };
+
+    const results = await generator.generate([testMetaMessages], tempDir);
+    const content = JSON.parse(results[0]!.content);
+
+    expect(content.included_message).toBe('This should be included');
+    expect(content.excluded_message).toBeUndefined();
+  });
+
+  it('should skip generation when inlang is not enabled', async () => {
+    const testMetaMessages: MetaMessages = {
+      meta: {
+        locale: 'en',
+        direction: 'ltr',
+        generator: {
+          inlang: {
+            enabled: false,
+            outputPath: './messages/{locale}.json',
+          },
+        },
+      },
+      messages: {
+        test: 'Test message',
+      },
+    };
+
+    const results = await generator.generate([testMetaMessages], tempDir);
+    expect(results).toHaveLength(0);
+  });
+
+  it('should handle simple string messages', async () => {
+    const testMetaMessages: MetaMessages = {
+      meta: {
+        locale: 'en',
+        direction: 'ltr',
+        generator: {
+          inlang: {
+            enabled: true,
+            outputPath: './messages/{locale}.json',
+          },
+        },
+      },
+      messages: {
+        simple: 'Simple message',
+      },
+    };
+
+    const results = await generator.generate([testMetaMessages], tempDir);
+    const content = JSON.parse(results[0]!.content);
+    expect(content.simple).toBe('Simple message');
+  });
+});

trakt/trakt-web

projects/client/i18n/generator/factory/GeneratorFactory.spec.ts
@@ -1,0 +1,157 @@
+/**
+ * Tests for GeneratorFactory
+ */
+
+import { describe, expect, it } from 'vitest';
+import type { MetaMessages } from '../model/MetaMessages.ts';
+import { Platform } from '../model/Platform.ts';
+import { AndroidGenerator } from '../platform/AndroidGenerator.ts';
+import { IOSGenerator } from '../platform/IOSGenerator.ts';
+import { WebGenerator } from '../platform/WebGenerator.ts';
+import { GeneratorFactory } from './GeneratorFactory.ts';
+
+describe('GeneratorFactory', () => {
+  describe('create', () => {
+    it('should create WebGenerator for WEB platform', () => {
+      const generator = GeneratorFactory.create(Platform.WEB);
+      expect(generator).toBe(WebGenerator);
+    });
+
+    it('should create AndroidGenerator for ANDROID platform', () => {
+      const generator = GeneratorFactory.create(Platform.ANDROID);
+      expect(generator).toBe(AndroidGenerator);
+    });
+
+    it('should create IOSGenerator for IOS platform', () => {
+      const generator = GeneratorFactory.create(Platform.IOS);
+      expect(generator).toBe(IOSGenerator);
+    });
+
+    it('should throw error for unsupported platform', () => {
+      expect(() => {
+        GeneratorFactory.create('unknown' as Platform);
+      }).toThrow('Unsupported platform: unknown');
+    });
+  });
+
+  describe('getEnabledPlatforms', () => {
+    it('should return all enabled platforms', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+          generator: {
+            inlang: {
+              enabled: true,
+              outputPath: './messages/{locale}.json',
+            },
+            android: {
+              enabled: true,
+              outputPath: './android/values-{locale}/strings.xml',
+              resourceName: 'strings',
+            },
+            ios: {
+              enabled: true,
+              outputPath: './ios/Localizable.xcstrings',
+            },
+          },
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(3);
+      expect(platforms).toContain(Platform.WEB);
+      expect(platforms).toContain(Platform.ANDROID);
+      expect(platforms).toContain(Platform.IOS);
+    });
+
+    it('should return only enabled platforms', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+          generator: {
+            inlang: {
+              enabled: true,
+              outputPath: './messages/{locale}.json',
+            },
+            android: {
+              enabled: false,
+              outputPath: './android/values-{locale}/strings.xml',
+              resourceName: 'strings',
+            },
+            ios: {
+              enabled: true,
+              outputPath: './ios/Localizable.xcstrings',
+            },
+          },
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(2);
+      expect(platforms).toContain(Platform.WEB);
+      expect(platforms).toContain(Platform.IOS);
+      expect(platforms).not.toContain(Platform.ANDROID);
+    });
+
+    it('should return empty array when no platforms are enabled', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+          generator: {
+            inlang: {
+              enabled: false,
+              outputPath: './messages/{locale}.json',
+            },
+            android: {
+              enabled: false,
+              outputPath: './android/values-{locale}/strings.xml',
+              resourceName: 'strings',
+            },
+            ios: {
+              enabled: false,
+              outputPath: './ios/Localizable.xcstrings',
+            },
+          },
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(0);
+    });
+
+    it('should return empty array when no generator config exists', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(0);
+    });
+
+    it('should handle partial generator configuration', () => {
+      const metaMessages: MetaMessages = {
+        meta: {
+          locale: 'en',
+          generator: {
+            inlang: {
+              enabled: true,
+              outputPath: './messages/{locale}.json',
+            },
+            // Missing android and ios configs
+          },
+        },
+        messages: {},
+      };
+
+      const platforms = GeneratorFactory.getEnabledPlatforms(metaMessages);
+      expect(platforms).toHaveLength(1);
+      expect(platforms).toContain(Platform.WEB);
+    });
+  });
+});

projects/client/i18n/generator/platform/WebGenerator.spec.ts
@@ -1,0 +1,139 @@
+/**
+ * Tests for WebGenerator
+ */
+
+import * as fs from 'node:fs';
+import * as os from 'node:os';
+import * as path from 'node:path';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { firstItem } from '../../../src/lib/utils/assert/firstItem.ts';
+import type { PlatformGenerator } from '../core/I18nGenerator.ts';
+import type { MetaMessages } from '../model/MetaMessages.ts';
+import { Platform } from '../model/Platform.ts';
+import { WebGenerator } from './WebGenerator.ts';
+
+describe('WebGenerator', () => {
+  let tempDir: string;
+  let generator: PlatformGenerator;
+
+  beforeEach(async () => {
+    tempDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'i18n-test-'));
+    generator = WebGenerator;
+  });
+
+  afterEach(async () => {
+    if (tempDir) {
+      await fs.promises.rm(tempDir, { recursive: true, force: true });
+    }
+  });
+
+  it('should generate correct Inlang JSON format', async () => {
+    const testMetaMessages: MetaMessages = {
+      meta: {
+        locale: 'en',
+        direction: 'ltr',
+        generator: {
+          inlang: {
+            enabled: true,
+            outputPath: './messages/{locale}.json',
+          },
+        },
+      },
+      messages: {
+        simple_message: 'Hello World',
+        greeting: {
+          default: 'Hello, {name}!',
+          description: 'Greeting message with user name',
+          variables: {
+            name: {
+              type: 'string',
+              description: 'User display name',
+            },
+          },
+        },
+      },
+    };
+
+    const results = await generator.generate([testMetaMessages], tempDir);
+    expect(results).toHaveLength(1);
+    expect(firstItem(results).platform).toBe('web');
+
+    const content = JSON.parse(firstItem(results).content);
+    expect(content.$schema).toBe(
+      'https://inlang.com/schema/inlang-message-format',
+    );
+    expect(content.simple_message).toBe('Hello World');
+    expect(content.greeting).toBe('Hello, {name}!');
+  });
+
+  it('should exclude messages marked for web exclusion', async () => {
+    const testMetaMessages: MetaMessages = {
+      meta: {
+        locale: 'en',
+        direction: 'ltr',
+        generator: {
+          inlang: {
+            enabled: true,
+            outputPath: './messages/{locale}.json',
+          },
+        },
+      },
+      messages: {
+        included_message: 'This should be included',
+        excluded_message: {
+          default: 'This should be excluded',
+          exclude: ['web' as Platform],
+        },
+      },
+    };
+
+    const results = await generator.generate([testMetaMessages], tempDir);
+    const content = JSON.parse(firstItem(results).content);
+
+    expect(content.included_message).toBe('This should be included');
+    expect(content.excluded_message).toBeUndefined();
+  });
+
+  it('should skip generation when inlang is not enabled', async () => {
+    const testMetaMessages: MetaMessages = {
+      meta: {
+        locale: 'en',
+        direction: 'ltr',
+        generator: {
+          inlang: {
+            enabled: false,
+            outputPath: './messages/{locale}.json',
+          },
+        },
+      },
+      messages: {
+        test: 'Test message',
+      },
+    };
+
+    const results = await generator.generate([testMetaMessages], tempDir);
+    expect(results).toHaveLength(0);
+  });
+
+  it('should handle simple string messages', async () => {
+    const testMetaMessages: MetaMessages = {
+      meta: {
+        locale: 'en',
+        direction: 'ltr',
+        generator: {
+          inlang: {
+            enabled: true,
+            outputPath: './messages/{locale}.json',
+          },
+        },
+      },
+      messages: {
+        simple: 'Simple message',
+      },
+    };
+
+    const results = await generator.generate([testMetaMessages], tempDir);
+    const content = JSON.parse(firstItem(results).content);
+    expect(content.simple).toBe('Simple message');
+  });
+});