40d1afd75c0f6a1678eafb0f3db1a408b7e4b11c
seferturan <seferturan85@gmail.com>
2025-07-13 20:15:32 +0200

    chore: removes stream on plex for now

--- projects/client/src/lib/sections/summary/components/stream/StreamOnButton.svelte
+++ projects/client/src/lib/sections/summary/components/stream/StreamOnButton.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import PlexButton from "$lib/components/buttons/plex/PlexButton.svelte";
   import StreamingServiceButton from "$lib/components/buttons/streaming-service/StreamingServiceButton.svelte";
   import RenderFor from "$lib/guards/RenderFor.svelte";
   import type { StreamOnButtonProps } from "./StreamOnButtonProps";
@@ -15,27 +14,15 @@
     target.type === "episode" ? target.episode.airDate : target.media.airDate,
   );
   const isAiredItem = $derived(airDate < new Date());
-
-  const hasPlexSlug = $derived(target.media.plexSlug !== undefined);
 </script>
 
-{#if isAiredItem}
+{#if isAiredItem && streamOn?.preferred}
   <RenderFor audience="authenticated">
-    {#if streamOn?.preferred}
-      <StreamingServiceButton
-        service={streamOn.preferred}
-        mediaTitle={target.media.title}
-        {style}
-        {size}
-      />
-    {:else if hasPlexSlug}
-      <PlexButton {style} {size} {target} />
-    {/if}
-  </RenderFor>
-
-  <RenderFor audience="director">
-    {#if streamOn?.preferred && hasPlexSlug}
-      <PlexButton {style} {size} {target} />
-    {/if}
+    <StreamingServiceButton
+      service={streamOn.preferred}
+      mediaTitle={target.media.title}
+      {style}
+      {size}
+    />
   </RenderFor>
 {/if}
