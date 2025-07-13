7f8af35274a0933eb42d496b6b1fa85295035faa
seferturan <seferturan85@gmail.com>
2025-07-13 20:50:15 +0200

    fix(auth): do not render content on the callback page

--- projects/client/src/routes/callback/+page.svelte
+++ projects/client/src/routes/callback/+page.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
   import { getUserManager } from "$lib/features/auth/stores/userManager";
+  import { error as printError } from "$lib/utils/console/print.ts";
   import { setCacheBuster } from "$lib/utils/url/setCacheBuster";
   import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
   import { onMount } from "svelte";
 
+  const navigateToHome = () => {
+    const homeUrl = new URL(
+      UrlBuilder.home(),
+      globalThis.window.location.origin,
+    );
+    globalThis.window.location.replace(setCacheBuster(homeUrl));
+  };
+
   onMount(() => {
     getUserManager()
       ?.signinCallback()
-      .then(() => {
-        const homeUrl = new URL(
-          UrlBuilder.home(),
-          globalThis.window.location.origin,
-        );
-        globalThis.window.location.replace(setCacheBuster(homeUrl));
+      .then(navigateToHome)
+      .catch((error) => {
+        printError("Error during sign-in callback:", error);
+        navigateToHome();
       });
   });
 </script>
+
+<style>
+  :global(body) {
+    display: none;
+  }
+</style>
