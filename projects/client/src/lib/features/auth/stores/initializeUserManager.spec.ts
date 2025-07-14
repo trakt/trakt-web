f411b3341475d93af33c71d0f4a589c6253d6424
seferturan <seferturan85@gmail.com>
2025-07-14 12:16:40 +0200

    test(auth): setting authorization state

--- projects/client/src/lib/features/auth/stores/initializeUserManager.spec.ts
+++ projects/client/src/lib/features/auth/stores/initializeUserManager.spec.ts
@@ -1,25 +1,31 @@
 import { time } from '$lib/utils/timing/time.ts';
 import { OidcUserMock } from '$mocks/data/auth/OidcUserMock.ts';
 import { renderStore, setAuthorization } from '$test/beds/store/renderStore.ts';
+import { get } from 'svelte/store';
 import { describe, expect, it } from 'vitest';
 import { getToken } from '../token/index.ts';
 import { initializeUserManager } from './initializeUserManager.ts';
+import { useAuth } from './useAuth.ts';
 
 describe('initializeUserManager', () => {
   it('should initialize unauthorized users', async () => {
     await renderStore(() => initializeUserManager(false));
+    const { isAuthorized } = await renderStore(() => useAuth());
 
     const token = getToken();
     expect(token.expiresAt).toBeNull();
     expect(token.value).toBeNull();
+    expect(get(isAuthorized)).toBe(false);
   });
 
   it('should initialize authorized users', async () => {
     setAuthorization(true);
     await renderStore(() => initializeUserManager(false));
+    const { isAuthorized } = await renderStore(() => useAuth());
 
     const token = getToken();
     expect(token.expiresAt).toEqual(time.seconds(OidcUserMock.expires_at));
     expect(token.value).toEqual(OidcUserMock.access_token);
+    expect(get(isAuthorized)).toBe(true);
   });
 });
