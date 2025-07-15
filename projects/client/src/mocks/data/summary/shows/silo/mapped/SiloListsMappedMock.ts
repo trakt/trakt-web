1a543488396d39db0d1603b98414855b11469b8e
seferturan <seferturan85@gmail.com>
2025-07-15 12:30:09 +0200

    test(requests): mapping list posters

--- projects/client/src/mocks/data/summary/shows/silo/mapped/SiloListsMappedMock.ts
+++ projects/client/src/mocks/data/summary/shows/silo/mapped/SiloListsMappedMock.ts
@@ -9,6 +9,87 @@ export const SiloListsMappedMock: MediaListSummary[] = [
     'slug': 'silos',
     'user': UserProfileHarryMappedMock,
     'count': 1,
-    'posters': [],
+    'posters': [
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/173/996/posters/medium/f498a2fa2b.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/173/996/posters/thumb/f498a2fa2b.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/156/110/posters/medium/1d002ba117.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/156/110/posters/thumb/1d002ba117.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/126/995/posters/medium/55b80503e9.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/126/995/posters/thumb/55b80503e9.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/170/649/posters/medium/39eb828a7a.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/170/649/posters/thumb/39eb828a7a.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/043/764/posters/medium/9ef9162506.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/043/764/posters/thumb/9ef9162506.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/058/454/posters/medium/e7ca7525cb.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/058/454/posters/thumb/e7ca7525cb.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/134/421/posters/medium/6295516e91.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/134/421/posters/thumb/6295516e91.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/001/390/posters/medium/93df9cd612.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/001/390/posters/thumb/93df9cd612.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/104/578/posters/medium/f43fc7f7db.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/104/578/posters/thumb/f43fc7f7db.jpg',
+        },
+      },
+      {
+        'url': {
+          'medium':
+            'https://walter-r2.trakt.tv/images/shows/000/191/758/posters/medium/3917ba20d9.jpg',
+          'thumb':
+            'https://walter-r2.trakt.tv/images/shows/000/191/758/posters/thumb/3917ba20d9.jpg',
+        },
+      },
+    ],
   },
 ];
