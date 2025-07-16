192944af8d51bcc86fc9481b9e5c4d2310ae8552
seferturan <seferturan85@gmail.com>
2025-07-16 12:04:39 +0200

    refactor: simplify list poster previews

--- projects/client/src/lib/requests/_internal/mapToMediaListSummary.ts
+++ projects/client/src/lib/requests/_internal/mapToMediaListSummary.ts
@@ -3,14 +3,10 @@ import type { MediaListSummary } from '../models/MediaListSummary.ts';
 import { mapToPoster } from './mapToPoster.ts';
 import { mapToUserProfile } from './mapToUserProfile.ts';
 
-// FIXME: remove this when the API is fixed
-const PLACEHOLDERS_PATH = 'placeholders/original/poster.png';
-
 export function mapToMediaListSummary(
   listResponse: ListResponse,
 ): MediaListSummary {
   const posters = (listResponse.images?.posters ?? [])
-    .filter((poster) => poster !== PLACEHOLDERS_PATH)
     .map((poster) => mapToPoster({ poster: [poster] }));
 
   return {
--- projects/client/src/mocks/data/summary/shows/silo/mapped/SiloListsMappedMock.ts
+++ projects/client/src/mocks/data/summary/shows/silo/mapped/SiloListsMappedMock.ts
@@ -13,81 +13,81 @@ export const SiloListsMappedMock: MediaListSummary[] = [
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/173/996/posters/medium/f498a2fa2b.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/173/996/posters/medium/f498a2fa2b.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/173/996/posters/thumb/f498a2fa2b.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/173/996/posters/thumb/f498a2fa2b.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/156/110/posters/medium/1d002ba117.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/156/110/posters/medium/1d002ba117.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/156/110/posters/thumb/1d002ba117.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/156/110/posters/thumb/1d002ba117.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/126/995/posters/medium/55b80503e9.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/126/995/posters/medium/55b80503e9.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/126/995/posters/thumb/55b80503e9.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/126/995/posters/thumb/55b80503e9.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/170/649/posters/medium/39eb828a7a.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/170/649/posters/medium/39eb828a7a.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/170/649/posters/thumb/39eb828a7a.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/170/649/posters/thumb/39eb828a7a.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/043/764/posters/medium/9ef9162506.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/043/764/posters/medium/9ef9162506.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/043/764/posters/thumb/9ef9162506.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/043/764/posters/thumb/9ef9162506.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/058/454/posters/medium/e7ca7525cb.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/058/454/posters/medium/e7ca7525cb.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/058/454/posters/thumb/e7ca7525cb.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/058/454/posters/thumb/e7ca7525cb.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/134/421/posters/medium/6295516e91.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/134/421/posters/medium/6295516e91.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/134/421/posters/thumb/6295516e91.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/134/421/posters/thumb/6295516e91.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/001/390/posters/medium/93df9cd612.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/001/390/posters/medium/93df9cd612.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/001/390/posters/thumb/93df9cd612.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/001/390/posters/thumb/93df9cd612.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/104/578/posters/medium/f43fc7f7db.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/104/578/posters/medium/f43fc7f7db.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/104/578/posters/thumb/f43fc7f7db.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/104/578/posters/thumb/f43fc7f7db.jpg.webp',
         },
       },
       {
         'url': {
           'medium':
-            'https://walter-r2.trakt.tv/images/shows/000/191/758/posters/medium/3917ba20d9.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/191/758/posters/medium/3917ba20d9.jpg.webp',
           'thumb':
-            'https://walter-r2.trakt.tv/images/shows/000/191/758/posters/thumb/3917ba20d9.jpg',
+            'https://walter-r2.trakt.tv/images/shows/000/191/758/posters/thumb/3917ba20d9.jpg.webp',
         },
       },
     ],
