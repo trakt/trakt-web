c5f4fc9caa727f2d9de226c8e442f17176cc85ad
seferturan <seferturan85@gmail.com>
2025-07-23 10:32:18 +0200

    fix(comment): removes unrealistic sentiment percentages

--- projects/client/src/lib/sections/summary/components/comments/_internal/SentimentHeader.svelte
+++ projects/client/src/lib/sections/summary/components/comments/_internal/SentimentHeader.svelte
@@ -1,33 +1,12 @@
 <script lang="ts">
   import CircularLogo from "$lib/components/icons/CircularLogo.svelte";
-  import { languageTag } from "$lib/features/i18n";
   import * as m from "$lib/features/i18n/messages.ts";
-  import { SimpleRating } from "$lib/models/SimpleRating";
-  import type { Sentiments } from "$lib/requests/models/Sentiments";
-  import { toPercentage } from "$lib/utils/formatting/number/toPercentage";
-
-  const { comment }: { comment: Sentiments } = $props();
-
-  const sentimentCount = $derived(comment.good.length + comment.bad.length);
-  const goodFactor = $derived(
-    sentimentCount > 0 ? comment.good.length / sentimentCount : 0,
-  );
-  const badFactor = $derived(1.0 - goodFactor);
 </script>
 
 <div class="trakt-sentiment-header">
   <CircularLogo variant="gradient" />
   <div class="trakt-sentiment-details">
     <p>{m.header_community_sentiment()}</p>
-    <div class="trakt-sentiment-distribution">
-      <p class="meta-info sentiment-good">
-        {toPercentage(goodFactor, languageTag())}
-      </p>
-      <p class="meta-info">/</p>
-      <p data-rating={SimpleRating.Bad} class="meta-info sentiment-bad">
-        {toPercentage(badFactor, languageTag())}
-      </p>
-    </div>
   </div>
 </div>
 
@@ -39,19 +18,5 @@
     height: var(--ni-32);
 
     position: relative;
-  }
-
-  .trakt-sentiment-distribution {
-    display: flex;
-    align-items: center;
-    gap: var(--gap-xxs);
-  }
-
-  .sentiment-good {
-    color: var(--color-text-sentiment-good);
-  }
-
-  .sentiment-bad {
-    color: var(--color-text-sentiment-bad);
   }
 </style>
