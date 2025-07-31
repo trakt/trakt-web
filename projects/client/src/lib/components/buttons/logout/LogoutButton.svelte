1b91ed54d916afa8e0d821ba85975c89da22bccf
seferturan <seferturan85@gmail.com>
2025-07-31 20:10:15 +0200

    chore: put settings under a feature flag

--- projects/client/src/lib/components/buttons/logout/LogoutButton.svelte
+++ projects/client/src/lib/components/buttons/logout/LogoutButton.svelte
@@ -1,19 +1,35 @@
 <script lang="ts">
   import Button from "$lib/components/buttons/Button.svelte";
+  import LogoutIcon from "$lib/components/icons/LogoutIcon.svelte";
   import { useAuth } from "$lib/features/auth/stores/useAuth";
   import * as m from "$lib/features/i18n/messages.ts";
   import { DpadNavigationType } from "$lib/features/navigation/models/DpadNavigationType";
+  import ActionButton from "../ActionButton.svelte";
+
+  const { style = "normal" }: { style?: "normal" | "action" } = $props();
 
   const { logout } = useAuth();
+
+  const commonProps: Omit<ButtonProps, "children"> = $derived({
+    label: m.button_label_logout(),
+    color: "red",
+    onclick: logout,
+  });
 </script>
 
-<Button
-  size="small"
-  onclick={logout}
-  label={m.button_label_logout()}
-  color="red"
-  style="flat"
-  navigationType={DpadNavigationType.Item}
->
-  {m.button_text_logout()}
-</Button>
+{#if style === "normal"}
+  <Button
+    size="small"
+    style="flat"
+    navigationType={DpadNavigationType.Item}
+    {...commonProps}
+  >
+    {m.button_text_logout()}
+  </Button>
+{/if}
+
+{#if style === "action"}
+  <ActionButton variant="secondary" {...commonProps}>
+    <LogoutIcon />
+  </ActionButton>
+{/if}
--- projects/client/src/lib/components/buttons/settings/SettingsButton.svelte
+++ projects/client/src/lib/components/buttons/settings/SettingsButton.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
   import ActionButton from "$lib/components/buttons/ActionButton.svelte";
   import GearIcon from "$lib/components/icons/GearIcon.svelte";
+  import { FeatureFlag } from "$lib/features/feature-flag/models/FeatureFlag";
   import * as m from "$lib/features/i18n/messages.ts";
+  import RenderForFeature from "$lib/guards/RenderForFeature.svelte";
   import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import LogoutButton from "../logout/LogoutButton.svelte";
 
   const { style = "flat" }: { style?: "flat" | "ghost" } = $props();
 </script>
 
-<ActionButton
-  {style}
-  href={UrlBuilder.settings()}
-  label={m.button_label_settings()}
->
-  <GearIcon />
-</ActionButton>
+<RenderForFeature flag={FeatureFlag.Settings}>
+  {#snippet enabled()}
+    <ActionButton
+      {style}
+      href={UrlBuilder.settings()}
+      label={m.button_label_settings()}
+    >
+      <GearIcon />
+    </ActionButton>
+  {/snippet}
+
+  <LogoutButton style="action" />
+</RenderForFeature>
