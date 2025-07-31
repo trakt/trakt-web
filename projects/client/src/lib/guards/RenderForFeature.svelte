9cf78dc1f48d2267fa01ab4ce595452a24c1b438
seferturan <seferturan85@gmail.com>
2025-07-31 15:26:59 +0200

    fix(comment): place reactions under feature flag

---
+++ projects/client/src/lib/guards/RenderForFeature.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { FeatureFlag } from "$lib/features/feature-flag/models/FeatureFlag";
+  import { useFeatureFlag } from "$lib/features/feature-flag/useFeatureFlag";
+  import type { Snippet } from "svelte";
+
+  const {
+    flag,
+    enabled,
+    children,
+  }: { flag: FeatureFlag; enabled: Snippet } & ChildrenProps = $props();
+
+  const { isEnabled } = $derived(useFeatureFlag(flag));
+</script>
+
+{#if $isEnabled}
+  {@render enabled()}
+{:else}
+  {@render children()}
+{/if}
