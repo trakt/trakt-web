8c7250aa46433c8c9af5edb44ec0184545ddb995
seferturan <seferturan85@gmail.com>
2025-07-31 20:10:15 +0200

    chore: put settings under a feature flag

---
+++ projects/client/src/lib/components/icons/LogoutIcon.svelte
@@ -0,0 +1,12 @@
+<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  style="transform: rotate(90deg);"
+>
+  <path d="M6 9L12 3L18 9" stroke="currentColor" stroke-width="2" />
+  <path d="M12 3V15" stroke="currentColor" stroke-width="2" />
+  <path d="M4 12V20H20V12" stroke="currentColor" stroke-width="2" />
+</svg>
--- projects/client/src/lib/features/feature-flag/models/FeatureFlag.ts
+++ projects/client/src/lib/features/feature-flag/models/FeatureFlag.ts
@@ -1,3 +1,4 @@
 export enum FeatureFlag {
   Reactions = 'reactions',
+  Settings = 'settings',
 }
