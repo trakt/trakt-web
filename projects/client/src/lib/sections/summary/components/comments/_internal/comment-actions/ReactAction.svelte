acf2c3cee38ec2b9192c9c0f96519b7dbe7042b2
seferturan <seferturan85@gmail.com>
2025-07-31 15:26:59 +0200

    fix(comment): place reactions under feature flag

--- projects/client/src/lib/sections/summary/components/comments/_internal/comment-actions/ReactAction.svelte
+++ projects/client/src/lib/sections/summary/components/comments/_internal/comment-actions/ReactAction.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
+  import { FeatureFlag } from "$lib/features/feature-flag/models/FeatureFlag";
   import RenderFor from "$lib/guards/RenderFor.svelte";
+  import RenderForFeature from "$lib/guards/RenderForFeature.svelte";
   import type { MediaComment } from "$lib/requests/models/MediaComment";
+  import LikeCommentAction from "./LikeCommentAction.svelte";
   import ReactionPopupMenu from "./ReactionPopupMenu.svelte";
   import ReactionsSummary from "./ReactionsSummary.svelte";
 
   const { comment }: { comment: MediaComment } = $props();
 </script>
 
-<RenderFor audience="public">
-  <ReactionsSummary {comment} />
-</RenderFor>
+<RenderForFeature flag={FeatureFlag.Reactions}>
+  {#snippet enabled()}
+    <RenderFor audience="public">
+      <ReactionsSummary {comment} />
+    </RenderFor>
 
-<RenderFor audience="authenticated">
-  <ReactionPopupMenu {comment} />
-</RenderFor>
+    <RenderFor audience="authenticated">
+      <ReactionPopupMenu {comment} />
+    </RenderFor>
+  {/snippet}
+
+  <LikeCommentAction {comment} />
+</RenderForFeature>
