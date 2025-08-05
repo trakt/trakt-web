314faa8b3adf343afe5acb19f3e49c9a155c1d43
seferturan <seferturan85@gmail.com>
2025-08-05 19:36:06 +0200

    fix(landing): responsiveness on smaller screens

--- projects/client/src/lib/sections/landing/Landing.svelte
+++ projects/client/src/lib/sections/landing/Landing.svelte
@@ -16,7 +16,9 @@
   </RenderFor>
 </div>
 
-<style>
+<style lang="scss">
+  @use "$style/scss/mixins/index" as *;
+
   .trakt-landing-container {
     width: calc(
       100dvw - 2 * var(--layout-distance-side) - var(--layout-sidebar-distance)
@@ -28,5 +30,16 @@
 
     display: grid;
     grid-template-columns: auto 1fr;
+
+    transition: padding var(--transition-increment) ease-in-out;
+
+    @include for-tablet-lg-and-below {
+      padding: 0;
+    }
+
+    @include for-mobile {
+      grid-template-columns: 1fr;
+      justify-items: center;
+    }
   }
 </style>
