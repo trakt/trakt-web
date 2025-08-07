8fb089172da468b02565c3a11bf1d1e46a53ba43
seferturan <seferturan85@gmail.com>
2025-08-07 19:20:14 +0200

    feat(upcoming): add support for a coalesced upcoming list

---
+++ projects/client/src/lib/sections/lists/stores/useUpcomingItems.ts
@@ -0,0 +1,77 @@
+import { useQuery } from '$lib/features/query/useQuery.ts';
+import type { MediaEntry } from '$lib/requests/models/MediaEntry.ts';
+import {
+  type UpcomingEpisodeEntry,
+  upcomingEpisodesQuery,
+} from '$lib/requests/queries/calendars/upcomingEpisodesQuery.ts';
+import { upcomingMoviesQuery } from '$lib/requests/queries/calendars/upcomingMoviesQuery.ts';
+import { assertDefined } from '$lib/utils/assert/assertDefined.ts';
+import { toLoadingState } from '$lib/utils/requests/toLoadingState.ts';
+import { type CreateQueryOptions } from '@tanstack/svelte-query';
+import { derived } from 'svelte/store';
+
+type Mode = 'episodes' | 'all';
+type UpcomingList = Array<MediaEntry | UpcomingEpisodeEntry>;
+
+const ONE_DAY = 1000 * 60 * 60 * 24;
+const DAYS_TO_FETCH = 14;
+
+function daysAgo(days: number) {
+  return new Date(Date.now() - ONE_DAY * days);
+}
+
+function modeToQueries(startDate: string, mode: Mode) {
+  const params = {
+    startDate,
+    days: DAYS_TO_FETCH,
+  };
+
+  switch (mode) {
+    case 'all':
+      return [
+        upcomingEpisodesQuery(params) as CreateQueryOptions<UpcomingList>,
+        upcomingMoviesQuery(params) as CreateQueryOptions<UpcomingList>,
+      ];
+    case 'episodes':
+      return [
+        upcomingEpisodesQuery(params) as CreateQueryOptions<UpcomingList>,
+      ];
+  }
+}
+
+export function useUpcomingItems(mode: Mode = 'episodes') {
+  const [YYYY_MM_DD] = daysAgo(0).toISOString().split('T');
+  const startDate = assertDefined(
+    YYYY_MM_DD,
+    'Could not extract current date.',
+  );
+
+  const queries = modeToQueries(startDate, mode)
+    .map((query) => useQuery(query));
+
+  const isLoading = derived(
+    queries,
+    ($queries) => $queries.some(toLoadingState),
+  );
+
+  const allItems = derived(
+    queries,
+    ($queries) =>
+      $queries
+        .flatMap((query) => query.data ?? [])
+        .sort((a, b) => {
+          return new Date(a.airDate).getTime() - new Date(b.airDate).getTime();
+        }),
+  );
+
+  const upcoming = derived(
+    allItems,
+    ($allItems) =>
+      $allItems.filter((d) => {
+        const distanceFromNow = d.airDate.getTime() - Date.now();
+        return distanceFromNow > 0;
+      }),
+  );
+
+  return { upcoming, isLoading };
+}
