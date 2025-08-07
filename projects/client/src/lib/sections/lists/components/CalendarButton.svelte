395336f6f8188e48aa1e6baa2e43037f32380c82
seferturan <seferturan85@gmail.com>
2025-08-07 19:21:13 +0200

    feat(upcoming): use feature flagged coalesced list with calendar button

---
+++ projects/client/src/lib/sections/lists/components/CalendarButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import ActionButton from "$lib/components/buttons/ActionButton.svelte";
+  import CalendarIcon from "$lib/components/icons/CalendarIcon.svelte";
+  import * as m from "$lib/features/i18n/messages";
+  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+</script>
+
+<ActionButton
+  href={UrlBuilder.calendar()}
+  label={m.button_label_calendar()}
+  style="flat"
+  variant="secondary"
+  color="blue"
+>
+  <CalendarIcon />
+</ActionButton>
