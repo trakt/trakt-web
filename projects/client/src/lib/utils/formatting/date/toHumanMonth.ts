c5adf85926c6424520ec6df66d59d2ee79ec1176
seferturan <seferturan85@gmail.com>
2025-08-07 17:20:11 +0200

    feat(formatter): add support for short month formatting

--- projects/client/src/lib/utils/formatting/date/toHumanMonth.ts
+++ projects/client/src/lib/utils/formatting/date/toHumanMonth.ts
@@ -1,7 +1,11 @@
 import type { AvailableLanguage } from '$lib/features/i18n/index.ts';
 
-export function toHumanMonth(date: Date, locale: AvailableLanguage = 'en') {
+export function toHumanMonth(
+  date: Date,
+  locale: AvailableLanguage = 'en',
+  format: 'short' | 'long' = 'long',
+) {
   return date.toLocaleString(locale, {
-    month: 'long',
+    month: format,
   });
 }
