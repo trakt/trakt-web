d4f7b71bc9c1a2d40cf82e0dcdb443d21a8aebbd
seferturan <seferturan85@gmail.com>
2025-08-12 19:39:54 +0200

    chore: remove genre sorting on watchlist

--- projects/client/src/lib/sections/lists/watchlist/useReleasedList.ts
+++ projects/client/src/lib/sections/lists/watchlist/useReleasedList.ts
@@ -1,32 +1,21 @@
-import { useUser } from '$lib/features/auth/stores/useUser.ts';
-import { derived, get } from 'svelte/store';
+import { derived } from 'svelte/store';
 import { useWatchList, type WatchListStoreProps } from './useWatchList.ts';
-import { genreCompareFactory } from './utils/genreCompareFactory.ts';
 
 // FIXME remove when sorting is fixed
 const RELEASED_LIST_LIMIT = 500;
 
 export function useReleasedList(params: Omit<WatchListStoreProps, 'sort'>) {
-  const { user } = useUser();
-
   const { list: watchlist, isLoading, page } = useWatchList({
     ...params,
     sort: 'released',
     limit: RELEASED_LIST_LIMIT,
   });
 
-  const { compare } = genreCompareFactory(
-    get(user)?.genres ?? [],
-    'desc',
-    'genre',
-  );
-
   const list = derived(
     watchlist,
     ($watchlist) =>
       $watchlist
-        .filter((item) => item?.airDate.getTime() <= Date.now())
-        .sort(compare),
+        .filter((item) => item?.airDate.getTime() <= Date.now()),
   );
 
   return {
