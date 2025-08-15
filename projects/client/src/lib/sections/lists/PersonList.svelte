06b6ab5f8bb1e21a326ff7cef372281b7e46ee84
seferturan <seferturan85@gmail.com>
2025-08-15 21:39:47 +0200

    feat(search): display results in a grid

--- projects/client/src/lib/sections/lists/PersonList.svelte
+++ projects/client/src/lib/sections/lists/PersonList.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
-  import CardCover from "$lib/components/card/CardCover.svelte";
-  import CardFooter from "$lib/components/card/CardFooter.svelte";
-  import Link from "$lib/components/link/Link.svelte";
   import SectionList from "$lib/components/lists/section-list/SectionList.svelte";
-  import PersonCard from "$lib/components/people/card/PersonCard.svelte";
-  import * as m from "$lib/features/i18n/messages";
   import type { PersonSummary } from "$lib/requests/models/PersonSummary";
-  import { toTranslatedValue } from "$lib/utils/formatting/string/toTranslatedValue";
-  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import DefaultPersonItem from "./components/DefaultPersonItem.svelte";
 
   type CastListProps = {
     title: string;
@@ -24,24 +18,6 @@
   --height-list="var(--height-person-list)"
 >
   {#snippet item(person)}
-    <PersonCard>
-      <Link focusable={false} href={UrlBuilder.people(person.slug)}>
-        <CardCover
-          title={person.name}
-          src={person.headshot.url.thumb}
-          alt={`${m.image_alt_person_headshot({ person: person.name })}`}
-        />
-        <CardFooter>
-          <p class="trakt-card-title ellipsis">
-            {person.name}
-          </p>
-          {#if person.knownFor}
-            <p class="trakt-card-subtitle ellipsis">
-              {toTranslatedValue("position", person.knownFor)}
-            </p>
-          {/if}
-        </CardFooter>
-      </Link>
-    </PersonCard>
+    <DefaultPersonItem {person} />
   {/snippet}
 </SectionList>
