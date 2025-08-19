db36dec4bb358eac177b3b686edd95385d2fe833
seferturan <seferturan85@gmail.com>
2025-08-19 08:29:42 +0200

    fix(upcoming): sort upcoming movies

--- projects/client/src/lib/sections/lists/watchlist/useUnreleasedList.ts
+++ projects/client/src/lib/sections/lists/watchlist/useUnreleasedList.ts
@@ -3,28 +3,23 @@ import {
   useWatchList,
   type WatchListStoreProps,
 } from '$lib/sections/lists/watchlist/useWatchList.ts';
+import { isMaxDate } from '$lib/utils/date/isMaxDate.ts';
 import { derived } from 'svelte/store';
 
 // FIXME remove when sorting is fixed
 const UNRELEASED_LIST_LIMIT = 500;
 
-/*
-  For unreleased lists, we consider 'released' also.
-  Depending on the region, an item may be released in general,
-  but not yet in a particular region.
-*/
 const VALID_STATUSES: MediaStatus[] = [
   'planned',
   'post production',
   'in production',
   'upcoming',
-  'released',
 ] as const;
 
 export function useUnreleasedList(params: Omit<WatchListStoreProps, 'sort'>) {
   const { list: watchlist, isLoading, page } = useWatchList({
     ...params,
-    sort: 'rank',
+    sort: 'released',
     limit: UNRELEASED_LIST_LIMIT,
   });
 
@@ -34,10 +29,21 @@ export function useUnreleasedList(params: Omit<WatchListStoreProps, 'sort'>) {
       $watchlist
         .filter((item) => {
           const isUpcomingItem = item.airDate.getTime() > Date.now();
-          const hasValidStatus = VALID_STATUSES.includes(item.status);
+          /*
+            For unreleased lists, we consider 'released' also.
+            Depending on the region, an item may be released in general,
+            but not yet in a particular region.
+          */
+          const isUpcomingReleasedItem = item.status === 'released' &&
+            !isMaxDate(item.airDate);
+
+          const hasValidStatus = VALID_STATUSES.includes(item.status) ||
+            isUpcomingReleasedItem;
 
           return isUpcomingItem && hasValidStatus;
-        }),
+        })
+        // FIXME: add sorting/filter support to api, or switch to calendar api
+        .sort((a, b) => a.airDate.getTime() - b.airDate.getTime()),
   );
 
   return {
