5626e017c08318482d28ae59484035d5db23ac35
seferturan <seferturan85@gmail.com>
2025-08-28 21:33:28 +0200

    chore: use tooltip component

--- projects/client/src/lib/components/badge/Preview.svelte
+++ projects/client/src/lib/components/badge/Preview.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
   import * as m from "$lib/features/i18n/messages.ts";
-  import { useMedia, WellKnownMediaQuery } from "$lib/stores/css/useMedia";
-  import { Tooltip } from "@svelte-plugins/tooltips";
   import { onMount } from "svelte";
   import { writable } from "svelte/store";
   import StemTag from "../tags/StemTag.svelte";
-
-  const isMouse = useMedia(WellKnownMediaQuery.mouse);
-  const tooltipAction = $derived(isMouse ? "hover" : "click");
+  import Tooltip from "../tooltip/Tooltip.svelte";
 
   const isMounted = writable(false);
 
@@ -17,13 +13,7 @@
 </script>
 
 {#if $isMounted}
-  <Tooltip
-    content={m.tooltip_text_preview_feature()}
-    theme="preview-tooltip"
-    hideOnClickOutside={true}
-    action={tooltipAction}
-    maxWidth={300}
-  >
+  <Tooltip content={m.tooltip_text_preview_feature()}>
     <div class="trakt-preview-badge">
       <StemTag
         --color-background-stem-tag={"var(--color-background-preview-tag)"}
@@ -37,23 +27,5 @@
 <style>
   .trakt-preview-badge {
     user-select: none;
-  }
-
-  :global(.preview-tooltip) {
-    --tooltip-z-index: var(--layer-overlay);
-
-    --tooltip-background-color: var(--color-tooltip-background);
-    --tooltip-color: var(--color-tooltip-text);
-
-    --tooltip-border-radius: var(--border-radius-s);
-    --tooltip-padding: var(--ni-8);
-
-    --tooltip-font-size: var(--ni-12);
-    --tooltip-font-weight: 400;
-    --tooltip-font-family: initial;
-
-    &:not(.show) {
-      display: none;
-    }
   }
 </style>
