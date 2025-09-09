52e676dfb25dc8c891b595ad620b90eaa3e55de0
seferturan <seferturan85@gmail.com>
2025-09-09 13:42:51 +0200

    feat(tags): add support for icons in info tags

--- projects/client/src/lib/components/media/tags/InfoTag.svelte
+++ projects/client/src/lib/components/media/tags/InfoTag.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import StemTag from "$lib/components/tags/StemTag.svelte";
+  import type { Snippet } from "svelte";
 
-  const { children }: ChildrenProps = $props();
+  const { children, icon }: { icon?: Snippet } & ChildrenProps = $props();
 </script>
 
-<StemTag>
+<StemTag {icon}>
   <p class="meta-info capitalize no-wrap">
     {@render children()}
   </p>
