83fe2248007c0a8add52bc1b8c5ea750a131aee6
seferturan <seferturan85@gmail.com>
2025-09-09 09:49:09 +0200

    feat(list): display skeleton list when loading media list items

---
+++ projects/client/src/lib/components/lists/SkeletonList.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import SkeletonCard from "$lib/sections/lists/components/SkeletonCard.svelte";
+  import { DEFAULT_PAGE_SIZE } from "$lib/utils/constants";
+
+  const {
+    id,
+    variant = "portrait",
+  }: { id: string; variant?: "portrait" | "landscape" } = $props();
+
+  const randomListIndex = Math.floor(Math.random() * 6) + 1;
+</script>
+
+<div class="trakt-skeleton-list trakt-list-item-container">
+  {#each Array(DEFAULT_PAGE_SIZE) as _, index (`skeleton_${id}_${index}`)}
+    <SkeletonCard {variant} {index} listIndex={randomListIndex} />
+  {/each}
+</div>
+
+<style>
+  .trakt-skeleton-list {
+    overflow: hidden;
+  }
+</style>
