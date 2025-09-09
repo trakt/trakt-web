908bd6358153ad3879bafbc04054291be2bd4fd1
seferturan <seferturan85@gmail.com>
2025-09-09 13:43:36 +0200

    feat(search): show media icon in media results

--- projects/client/src/routes/search/+page.svelte
+++ projects/client/src/routes/search/+page.svelte
@@ -3,12 +3,17 @@
 
   import { page } from "$app/state";
   import CoverImageSetter from "$lib/components/background/CoverImageSetter.svelte";
+  import MovieIcon from "$lib/components/icons/MovieIcon.svelte";
+  import ShowIcon from "$lib/components/icons/ShowIcon.svelte";
   import GridList from "$lib/components/lists/grid-list/GridList.svelte";
+  import AirDateTag from "$lib/components/media/tags/AirDateTag.svelte";
   import InfoTag from "$lib/components/media/tags/InfoTag.svelte";
+  import { TagIntlProvider } from "$lib/components/media/tags/TagIntlProvider";
   import SearchInput from "$lib/features/search/SearchInput.svelte";
   import SearchModeToggles from "$lib/features/search/SearchModeToggles.svelte";
   import { useSearch } from "$lib/features/search/useSearch";
   import RenderFor from "$lib/guards/RenderFor.svelte";
+  import { type MediaEntry } from "$lib/requests/models/MediaEntry";
   import TraktPage from "$lib/sections/layout/TraktPage.svelte";
   import TraktPageCoverSetter from "$lib/sections/layout/TraktPageCoverSetter.svelte";
   import DefaultMediaItem from "$lib/sections/lists/components/DefaultMediaItem.svelte";
@@ -52,10 +57,21 @@
   const showOnPageSearch = isMobileAppleDevice();
 </script>
 
-{#snippet showTag()}
+{#snippet mediaTag(item: MediaEntry)}
   <InfoTag>
-    {toTranslatedValue("type", "show")}
+    {#snippet icon()}
+      {#if item.type === "movie"}
+        <MovieIcon />
+      {/if}
+
+      {#if item.type === "show"}
+        <ShowIcon />
+      {/if}
+    {/snippet}
+    {toTranslatedValue("type", item.type)}
   </InfoTag>
+
+  <AirDateTag i18n={TagIntlProvider} airDate={item.airDate} />
 {/snippet}
 
 {#snippet searchControls()}
@@ -89,11 +105,15 @@
           --width-item="var(--width-portrait-card)"
         >
           {#snippet item(result)}
+            {#snippet mediaResultTag()}
+              {@render mediaTag(result)}
+            {/snippet}
+
             <DefaultMediaItem
               type={result.type}
               media={result}
               style="cover"
-              tag={result.type === "show" ? showTag : undefined}
+              tag={$mode === "media" ? mediaResultTag : undefined}
             />
           {/snippet}
         </GridList>
