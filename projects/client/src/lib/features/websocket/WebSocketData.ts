281bb3074ff06dfbf89e3b0a8ba9c049c96aa4e1
Vlad Jerca <vlad.jerca@gmail.com>
2025-09-15 17:13:31 +0200

    feat(ws): expands websocket key types
    Adds support for episode and movie watched events to the WebSocket data handling.
    
    Updates the WSInvalidator component to invalidate relevant cache keys when these new events are received.\n\n
    
    Includes `invalidate:check_in` and `invalidate:mark_as_watched` invalidations for 'episode:watched' and 'movie:watched' events.  (FIXME: further specific check-in invalidation)\n

--- projects/client/src/lib/features/websocket/WebSocketData.ts
+++ projects/client/src/lib/features/websocket/WebSocketData.ts
@@ -1,4 +1,9 @@
-type WebSocketKey = 'show:watchlist' | 'movie:watchlist';
+type WebSocketKey =
+  | 'show:watchlist'
+  | 'movie:watchlist'
+  | 'episode:watched'
+  | 'movie:watched';
+
 export type WebSocketData = {
   key: WebSocketKey;
 };
