92d2c077c05d32a38eb250cd20c177f73775523c
Vlad Jerca <vlad.jerca@gmail.com>
2025-09-15 16:49:32 +0200

    feat(ws): add invalidation for watchlist updates
    Adds functionality to invalidate data in the client when watchlist data changes are received over the websocket.
    
    Creates a new WebSocketData type and a new Svelte component that handles incoming messages from the websocket.
    The component subscribes to the authentication token and creates or destroys the websocket connection accordingly.
    When a message is received, it parses the data and invalidates specific caches based on the message key.

--- projects/client/src/lib/features/websocket/WSInvalidator.svelte
+++ projects/client/src/lib/features/websocket/WSInvalidator.svelte
@@ -1,15 +1,42 @@
 <script lang="ts">
   import { getAuthContext } from "$lib/features/auth/stores/getAuthContext.ts";
+  import { useInvalidator } from "$lib/stores/useInvalidator";
+  import { LogLevel, print } from "$lib/utils/console/print";
+  import { setMarker } from "$lib/utils/date/Marker";
   import { onDestroy } from "svelte";
   import { createConnection } from "./createConnection.ts";
   import { destroySocket } from "./destroySocket.ts";
+  import type { WebSocketData } from "./WebSocketData.ts";
 
   const { token } = getAuthContext();
+  const { invalidate } = useInvalidator();
 
   let socket = $state<WebSocket | Nil>(null);
 
+  function wsInvalidate(event: MessageEvent) {
+    try {
+      const data: WebSocketData = JSON.parse(event.data);
+      setMarker();
+
+      switch (data.key) {
+        case "show:watchlist":
+          invalidate("invalidate:watchlisted:show");
+        case "movie:watchlist":
+          invalidate("invalidate:watchlisted:movie");
+          break;
+        default:
+          print(LogLevel.Log, "warn", "WS Warning: Unknown key", data.key);
+      }
+    } catch (e) {
+      print(LogLevel.Log, "error", "WS Error: Invalid JSON", e);
+      return;
+    }
+  }
+
   token.subscribe(($token) => {
+    socket?.removeEventListener("message", wsInvalidate);
     socket = createConnection(socket, $token?.value);
+    socket?.addEventListener("message", wsInvalidate);
   });
 
   onDestroy(() => destroySocket(socket));
