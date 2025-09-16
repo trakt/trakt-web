adc73bac48c1a62786908b6795f557b19c3a7be3
seferturan <seferturan85@gmail.com>
2025-09-16 20:57:05 +0200

    chore: redirect from season to show

---
+++ projects/client/src/routes/shows/[slug]/seasons/[season]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Redirect from "$lib/components/router/Redirect.svelte";
+  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import type { PageProps } from "./$types";
+
+  const { params }: PageProps = $props();
+</script>
+
+<Redirect to={UrlBuilder.show(params.slug, { season: params.season })} />
