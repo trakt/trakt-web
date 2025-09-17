302adc4599cad98bbf4dbc07c7836d268b3d8842
seferturan <seferturan85@gmail.com>
2025-09-17 12:27:12 +0200

    refactor(summary): extract generic summary component

---
+++ projects/client/src/lib/sections/summary/components/_internal/SummarySideActions.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  const { children }: ChildrenProps = $props();
+</script>
+
+<div class="trakt-summary-side-actions">
+  {@render children()}
+</div>
+
+<style>
+  .trakt-summary-side-actions {
+    display: flex;
+    flex-direction: column;
+
+    justify-content: flex-start;
+    align-items: flex-start;
+
+    gap: var(--gap-s);
+
+    :global(.trakt-action-button) {
+      background: transparent;
+      backdrop-filter: none;
+
+      :global(svg) {
+        color: var(--color-foreground);
+      }
+    }
+  }
+</style>
