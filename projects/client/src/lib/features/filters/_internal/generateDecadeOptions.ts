8944a053ffff6103eab206ccc1b1b053e4aa177d
seferturan <seferturan85@gmail.com>
2025-09-19 10:49:32 +0200

    feat(filter): add 'this year'

--- projects/client/src/lib/features/filters/_internal/generateDecadeOptions.ts
+++ projects/client/src/lib/features/filters/_internal/generateDecadeOptions.ts
@@ -1,4 +1,5 @@
 import type { FilterOption } from '$lib/features/filters/models/Filter.ts';
+import * as m from '$lib/features/i18n/messages.ts';
 
 const START_YEAR = 1960;
 const DECADE_YEARS = 10;
@@ -7,7 +8,7 @@ export function generateDecadeOptions(): FilterOption[] {
   const currentYear = new Date().getFullYear();
   const numDecades = Math.floor((currentYear - START_YEAR) / DECADE_YEARS) + 1;
 
-  return Array.from({ length: numDecades }, (_, index) => {
+  const decades = Array.from({ length: numDecades }, (_, index) => {
     const decadeStart = START_YEAR + index * DECADE_YEARS;
     const decadeEnd = decadeStart + DECADE_YEARS - 1;
 
@@ -16,4 +17,12 @@ export function generateDecadeOptions(): FilterOption[] {
       value: `${decadeStart}-${decadeEnd}`,
     };
   }).reverse();
+
+  return [
+    {
+      label: m.filter_label_this_year(),
+      value: `${currentYear}`,
+    },
+    ...decades,
+  ];
 }
