ff13631d81135c7a51233e4d0f637f13a080f4df
seferturan <seferturan85@gmail.com>
2025-09-22 13:16:41 +0200

    chore(analytics): record user data for page views

--- projects/client/src/lib/features/analytics/PageView.svelte
+++ projects/client/src/lib/features/analytics/PageView.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
   import { page } from "$app/state";
+  import { useUser } from "$lib/features/auth/stores/useUser";
+  import { getUserAnalyticsData } from "./_internal/getUserAnalyticsData";
   import AnalyticsLoader from "./_internal/loader/AnalyticsLoader.svelte";
   import { useAnalytics } from "./useAnalytics";
 
   const eventName = "page_view";
 
   const { setUser, record } = useAnalytics();
+  const { user } = useUser();
 
   const recordPageView = async (userId: string | Nil) => {
     setUser(userId);
     record(eventName, {
       page_location: page.url.href,
       page_path: page.url.pathname,
+      ...getUserAnalyticsData($user),
     });
   };
 </script>
