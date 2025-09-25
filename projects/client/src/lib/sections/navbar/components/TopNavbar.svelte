2077e2ebeffd883587b2cb7687ca365d39b9019b
seferturan <seferturan85@gmail.com>
2025-09-25 09:44:11 +0200

    feat(navbar): hide top navbar on summary pages

--- projects/client/src/lib/sections/navbar/components/TopNavbar.svelte
+++ projects/client/src/lib/sections/navbar/components/TopNavbar.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
+  import { page } from "$app/state";
   import PlusIcon from "$lib/components/icons/PlusIcon.svelte";
   import RenderFor from "$lib/guards/RenderFor.svelte";
   import { trackWindowScroll } from "$lib/utils/actions/trackWindowScroll";
   import { trackWindowScrollDirection } from "$lib/utils/actions/trackWindowScrollDirection";
+  import { HIDDEN_ROUTE_IDS } from "./_internal/constants";
   import Greeting from "./_internal/Greeting.svelte";
   import FilterButton from "./filter/FilterButton.svelte";
   import JoinTraktButton from "./JoinTraktButton.svelte";
   import ProfileLink from "./ProfileLink.svelte";
+
+  const isHidden = $derived(HIDDEN_ROUTE_IDS.includes(page.route.id ?? ""));
 </script>
 
 <header>
   <nav
     class="trakt-navbar"
+    class:is-hidden={isHidden}
     use:trackWindowScroll={"trakt-navbar-scroll"}
     use:trackWindowScrollDirection={{
       upClassName: "trakt-navbar-scroll-up",
@@ -38,7 +43,7 @@
     </div>
   </nav>
 
-  <div class="trakt-navbar-spacer"></div>
+  <div class="trakt-navbar-spacer" class:is-hidden={isHidden}></div>
 </header>
 
 <style lang="scss">
@@ -50,6 +55,12 @@
 
     margin-top: env(safe-area-inset-top);
     margin-bottom: var(--ni-12);
+
+    transition: height calc(2 * var(--transition-increment)) ease-in-out;
+
+    &.is-hidden {
+      height: calc(var(--gap-m) + env(safe-area-inset-top));
+    }
   }
 
   .trakt-navbar {
@@ -81,7 +92,8 @@
     border-bottom-right-radius: var(--border-radius-xxl);
 
     transition: calc(2 * var(--transition-increment)) ease-in-out;
-    transition-property: width, background-color, box-shadow, top, opacity;
+    transition-property:
+      height, width, background-color, box-shadow, top, opacity;
 
     .trakt-navbar-links {
       display: flex;
@@ -107,13 +119,19 @@
       opacity: 1;
     }
 
+    &.is-hidden {
+      height: 0;
+      opacity: 0;
+      pointer-events: none;
+    }
+
     @include for-mobile {
       gap: var(--gap-xs);
     }
   }
 
   :global([data-mobile-os="ios"]) {
-    .trakt-navbar {
+    .trakt-navbar:not(.is-hidden) {
       margin-top: 0;
       padding-top: calc(
         var(--navbar-vertical-padding) + env(safe-area-inset-top, 0)
@@ -121,7 +139,7 @@
       height: calc(var(--navbar-height) + env(safe-area-inset-top, 0));
     }
 
-    .trakt-navbar-spacer {
+    .trakt-navbar-spacer:not(.is-hidden) {
       height: calc(var(--navbar-height) + env(safe-area-inset-top, 0));
       margin-top: 0;
     }
---
+++ projects/client/src/lib/sections/navbar/components/_internal/constants.ts
@@ -0,0 +1,7 @@
+// FIXME: replace with context when adding navbar context provider
+export const HIDDEN_ROUTE_IDS = [
+  '/movies/[slug]',
+  '/shows/[slug]',
+  '/shows/[slug]/seasons/[season]/episodes/[episode]',
+  '/people/[slug]',
+];
