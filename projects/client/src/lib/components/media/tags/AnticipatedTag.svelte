16af8a99b6e946916e0070acd7d5719e98913ce6
seferturan <seferturan85@gmail.com>
2025-10-07 15:38:18 +0200

    fix(card): do not hide external tags on summary card

--- projects/client/src/lib/components/media/tags/AnticipatedTag.svelte
+++ projects/client/src/lib/components/media/tags/AnticipatedTag.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
   import AnticipatedIcon from "$lib/components/icons/AnticipatedIcon.svelte";
   import StemTag from "$lib/components/tags/StemTag.svelte";
+  import TextTag from "$lib/components/tags/TextTag.svelte";
   import type { TagIntl } from "./TagIntl";
 
   const {
     score,
     i18n,
+    isTextOnly = false,
   }: {
     score: number;
     i18n: TagIntl;
+    isTextOnly?: boolean;
   } = $props();
 </script>
 
-<StemTag>
-  {#snippet icon()}
-    <AnticipatedIcon />
-  {/snippet}
+{#snippet icon()}
+  <AnticipatedIcon />
+{/snippet}
+
+{#snippet content()}
   <p class="meta-info capitalize no-wrap">
     {i18n.toAnticipatedCount(score)}
   </p>
-</StemTag>
+{/snippet}
+
+{#if isTextOnly}
+  <TextTag {icon}>
+    {@render content()}
+  </TextTag>
+{:else}
+  <StemTag {icon}>
+    {@render content()}
+  </StemTag>
+{/if}
