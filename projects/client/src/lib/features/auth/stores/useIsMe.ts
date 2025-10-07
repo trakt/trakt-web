7bdff694206792ba76d8b0c3080d4db4ea9a4975
seferturan <seferturan85@gmail.com>
2025-10-07 09:55:23 +0200

    fix: check actual authorized state

--- projects/client/src/lib/features/auth/stores/useIsMe.ts
+++ projects/client/src/lib/features/auth/stores/useIsMe.ts
@@ -1,21 +1,16 @@
-import { derived, readable } from 'svelte/store';
+import { derived } from 'svelte/store';
 import { useAuth } from './useAuth.ts';
 import { useUser } from './useUser.ts';
 
 export function useIsMe(slug: string) {
   const { isAuthorized } = useAuth();
-
-  if (!isAuthorized) {
-    return {
-      isMe: readable(false),
-    };
-  }
-
   const { user } = useUser();
+
   return {
     isMe: derived(
-      user,
-      ($user) => slug === 'me' || slug === $user.slug,
+      [user, isAuthorized],
+      ([$user, $isAuthorized]) =>
+        $isAuthorized && (slug === 'me' || slug === $user.slug),
     ),
   };
 }
