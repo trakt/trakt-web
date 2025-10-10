bbf14807327179ec1f197cf797ffed495ca336de
seferturan <seferturan85@gmail.com>
2025-10-10 09:29:38 +0200

    refactor: post theme in useTheme

--- projects/client/src/lib/features/theme/useTheme.ts
+++ projects/client/src/lib/features/theme/useTheme.ts
@@ -5,18 +5,27 @@ import { computeVariable } from '$lib/stores/css/computeVariable.ts';
 import { getContext } from 'svelte';
 import { derived, type Writable, writable } from 'svelte/store';
 import { THEME_COOKIE_NAME } from './constants.ts';
+import type { ThemeResponse } from './handle.ts';
 import { Theme } from './models/Theme.ts';
+import { ThemeEndpoint } from './ThemeEndpoint.ts';
 
 export function useTheme() {
   const theme: Writable<Theme> = getContext(THEME_COOKIE_NAME);
   const { track } = useTrack(AnalyticsEvent.Theme);
   const systemTheme = writable();
 
-  function set(value: Theme) {
+  async function set(value: Theme) {
     globalThis.document.documentElement.dataset.theme = value;
 
     track({ theme: value });
     theme.set(value);
+    await fetch(ThemeEndpoint.Set, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ theme: value }),
+    }).then((res) => res.json() as Promise<ThemeResponse>);
   }
 
   if (browser) {
