fea3847e63d2cf1e0798f22f8810fe9b1787cdf6
seferturan <seferturan85@gmail.com>
2025-10-10 09:28:44 +0200

    chore: do not prefetch user settings

--- projects/client/src/routes/+layout.ts
+++ projects/client/src/routes/+layout.ts
@@ -1,16 +1,11 @@
 import type { LayoutLoad } from './$types.ts';
 
 import { browser } from '$app/environment';
-import { currentUserSettingsQuery } from '$lib/features/auth/queries/currentUserSettingsQuery.ts';
 import { setToken } from '$lib/features/auth/token/index.ts';
 import { retryDelay } from '$lib/utils/retry/retryDelay.ts';
 import { QueryClient } from '@tanstack/svelte-query';
 
-function isExpired(expiresAt: number | Nil): boolean {
-  return expiresAt ? new Date(expiresAt).getTime() < Date.now() : true;
-}
-
-export const load: LayoutLoad = async ({ data, fetch }) => {
+export const load: LayoutLoad = ({ data }) => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -24,11 +19,6 @@ export const load: LayoutLoad = async ({ data, fetch }) => {
 
   const auth = data.auth.isAuthorized ? data.auth : data.oidcAuth;
   setToken({ value: auth.token, expiresAt: auth.expiresAt });
-
-  const canPrefetch = data.auth.isAuthorized || !isExpired(auth.expiresAt);
-  if (canPrefetch) {
-    await queryClient.prefetchQuery(currentUserSettingsQuery({ fetch }));
-  }
 
   return { queryClient, ...data };
 };
