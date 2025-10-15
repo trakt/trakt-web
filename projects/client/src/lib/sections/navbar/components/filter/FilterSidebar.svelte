b568591fcf07a745bf6c4627b645d5ed716741f9
seferturan <seferturan85@gmail.com>
2025-10-15 12:57:29 +0200

    feat: replace sidebar with drawer component

--- projects/client/src/lib/sections/navbar/components/filter/FilterSidebar.svelte
+++ projects/client/src/lib/sections/navbar/components/filter/FilterSidebar.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+  import Drawer from "$lib/components/drawer/Drawer.svelte";
   import { useFilter } from "$lib/features/filters/useFilter";
   import * as m from "$lib/features/i18n/messages.ts";
   import { DpadNavigationType } from "$lib/features/navigation/models/DpadNavigationType";
   import ResetAllButton from "./_internal/ResetAllButton.svelte";
   import SaveFiltersButton from "./_internal/SaveFiltersButton.svelte";
-  import Sidebar from "./_internal/Sidebar.svelte";
   import ListFilter from "./filters/ListFilter.svelte";
   import RatingsFilter from "./filters/RatingsFilter.svelte";
   import ToggleFilter from "./filters/ToggleFilter.svelte";
@@ -13,7 +13,7 @@
   const { filters } = useFilter();
 </script>
 
-<Sidebar {onClose} title={m.header_filters()}>
+<Drawer {onClose} title={m.header_filters()} trapSelector=".trakt-filter">
   {#each $filters as filter}
     {#if filter.type === "list"}
       <ListFilter {filter} />
@@ -35,7 +35,7 @@
     <SaveFiltersButton onSave={onClose} />
     <ResetAllButton />
   </div>
-</Sidebar>
+</Drawer>
 
 <style>
   .trakt-filter-actions {
