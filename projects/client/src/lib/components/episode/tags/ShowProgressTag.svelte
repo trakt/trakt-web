7521c91c0891937ade496d026561c173244d12f3
seferturan <seferturan85@gmail.com>
2025-10-16 09:34:21 +0200

    refactor(tags): extract progress tag

--- projects/client/src/lib/components/episode/tags/ShowProgressTag.svelte
+++ projects/client/src/lib/components/episode/tags/ShowProgressTag.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import TagContent from "$lib/components/tags/TagContent.svelte";
+  import ProgressTag from "$lib/components/media/tags/ProgressTag.svelte";
   import { stretchedPercentage } from "$lib/utils/number/stretchedPercentage";
 
   type ShowProgressTagProps = {
@@ -11,58 +11,6 @@
   const percentage = $derived(stretchedPercentage({ value: progress, total }));
 </script>
 
-<div
-  class="show-progress-tag"
-  style:--progress-width={`${percentage}%`}
-  role="progressbar"
-  aria-valuenow={progress}
-  aria-valuemin={0}
-  aria-valuemax={total}
->
-  <TagContent>
-    <p class="meta-info capitalize tag-label">
-      {@render children()}
-    </p>
-  </TagContent>
-</div>
-
-<style lang="scss">
-  @use "$style/scss/mixins/index.scss" as *;
-
-  .tag-label {
-    width: 100%;
-  }
-
-  .show-progress-tag {
-    width: 100%;
-    min-width: 0;
-
-    :global(.trakt-tag) {
-      --progress-bar-spacing: var(--ni-6);
-      --progress-bar-offset: calc(var(--progress-bar-spacing) / 2);
-      overflow: hidden;
-      padding: var(--ni-4) var(--ni-12);
-
-      &::before {
-        content: "";
-        display: block;
-        position: absolute;
-        left: var(--progress-bar-offset);
-        top: var(--progress-bar-offset);
-        width: calc(100% - var(--progress-bar-spacing));
-        height: calc(100% - var(--progress-bar-spacing));
-
-        border-radius: inherit;
-
-        width: calc(var(--progress-width) - var(--progress-bar-offset));
-        background-color: var(--color-background-progress-tag);
-
-        transition: width var(--transition-increment) ease-in;
-      }
-
-      position: relative;
-      background: var(--color-background-cover-tag);
-      color: var(--color-text-progress-tag);
-    }
-  }
-</style>
+<ProgressTag progress={percentage} {total}>
+  {@render children()}
+</ProgressTag>
