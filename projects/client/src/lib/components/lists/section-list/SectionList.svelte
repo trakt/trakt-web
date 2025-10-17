a0c4f6ddf700702f8623298e2b5069e2ed115c4f
seferturan <seferturan85@gmail.com>
2025-10-17 16:24:44 +0200

    chore: remove list scroll buttons

--- projects/client/src/lib/components/lists/section-list/SectionList.svelte
+++ projects/client/src/lib/components/lists/section-list/SectionList.svelte
@@ -1,10 +1,5 @@
 <script lang="ts" generics="T extends { id: unknown }">
-  import ActionButton from "$lib/components/buttons/ActionButton.svelte";
-  import CaretLeftIcon from "$lib/components/icons/CaretLeftIcon.svelte";
-  import CaretRightIcon from "$lib/components/icons/CaretRightIcon.svelte";
-  import RenderFor from "$lib/guards/RenderFor.svelte";
   import { type Snippet } from "svelte";
-  import { writable } from "svelte/store";
   import type { ListProps } from "../ListProps";
   import ShadowList from "./ShadowList.svelte";
 
@@ -27,39 +22,6 @@
     badge,
     drilldownLink,
   }: SectionListProps<T> = $props();
-
-  const scrollContainer = writable<HTMLDivElement>();
-  const scrollX = writable({ left: 0, right: 0 });
-
-  function scrollToLeft() {
-    const left = Math.max(
-      0,
-      $scrollContainer.scrollLeft - window.innerWidth / 2,
-    );
-
-    $scrollContainer.scrollTo({
-      left,
-      behavior: "smooth",
-    });
-  }
-
-  function scrollToRight() {
-    const maxScrollLeft =
-      $scrollContainer.scrollWidth - $scrollContainer.clientWidth;
-
-    const left = Math.min(
-      maxScrollLeft,
-      $scrollContainer.scrollLeft + window.innerWidth / 2,
-    );
-
-    $scrollContainer.scrollTo({
-      left,
-      behavior: "smooth",
-    });
-  }
-
-  const isLeftScrollDisabled = $derived($scrollX.left <= 0);
-  const isRightScrollDisabled = $derived($scrollX.right <= 0);
 </script>
 
 <ShadowList
@@ -71,35 +33,12 @@
   {ctaItem}
   {empty}
   {badge}
-  {scrollX}
-  {scrollContainer}
   {drilldownLink}
 >
   {#snippet actions()}
     {#if dynamicActions != null}
       {@render dynamicActions()}
     {/if}
-
-    <RenderFor audience="all" device={["tablet-sm", "tablet-lg", "desktop"]}>
-      <ActionButton
-        onclick={scrollToLeft}
-        label={`Scroll ${title} to the left`}
-        color="default"
-        style="ghost"
-        disabled={isLeftScrollDisabled}
-      >
-        <CaretLeftIcon />
-      </ActionButton>
-      <ActionButton
-        onclick={scrollToRight}
-        label={`Scroll ${title} to the right`}
-        color="default"
-        style="ghost"
-        disabled={isRightScrollDisabled}
-      >
-        <CaretRightIcon />
-      </ActionButton>
-    </RenderFor>
 
     {#if externalActions != null}
       {@render externalActions()}
