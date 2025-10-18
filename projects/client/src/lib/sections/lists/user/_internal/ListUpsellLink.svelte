ecbc619b79657bb5c48be108f07134793e928833
seferturan <seferturan85@gmail.com>
2025-10-18 17:29:56 +0200

    refactor: extract upsell link component

--- projects/client/src/lib/sections/lists/user/_internal/ListUpsellLink.svelte
+++ projects/client/src/lib/sections/lists/user/_internal/ListUpsellLink.svelte
@@ -1,51 +1,8 @@
 <script lang="ts">
-  import VipUpsellBadge from "$lib/components/badge/VipUpsellBadge.svelte";
-  import Link from "$lib/components/link/Link.svelte";
   import * as m from "$lib/features/i18n/messages";
-  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import UpsellLink from "$lib/features/upsell/UpsellLink.svelte";
 </script>
 
-<trakt-list-upsell-link>
-  <Link href={UrlBuilder.vip()} label={m.link_label_get_vip()} color="inherit">
-    <VipUpsellBadge />
-    <span class="meta-info">{m.link_text_vip_list_upsell()}</span>
-  </Link>
-</trakt-list-upsell-link>
-
-<style lang="scss">
-  @use "$style/scss/mixins/index" as *;
-
-  trakt-list-upsell-link {
-    :global(.trakt-link) {
-      text-decoration: none;
-
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-
-      gap: var(--gap-xs);
-      padding: var(--ni-8);
-      padding-right: var(--ni-12);
-      box-sizing: border-box;
-
-      border-radius: var(--border-radius-xl);
-
-      background-color: var(--color-background-stem-tag);
-      color: var(--color-foreground-stem-tag);
-
-      transition: padding calc(var(--transition-increment) * 2) ease-in-out;
-
-      &:hover {
-        text-decoration: underline;
-      }
-
-      @include for-tablet-sm {
-        padding: 0;
-
-        :global(span) {
-          display: none;
-        }
-      }
-    }
-  }
-</style>
+<UpsellLink>
+  <span class="meta-info">{m.link_text_vip_list_upsell()}</span>
+</UpsellLink>
