fee7831a8bd200c10e59f0d355fe2dac025a3d49
seferturan <seferturan85@gmail.com>
2025-10-20 11:43:12 +0200

    feat(up-next): use up next intents in discover mode

--- projects/client/src/lib/sections/lists/progress/useUpNextList.ts
+++ projects/client/src/lib/sections/lists/progress/useUpNextList.ts
@@ -1,16 +1,19 @@
 import type { ExtendedMediaType } from '$lib/requests/models/ExtendedMediaType.ts';
 import type { Paginatable } from '$lib/requests/models/Paginatable.ts';
 import type { PaginationParams } from '$lib/requests/models/PaginationParams.ts';
+import type { UpNextIntentParams } from '$lib/requests/models/UpNextIntentParams.ts';
 import {
   type MovieProgressEntry,
   movieProgressQuery,
 } from '$lib/requests/queries/sync/movieProgressQuery.ts';
-import { upNextNitroQuery } from '$lib/requests/queries/sync/upNextNitroQuery.ts';
-import type { UpNextEntry } from '$lib/requests/queries/sync/upNextQuery.ts';
+import {
+  type UpNextEntry,
+  upNextNitroQuery,
+} from '$lib/requests/queries/sync/upNextNitroQuery.ts';
 import { usePaginatedListQuery } from '$lib/sections/lists/stores/usePaginatedListQuery.ts';
 import type { CreateQueryOptions } from '@tanstack/svelte-query';
 
-export type UpNextStoreProps = PaginationParams & {
+export type UpNextStoreProps = PaginationParams & UpNextIntentParams & {
   type: ExtendedMediaType;
 };
 
