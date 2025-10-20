5da92bae68643381bb4eb6c155c5aeec72f8cbcd
seferturan <seferturan85@gmail.com>
2025-10-20 11:42:30 +0200

    chore(query): remove upnextquery

--- projects/client/src/lib/requests/queries/sync/upNextNitroQuery.spec.ts
+++ projects/client/src/lib/requests/queries/sync/upNextNitroQuery.spec.ts
@@ -2,14 +2,14 @@ import { UpNextMappedMock } from '$mocks/data/sync/mapped/UpNextMappedMock.ts';
 import { runQuery } from '$test/beds/query/runQuery.ts';
 import { createQuery } from '@tanstack/svelte-query';
 import { describe, expect, it } from 'vitest';
-import { upNextQuery } from './upNextQuery.ts';
+import { upNextNitroQuery } from './upNextNitroQuery.ts';
 
-describe('upNextQuery', () => {
+describe('upNextNitroQuery', () => {
   it('should query up next', async () => {
     const result = await runQuery({
       factory: () =>
         createQuery(
-          upNextQuery(),
+          upNextNitroQuery(),
         ),
       mapper: (response) => response?.data?.entries,
     });
