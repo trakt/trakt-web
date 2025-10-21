f9e6cf4750888c76868a78ed36d3ec699440d816
seferturan <seferturan85@gmail.com>
2025-10-21 12:17:59 +0200

    feat(query): add support for getting trakt team members

---
+++ projects/client/src/lib/requests/queries/team/traktTeamQuery.ts
@@ -0,0 +1,26 @@
+import { defineQuery } from '$lib/features/query/defineQuery.ts';
+import { api, type ApiParams } from '$lib/requests/api.ts';
+import { time } from '$lib/utils/timing/time.ts';
+import { mapToUserProfile } from '../../_internal/mapToUserProfile.ts';
+import { UserProfileSchema } from '../../models/UserProfile.ts';
+
+const traktTeamRequest = (
+  { fetch }: ApiParams,
+) =>
+  api({ fetch })
+    .team
+    .members({
+      query: {
+        extended: 'full,images',
+      },
+    });
+
+export const traktTeamQuery = defineQuery({
+  key: 'traktTeam',
+  invalidations: [],
+  dependencies: [],
+  request: traktTeamRequest,
+  mapper: (response) => response.body.map(({ user }) => mapToUserProfile(user)),
+  schema: UserProfileSchema.array(),
+  ttl: time.days(30),
+});
