b31e7f80f6695239aed742a3f10e5521f94e7243
seferturan <seferturan85@gmail.com>
2025-10-22 14:11:49 +0200

    fix(tags): positioning in firefox

--- projects/client/src/lib/components/tags/IndicatorTags.svelte
+++ projects/client/src/lib/components/tags/IndicatorTags.svelte
@@ -2,18 +2,26 @@
   const { children }: ChildrenProps = $props();
 </script>
 
-<div class="trakt-indicator-tags">
-  {@render children()}
+<div class="trakt-indicator-tags-container">
+  <div class="trakt-indicator-tags">
+    {@render children()}
+  </div>
 </div>
 
 <style>
+  /* To make sure Firefox can position it properly */
+  .trakt-indicator-tags-container {
+    position: relative;
+  }
+
   .trakt-indicator-tags {
     z-index: var(--layer-raised);
 
     position: absolute;
     width: var(--width-override-card, var(--width-card, 100%));
 
-    bottom: var(--ni-neg-10);
+    bottom: 0;
+    transform: translateY(50%);
 
     display: inline-flex;
     justify-content: center;
