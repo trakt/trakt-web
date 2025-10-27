2c78abb7d97fa186c2565552ee6d3fb94b3f0579
seferturan <seferturan85@gmail.com>
2025-10-27 08:44:10 +0100

    feat(list): add list category headers

--- projects/client/src/routes/users/[user]/lists/+page.svelte
+++ projects/client/src/routes/users/[user]/lists/+page.svelte
@@ -5,6 +5,7 @@
   import TraktPage from "$lib/sections/layout/TraktPage.svelte";
   import TraktPageCoverSetter from "$lib/sections/layout/TraktPageCoverSetter.svelte";
   import SmartListRenderer from "$lib/sections/lists/smart/SmartListRenderer.svelte";
+  import ListsHeader from "$lib/sections/lists/user/_internal/ListsHeader.svelte";
   import PersonalLists from "$lib/sections/lists/user/PersonalLists.svelte";
   import WatchList from "$lib/sections/lists/watchlist/WatchList.svelte";
   import { DEFAULT_SHARE_COVER } from "$lib/utils/constants";
@@ -27,7 +28,18 @@
   <PersonalLists slug="me" type="collaboration" />
 
   <RenderFor audience="director">
-    <SmartListRenderer type="movie" />
-    <SmartListRenderer type="show" />
+    <div class="trakt-lists-preview">
+      <ListsHeader title="Smart lists" />
+      <SmartListRenderer type="movie" />
+      <SmartListRenderer type="show" />
+    </div>
   </RenderFor>
 </TraktPage>
+
+<style>
+  .trakt-lists-preview {
+    display: flex;
+    flex-direction: column;
+    gap: var(--gap-micro);
+  }
+</style>
