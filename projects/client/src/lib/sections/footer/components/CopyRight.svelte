2eb0be975648dc9456563128635b9fd8a2178e9f
seferturan <seferturan85@gmail.com>
2025-10-27 18:42:53 +0100

    fix(footer): multiline copyright notice on small devices

--- projects/client/src/lib/sections/footer/components/CopyRight.svelte
+++ projects/client/src/lib/sections/footer/components/CopyRight.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
   import * as m from "$lib/features/i18n/messages.ts";
+  import RenderFor from "$lib/guards/RenderFor.svelte";
 
   const currentYear = new Date().getFullYear();
+  const copyright = $derived(`© 2010-${currentYear} trakt, inc.`);
 </script>
 
-<div class="trakt-copyright">
+{#snippet content(copyrightText: string, subtitleText: string)}
   <p class="secondary meta-info">
-    © 2010-{currentYear} trakt, inc. {m.text_copyright_notice()}
+    {copyrightText}
   </p>
-  <p class="secondary meta-info copyright-crafted-by">
-    {m.text_copyright_crafted_by()}
+  <p class="secondary meta-info">
+    {subtitleText}
   </p>
+{/snippet}
+
+<div class="trakt-copyright">
+  <RenderFor audience="all" device={["tablet-lg", "desktop"]}>
+    {@render content(
+      `${copyright} ${m.text_copyright_notice()}`,
+      m.text_copyright_crafted_by(),
+    )}
+  </RenderFor>
+
+  <RenderFor audience="all" device={["tablet-sm", "mobile"]}>
+    {@render content(copyright, m.text_copyright_notice())}
+  </RenderFor>
 </div>
 
 <style lang="scss">
@@ -22,11 +37,5 @@
     gap: var(--ni-8);
 
     width: fit-content;
-
-    @include for-tablet-sm-and-below {
-      .copyright-crafted-by {
-        display: none;
-      }
-    }
   }
 </style>
