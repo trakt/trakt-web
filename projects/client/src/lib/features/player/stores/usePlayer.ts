ca169aa2f19a4ba307d98aa7e2276126808455fc
Vlad Jerca <vlad.jerca@gmail.com>
2025-10-30 15:41:04 +0100

    feat(player): prevent autoplay after entering fullscreen on safari
    This commit prevents autoplay of the video after entering fullscreen mode on Safari.
    The `shouldAutoplay` variable is set to false after the video starts playing. This is a fix for safari to prevent the video from replaying.

--- projects/client/src/lib/features/player/stores/usePlayer.ts
+++ projects/client/src/lib/features/player/stores/usePlayer.ts
@@ -1,3 +1,4 @@
+import { get } from 'svelte/store';
 import { getPlayerContext } from '../_internal/getPlayerContext.ts';
 
 export function usePlayer() {
@@ -7,6 +8,13 @@ export function usePlayer() {
     const key = new URL(url).searchParams.get('v');
 
     embedId.set(key);
+    /**
+     * Reset autoplay to ensure reactivity when playing the same video twice in a row
+     * this is especially relevant on iOS for the native player
+     */
+    if (get(shouldAutoplay)) {
+      shouldAutoplay.set(false);
+    }
     shouldAutoplay.set(autoplay);
   };
 
