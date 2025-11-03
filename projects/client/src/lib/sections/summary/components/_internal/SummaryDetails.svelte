b263819eb295e3d40d4c99995d489249ab6988bf
seferturan <seferturan85@gmail.com>
2025-11-03 13:00:07 +0100

    refactor(summary): extract state from collapsable content component

--- projects/client/src/lib/sections/summary/components/_internal/SummaryDetails.svelte
+++ projects/client/src/lib/sections/summary/components/_internal/SummaryDetails.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import * as m from "$lib/features/i18n/messages.ts";
+  import { writable } from "svelte/store";
   import CollapsableContent from "./CollapsableContent.svelte";
 
   const { children }: ChildrenProps = $props();
@@ -8,8 +9,13 @@
     view: m.button_text_view_details(),
     hide: m.button_text_hide_details(),
   };
+
+  const isCollapsed = writable(true);
+  const toggle = () => {
+    isCollapsed.update((v) => !v);
+  };
 </script>
 
-<CollapsableContent {labels}>
+<CollapsableContent {labels} isCollapsed={$isCollapsed} {toggle}>
   {@render children()}
 </CollapsableContent>
