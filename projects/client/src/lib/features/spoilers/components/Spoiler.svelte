40f0c805f299fb39594c229630f125524f7e0671
seferturan <seferturan85@gmail.com>
2025-11-03 16:20:57 +0100

    fix(spoiler): blur spoilered content

--- projects/client/src/lib/features/spoilers/components/Spoiler.svelte
+++ projects/client/src/lib/features/spoilers/components/Spoiler.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
   import { type IsWatchedProps } from "$lib/sections/media-actions/mark-as-watched/useIsWatched";
+  import { NOOP_FN } from "$lib/utils/constants";
   import { useSpoilerAction } from "../_internal/useSpoilerAction";
   import { spoilMeAnyway } from "./spoilMeAnyway";
 
-  const { children, ...rest }: ChildrenProps & IsWatchedProps = $props();
+  const {
+    children,
+    variant = "dismissible",
+    ...rest
+  }: { variant?: "persistent" | "dismissible" } & ChildrenProps &
+    IsWatchedProps = $props();
 
   const { spoiler } = $derived(useSpoilerAction(rest));
+  const spoilAction = $derived(
+    variant === "persistent" ? NOOP_FN : spoilMeAnyway,
+  );
 </script>
 
-<trakt-spoiler use:spoiler use:spoilMeAnyway>
+<trakt-spoiler use:spoiler use:spoilAction>
   {@render children()}
 </trakt-spoiler>
 
-<style>
+<style lang="scss">
+  @use "$style/scss/mixins/index" as *;
+
   trakt-spoiler {
     &:global(:not(:empty):not(:has(*))),
     :global(p:not(button p):not(a p)),
+    :global(.trakt-comment p),
     :global(span:not(button span):not(a span)) {
       transition: var(--transition-increment) ease-in-out;
       transition-property: filter, padding;
     }
 
-    :global(.trakt-spoiler) {
+    &:global(.trakt-spoiler) {
       /* Target elements that contain only text */
       /* Target p and span that don't have button/anchor parents */
-      :global(:not(:empty):not(:has(*))),
+      &:global(:not(:empty):not(:has(*))),
       :global(p:not(button p):not(a p)),
+      :global(.trakt-comment p),
       :global(span:not(button span):not(a span)) {
-        --blur-size: calc(var(--ni-2) * 1.5);
-        filter: blur(var(--blur-size));
-        padding: 0 var(--blur-size);
         pointer-events: none;
+
+        @include spoiler-blur();
       }
     }
   }
