a71dc9370959482b318f2d7946727e9c1ef135de
seferturan <seferturan85@gmail.com>
2025-11-12 15:50:43 +0100

    feat(summary): adds known for to person title

---
+++ projects/client/src/lib/sections/summary/components/people/v2/_internal/PersonTitle.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { toTranslatedValue } from "$lib/utils/formatting/string/toTranslatedValue";
+
+  const { name, knownFor }: { name: string; knownFor: string | Nil } = $props();
+</script>
+
+<div class="trakt-person-title">
+  <h2 class:short-title={name.length < 15} class:long-title={name.length > 25}>
+    {name}
+  </h2>
+
+  {#if knownFor}
+    <p class="secondary smaller">
+      {toTranslatedValue("position", knownFor)}
+    </p>
+  {/if}
+</div>
+
+<style>
+  .trakt-person-title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h2 {
+    --text-size: 7cqi;
+
+    &.short-title {
+      --text-size: 10cqi;
+    }
+
+    &.long-title {
+      --text-size: 2.5cqi;
+    }
+
+    font-size: clamp(var(--ni-24), var(--text-size), var(--ni-32));
+    text-align: center;
+  }
+</style>
