4732b29c898c3b96f3700526672889e4bff62a7d
Vlad Jerca <vlad.jerca@gmail.com>
2025-11-12 21:54:56 +0100

    fix(summary): adjust confetti position and timing

--- projects/client/src/lib/sections/summary/components/people/v2/_internal/Celebration.svelte
+++ projects/client/src/lib/sections/summary/components/people/v2/_internal/Celebration.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+  import { time } from "$lib/utils/timing/time";
   import { Confetti } from "svelte-confetti";
 </script>
 
 <div class="confetti-container">
   <Confetti
     y={[0, 0.5]}
-    x={[-4, 4]}
+    x={[-1, 2]}
+    delay={[time.seconds(1), time.seconds(0.25)]}
     fallDistance={"10px"}
     rounded
     colorArray={["var(--shade-200)", "var(--purple-600)", "var(--red-400)"]}
