5f8a592b48e00b36a160072135025d0bf6df6b84
seferturan <seferturan85@gmail.com>
2025-11-19 16:42:55 +0100

    revert: "chore(streaming): remove colorize images feature flag"
    This reverts commit db9e02cdf98cf3ea53045577550bf74bb88c86d1.

--- projects/client/src/lib/sections/lists/where-to-watch/_internal/WhereToWatchItem.svelte
+++ projects/client/src/lib/sections/lists/where-to-watch/_internal/WhereToWatchItem.svelte
@@ -4,7 +4,9 @@
   import { StreamingServiceLogoIntlProvider } from "$lib/components/media/streaming-service/StreamingServiceLogoIntlProvider";
   import { AnalyticsEvent } from "$lib/features/analytics/events/AnalyticsEvent";
   import { useTrack } from "$lib/features/analytics/useTrack";
+  import { FeatureFlag } from "$lib/features/feature-flag/models/FeatureFlag";
   import * as m from "$lib/features/i18n/messages.ts";
+  import RenderForFeature from "$lib/guards/RenderForFeature.svelte";
   import type { StreamingServiceOption } from "$lib/requests/models/StreamingServiceOptions";
   import type { LibraryOption } from "../models/LibraryOption";
   import { getMediaCost } from "./getMediaCost";
@@ -43,12 +45,21 @@
     onclick={() => track({ source: service.source })}
   >
     <div class="where-to-watch-item-content">
-      <StreamingServiceLogo
-        source={service.source}
-        {country}
-        i18n={StreamingServiceLogoIntlProvider}
-        variant="colored"
-      />
+      <RenderForFeature flag={FeatureFlag.ColorizeLogos}>
+        {#snippet enabled()}
+          <StreamingServiceLogo
+            source={service.source}
+            {country}
+            i18n={StreamingServiceLogoIntlProvider}
+            variant="colored"
+          />
+        {/snippet}
+        <StreamingServiceLogo
+          source={service.source}
+          {country}
+          i18n={StreamingServiceLogoIntlProvider}
+        />
+      </RenderForFeature>
       <p class="meta-info">{text}</p>
     </div>
   </Link>
