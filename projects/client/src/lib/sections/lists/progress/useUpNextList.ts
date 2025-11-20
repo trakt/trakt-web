63443d479f0c9546a60ff36afea781147882f5d6
seferturan <seferturan85@gmail.com>
2025-11-20 14:48:46 +0100

    fix(up-next): filter out in progress movies from start watching movies

--- projects/client/src/lib/sections/lists/progress/useUpNextList.ts
+++ projects/client/src/lib/sections/lists/progress/useUpNextList.ts
@@ -12,6 +12,7 @@ import {
 } from '$lib/requests/queries/sync/upNextNitroQuery.ts';
 import { usePaginatedListQuery } from '$lib/sections/lists/stores/usePaginatedListQuery.ts';
 import type { CreateQueryOptions } from '@tanstack/svelte-query';
+import { derived } from 'svelte/store';
 import { mediaProgressQuery } from '../../../requests/queries/sync/mediaProgressQuery.ts';
 
 const RELEASED_LIST_LIMIT = 500;
@@ -45,5 +46,37 @@ function typeToQuery(props: UpNextStoreProps) {
 export function useUpNextList(
   props: UpNextStoreProps,
 ) {
-  return usePaginatedListQuery(typeToQuery(props));
+  const isStartWatchingMovie = props.type === 'movie' &&
+    props.intent === 'start';
+
+  if (!isStartWatchingMovie) {
+    return usePaginatedListQuery(typeToQuery(props));
+  }
+
+  /*
+    In case of a `start watching` movies, we also fetch the `continue` list
+    to filter out movies that are already in progress.
+  */
+  const startQuery = usePaginatedListQuery(typeToQuery(props));
+  const continueQuery = usePaginatedListQuery(typeToQuery({
+    ...props,
+    intent: 'continue',
+  }));
+
+  const filteredList = derived(
+    [startQuery.list, continueQuery.list],
+    ([$startList, $continueList]) => {
+      const continueMovieIds = new Set($continueList.map((entry) => entry.id));
+      return $startList.filter((entry) => !continueMovieIds.has(entry.id));
+    },
+  );
+
+  return {
+    ...startQuery,
+    list: filteredList,
+    isLoading: derived(
+      [startQuery.isLoading, continueQuery.isLoading],
+      ([$startLoading, $continueLoading]) => $startLoading || $continueLoading,
+    ),
+  };
 }
