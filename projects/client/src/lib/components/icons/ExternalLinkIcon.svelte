07b22e79be2c7bcf3d3c567b4ef6d246de01a6ae
seferturan <seferturan85@gmail.com>
2025-11-26 20:25:45 +0100

    feat(banner): add black friday banner

--- projects/client/src/lib/components/icons/ExternalLinkIcon.svelte
+++ projects/client/src/lib/components/icons/ExternalLinkIcon.svelte
@@ -1,12 +1,33 @@
-<svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path
-    d="M8 4H4V20H20V16H22V22H2V2H8V4ZM22 13H20V5.41406L10.707 14.707L9.29297 13.293L18.5859 4H11V2H22V13Z"
-    fill="currentColor"
-  />
-</svg>
+<script lang="ts">
+  const { size = "large" }: { size?: "small" | "large" } = $props();
+</script>
+
+{#if size === "large"}
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8 4H4V20H20V16H22V22H2V2H8V4ZM22 13H20V5.41406L10.707 14.707L9.29297 13.293L18.5859 4H11V2H22V13Z"
+      fill="currentColor"
+    />
+  </svg>
+{/if}
+
+{#if size === "small"}
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M15 15H13V4.41406L2.70703 14.707L1.29297 13.293L11.5859 3H1V1H15V15Z"
+      fill="currentColor"
+    />
+  </svg>
+{/if}
---
+++ projects/client/src/lib/sections/banner/black-friday/_internal/ClaimOfferLink.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import ExternalLinkIcon from "$lib/components/icons/ExternalLinkIcon.svelte";
+  import Link from "$lib/components/link/Link.svelte";
+  import RenderFor from "$lib/guards/RenderFor.svelte";
+  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import DealEnd from "./DealEnd.svelte";
+
+  const { endDate }: { endDate: Date } = $props();
+</script>
+
+<div class="trakt-black-friday-claim-offer">
+  <RenderFor audience="all" device={["desktop", "tablet-lg"]}>
+    <DealEnd {endDate} />
+  </RenderFor>
+
+  <Link href={UrlBuilder.vip()} target="_blank">
+    <ExternalLinkIcon size="small" />
+    <span class="no-wrap">Claim offer</span>
+  </Link>
+</div>
+
+<style lang="scss">
+  @use "$style/scss/mixins/index" as *;
+
+  .trakt-black-friday-claim-offer {
+    display: flex;
+    align-items: center;
+    gap: var(--gap-m);
+
+    :global(svg) {
+      width: var(--ni-14);
+      height: var(--ni-14);
+    }
+
+    :global(.trakt-link) {
+      display: flex;
+      align-items: center;
+      gap: var(--gap-xs);
+
+      background-color: var(--purple-500);
+      border-radius: var(--border-radius-l);
+      padding: var(--ni-8) var(--ni-12);
+
+      text-decoration: none;
+
+      transition: padding var(--transition-increment) ease-in-out;
+
+      @include for-mouse() {
+        &:hover {
+          color: var(--purple-100);
+        }
+      }
+    }
+
+    @include for-tablet-lg {
+      flex-direction: column-reverse;
+      gap: var(--gap-micro);
+
+      :global(.trakt-link) {
+        padding: var(--ni-4) var(--ni-10);
+      }
+    }
+
+    @include for-tablet-sm-and-below {
+      flex-direction: row-reverse;
+      gap: var(--gap-xs);
+    }
+  }
+</style>
