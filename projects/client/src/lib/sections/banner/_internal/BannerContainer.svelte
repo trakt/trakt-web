866bd594253e7c0d9ffbcc111e5ce795a2669ce1
seferturan <seferturan85@gmail.com>
2025-11-26 13:01:16 +0100

    feat(banner): limited size on large screens

--- projects/client/src/lib/sections/banner/_internal/BannerContainer.svelte
+++ projects/client/src/lib/sections/banner/_internal/BannerContainer.svelte
@@ -3,12 +3,22 @@
 </script>
 
 <div class="trakt-banner-container">
-  {@render children()}
+  <div class="trakt-banner-content">
+    {@render children()}
+  </div>
 </div>
 
 <style>
   .trakt-banner-container {
     margin-left: var(--layout-distance-side);
     margin-right: var(--layout-distance-side);
+
+    display: flex;
+    justify-content: center;
+  }
+
+  .trakt-banner-content {
+    width: 100%;
+    max-width: var(--ni-920);
   }
 </style>
