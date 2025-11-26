6434f79997bd3125629e7302ab750f5b8b1bf2e2
seferturan <seferturan85@gmail.com>
2025-11-26 17:20:52 +0100

    refactor(banner): extract dismiss button

---
+++ projects/client/src/lib/sections/banner/_internal/DismissButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import ActionButton from "$lib/components/buttons/ActionButton.svelte";
+  import CloseIcon from "$lib/components/icons/CloseIcon.svelte";
+  import * as m from "$lib/features/i18n/messages.ts";
+
+  const { onDismiss }: { onDismiss: () => void } = $props();
+</script>
+
+<trakt-banner-dismiss-button>
+  <ActionButton
+    onclick={onDismiss}
+    label={m.button_label_dismiss()}
+    size="small"
+    style="ghost"
+  >
+    <CloseIcon />
+  </ActionButton>
+</trakt-banner-dismiss-button>
+
+<style>
+  trakt-banner-dismiss-button {
+    :global(.trakt-action-button) {
+      color: var(--shade-10);
+    }
+  }
+</style>
