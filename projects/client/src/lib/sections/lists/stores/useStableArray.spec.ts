75eaf1637c9fd3170dceae21724fce3079d37368
seferturan <seferturan85@gmail.com>
2025-11-26 09:41:18 +0100

    fix: make stable array reactive outside of component lifecycle

--- projects/client/src/lib/sections/lists/stores/useStableArray.spec.ts
+++ projects/client/src/lib/sections/lists/stores/useStableArray.spec.ts
@@ -1,22 +1,29 @@
-import { get } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 import { describe, expect, it } from 'vitest';
 import { useStableArray } from './useStableArray.ts';
 
-const item1 = {
+type MockItem = {
+  show: {
+    id: number;
+    title: string;
+  };
+};
+
+const item1: MockItem = {
   show: {
     id: 1,
     title: 'Show 1',
   },
 };
 
-const item2 = {
+const item2: MockItem = {
   show: {
     id: 2,
     title: 'Show 2',
   },
 };
 
-const item3 = {
+const item3: MockItem = {
   show: {
     id: 3,
     title: 'Show 3',
@@ -28,82 +35,96 @@ const compareFn = (left: typeof item1, right: typeof item1) =>
 
 describe('useStableArray', () => {
   it('should return a store with an empty array', () => {
-    expect(get(useStableArray(compareFn).list)).toEqual([]);
+    const source = writable([]);
+    expect(get(useStableArray(compareFn, source).list)).toEqual([]);
   });
 
   it('should update the store with a new item', () => {
-    const store = useStableArray(compareFn);
+    const source = writable<MockItem[]>([]);
+    const store = useStableArray(compareFn, source);
 
-    store.set([item1]);
+    source.set([item1]);
 
     expect(get(store.list)).toEqual([item1]);
   });
 
   it('should update the store with multiple items', () => {
-    const store = useStableArray(compareFn);
+    const source = writable<MockItem[]>([]);
+    const store = useStableArray(compareFn, source);
     const items = [item1, item2];
 
-    store.set(items);
+    source.set(items);
 
     expect(get(store.list)).toEqual(items);
   });
 
   it('should update the store with the same item', () => {
-    const store = useStableArray(compareFn);
+    const source = writable<MockItem[]>([]);
+    const store = useStableArray(compareFn, source);
 
-    store.set([item1]);
-    store.set([item1]);
+    source.set([item1]);
+    source.set([item1]);
 
     expect(get(store.list)).toEqual([item1]);
   });
 
   it('should update the store with the same item with different data', () => {
-    const store = useStableArray(compareFn);
+    const source = writable<MockItem[]>([]);
+    const store = useStableArray(compareFn, source);
 
     const update = { ...item1, title: 'Updated Show 1' };
-    store.set([item1]);
-    store.set([update]);
+    source.set([item1]);
+    source.set([update]);
 
     expect(get(store.list)).toEqual([update]);
   });
 
   it('should update the store with multiple items with the same item', () => {
-    const store = useStableArray(compareFn);
+    const source = writable<MockItem[]>([]);
+    const store = useStableArray(compareFn, source);
     const items = [
       item1,
       item1,
     ];
 
-    store.set(items);
+    source.set(items);
 
     expect(get(store.list)).toEqual([item1]);
   });
 
   it('should add new items at the end of the list', () => {
-    const store = useStableArray(compareFn);
+    const source = writable<MockItem[]>([]);
+    const store = useStableArray(compareFn, source);
+    const unsubscribe = store.list.subscribe(() => {});
 
-    store.set([item1, item2]);
-    store.set([item3, item1, item2]);
+    source.set([item1, item2]);
+    source.set([item3, item1, item2]);
 
     expect(get(store.list)).toEqual([item1, item2, item3]);
+    unsubscribe();
   });
 
   it('should remove items that are not in the new list', () => {
-    const store = useStableArray(compareFn);
+    const source = writable<MockItem[]>([]);
+    const store = useStableArray(compareFn, source);
 
-    store.set([item1, item2]);
-    store.set([item3]);
+    source.set([item1, item2]);
+    source.set([item3]);
 
     expect(get(store.list)).toEqual([item3]);
   });
 
   it('should keep the order of the items', () => {
-    const store = useStableArray(compareFn);
+    const source = writable<MockItem[]>([]);
+    const store = useStableArray(compareFn, source);
     const update2 = { ...item2, title: 'Updated Show 2' };
 
-    store.set([item1, item2, item3]);
-    store.set([update2, item1, item3]);
+    const unsubscribe = store.list.subscribe(() => {});
+
+    source.set([item1, item2, item3]);
+    source.set([update2, item1, item3]);
 
     expect(get(store.list)).toEqual([item1, update2, item3]);
+    unsubscribe();
   });
 });
