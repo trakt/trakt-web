f80d1fdf667d75671904df52b1037fccf6daa434
seferturan <seferturan85@gmail.com>
2025-11-28 14:29:06 +0100

    refactor: extract paginated list component

---
+++ projects/client/src/lib/components/lists/models/PaginatedListProps.ts
@@ -0,0 +1,9 @@
+import type { FilterParams } from '$lib/requests/models/FilterParams.ts';
+import type { PaginatableStore } from '$lib/sections/lists/drilldown/PaginatableStore.ts';
+import type { Snippet } from 'svelte';
+
+export type PaginatedListProps<T, M> = {
+  useList: PaginatableStore<T, M>;
+  type: M;
+  items: Snippet<[T[]]>;
+} & FilterParams;
