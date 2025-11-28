5998c97dc3ae36a7242be1f6400865f0119e9217
seferturan <seferturan85@gmail.com>
2025-11-28 14:29:06 +0100

    refactor: extract paginated list component

---
+++ projects/client/src/lib/components/lists/PaginatedList.svelte
@@ -0,0 +1,65 @@
+<script lang="ts" generics="T, M">
+  import LoadingIndicator from "$lib/sections/lists/drilldown/_internal/LoadingIndicator.svelte";
+  import { useLazyLoader } from "$lib/sections/lists/drilldown/_internal/useLazyLoader";
+  import { DEFAULT_DRILL_SIZE } from "$lib/utils/constants";
+  import { writable } from "svelte/store";
+  import type { PaginatedListProps } from "./models/PaginatedListProps";
+
+  const { useList, type, filter, items }: PaginatedListProps<T, M> = $props();
+
+  const currentPage = writable(1);
+  const loadedPages = writable<Map<number, T[]>>(new Map());
+  const initialType = writable(type);
+
+  const { list, page, isLoading } = $derived(
+    useList({
+      type,
+      filter,
+      page: $currentPage,
+      limit: DEFAULT_DRILL_SIZE,
+    }),
+  );
+
+  $effect(() => {
+    if (type !== $initialType) {
+      currentPage.set(1);
+      loadedPages.set(new Map());
+      initialType.set(type);
+    }
+  });
+
+  $effect(() => {
+    if ($isLoading) {
+      return;
+    }
+    loadedPages.update((pages) => {
+      pages.set($page.current ?? 1, $list);
+      return pages;
+    });
+  });
+
+  const loadMore = () => {
+    if ($isLoading) {
+      return;
+    }
+
+    const hasMorePages = ($page?.total ?? 1) > $currentPage;
+    const isCurrentPageFetched =
+      $loadedPages.get($page?.current ?? 1) !== undefined;
+
+    if (isCurrentPageFetched && hasMorePages) {
+      currentPage.update((page) => page + 1);
+    }
+  };
+
+  const { observeDimension } = $derived(useLazyLoader({ loadMore }));
+  const allItems = $derived(Array.from($loadedPages.values()).flat());
+</script>
+
+<div class="trakt-paginated-list" use:observeDimension>
+  {@render items(allItems)}
+</div>
+
+{#if $isLoading}
+  <LoadingIndicator />
+{/if}
--- projects/client/src/lib/sections/lists/drilldown/DrilledMediaList.svelte
+++ projects/client/src/lib/sections/lists/drilldown/DrilledMediaList.svelte
@@ -1,12 +1,9 @@
 <script lang="ts" generics="T extends { key: string }, M">
   import GridList from "$lib/components/lists/grid-list/GridList.svelte";
-  import { DEFAULT_DRILL_SIZE } from "$lib/utils/constants";
+  import PaginatedList from "$lib/components/lists/PaginatedList.svelte";
   import type { Snippet } from "svelte";
-  import { writable } from "svelte/store";
   import { mediaCardWidthResolver } from "../utils/mediaCardWidthResolver";
   import type { MediaListProps } from "./MediaListProps";
-  import LoadingIndicator from "./_internal/LoadingIndicator.svelte";
-  import { useLazyLoader } from "./_internal/useLazyLoader";
 
   type DrilledMediaListProps = MediaListProps<T, M> & {
     actions?: Snippet<[]>;
@@ -22,73 +19,19 @@
     cardOrientation = "portrait",
     ...props
   }: DrilledMediaListProps = $props();
-
-  const currentPage = writable(1);
-  const loadedPages = writable<Map<number, T[]>>(new Map());
-  const initialType = writable(type);
-
-  const { list, page, isLoading } = $derived(
-    useList({
-      type,
-      filter,
-      page: $currentPage,
-      limit: DEFAULT_DRILL_SIZE,
-    }),
-  );
-
-  // FIXME: simplify and get rid of effects
-  $effect(() => {
-    if ($isLoading) {
-      return;
-    }
-    loadedPages.update((pages) => {
-      pages.set($page.current ?? 1, $list);
-      return pages;
-    });
-  });
-
-  $effect(() => {
-    if ($initialType === type || $currentPage === 1) {
-      return;
-    }
-
-    currentPage.set(1);
-    loadedPages.set(new Map());
-    initialType.set(type);
-  });
-
-  const loadMore = () => {
-    if ($isLoading) {
-      return;
-    }
-
-    const hasMorePages = ($page?.total ?? 1) > $currentPage;
-    const isCurrentPageFetched =
-      $loadedPages.get($page?.current ?? 1) !== undefined;
-
-    if (isCurrentPageFetched && hasMorePages) {
-      currentPage.update((page) => page + 1);
-    }
-  };
-
-  const { observeDimension } = $derived(useLazyLoader({ loadMore }));
-  const allItems = $derived(Array.from($loadedPages.values()).flat());
 </script>
 
-<GridList
-  {...props}
-  {actions}
-  items={allItems}
-  dimensionObserver={observeDimension}
-  --width-item={mediaCardWidthResolver(cardOrientation)}
->
-  {#snippet empty()}
-    {#if !$isLoading}
-      {@render externalEmpty?.()}
-    {/if}
+<PaginatedList {type} {filter} {useList}>
+  {#snippet items(items)}
+    <GridList
+      {...props}
+      {actions}
+      {items}
+      --width-item={mediaCardWidthResolver(cardOrientation)}
+    >
+      {#snippet empty()}
+        {@render externalEmpty?.()}
+      {/snippet}
+    </GridList>
   {/snippet}
-</GridList>
-
-{#if $isLoading}
-  <LoadingIndicator />
-{/if}
+</PaginatedList>
