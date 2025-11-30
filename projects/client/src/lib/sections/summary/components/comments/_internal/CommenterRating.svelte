8fa5f521dae624f71f5e90a21da49af9baeb50fb
seferturan <seferturan85@gmail.com>
2025-11-30 13:40:43 +0100

    feat(comment): show commenter ratings

---
+++ projects/client/src/lib/sections/summary/components/comments/_internal/CommenterRating.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import StarIcon from "$lib/components/icons/StarIcon.svelte";
+  import { useUser } from "$lib/features/auth/stores/useUser";
+  import { getLocale } from "$lib/features/i18n";
+  import type { MediaComment } from "$lib/requests/models/MediaComment";
+  import { toHumanNumber } from "$lib/utils/formatting/number/toHumanNumber";
+  import { STAR_RATINGS } from "../../rating/constants";
+
+  const MAX_RATING = 10;
+
+  const { comment }: { comment: MediaComment } = $props();
+
+  const { user } = useUser();
+  const isOwnComment = $derived(comment.user.id === $user.id);
+
+  const rating = $derived.by(() => {
+    const rating = comment.user.stats.rating;
+    if (!rating) return;
+
+    const factor = MAX_RATING / STAR_RATINGS.length;
+    return rating / factor;
+  });
+</script>
+
+{#if !isOwnComment && rating}
+  <div class="trakt-commenter-rating">
+    <span>{toHumanNumber(rating, getLocale())}</span>
+    <StarIcon fillPercent={100} />
+  </div>
+{/if}
+
+<style>
+  .trakt-commenter-rating {
+    display: flex;
+    align-items: center;
+    gap: var(--gap-xxs);
+
+    :global(svg) {
+      width: var(--ni-16);
+      height: var(--ni-16);
+    }
+  }
+</style>
