c7193ccb1693319e7835e24dfaaf420d110d7ee3
seferturan <seferturan85@gmail.com>
2025-11-30 15:46:17 +0100

    chore(streaming): remove unused stream buttons

--- projects/client/src/lib/components/buttons/streaming-service/StreamingServiceButtonIntlProvider.ts
+++
@@ -1,7 +0,0 @@
-import * as m from '$lib/features/i18n/messages.ts';
-import type { StreamingServiceButtonIntl } from './StreamingServiceButtonIntl.ts';
-
-export const StreamingServiceButtonIntlProvider: StreamingServiceButtonIntl = {
-  title: (title: string) => m.button_label_stream_on({ title }),
-  streamOn: () => m.button_text_stream_on(),
-};
--- projects/client/src/lib/sections/summary/components/stream/StreamOnButtonProps.ts
+++
@@ -1,8 +0,0 @@
-import type { StreamOn } from '$lib/requests/models/StreamOn.ts';
-import type { MetaInfoProps } from '../media/useMediaMetaInfo.ts';
-
-export type StreamOnButtonProps = {
-  streamOn?: StreamOn;
-  style: 'logo' | 'normal' | 'dropdown-item';
-  size?: 'small' | 'normal';
-} & MetaInfoProps;
