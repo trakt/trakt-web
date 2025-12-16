ef6bfc38c13a81b6ed2660ccd2095f49e9533b57
seferturan <seferturan85@gmail.com>
2025-12-16 14:11:26 +0100

    fix: bind this to class instance

--- projects/client/src/lib/utils/store/WritableSubject.ts
+++ projects/client/src/lib/utils/store/WritableSubject.ts
@@ -1,13 +1,13 @@
 import { BehaviorSubject } from 'rxjs';
 
 export class WritableSubject<T> extends BehaviorSubject<T> {
-  public set(value: T): void {
+  public set = (value: T): void => {
     this.next(value);
-  }
+  };
 
-  public update(updater: (value: T) => T): void {
+  public update = (updater: (value: T) => T): void => {
     this.next(updater(this.getValue()));
-  }
+  };
 }
 
 export function writable<T>(initialValue: T): WritableSubject<T> {
