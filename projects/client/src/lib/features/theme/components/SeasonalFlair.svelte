52b31ebd30bf9f026c874e0361a1e5c6200af257
seferturan <seferturan85@gmail.com>
2025-12-18 15:57:17 +0100

    feat(theme): also show splash screen on larger screens

--- projects/client/src/lib/features/theme/components/SeasonalFlair.svelte
+++ projects/client/src/lib/features/theme/components/SeasonalFlair.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import RenderFor from "$lib/guards/RenderFor.svelte";
   import { useSeasonalTheme } from "../useSeasonalTheme";
   import SeasonalBackground from "./_internal/SeasonalBackground.svelte";
   import SeasonalSplashscreen from "./_internal/SeasonalSplashscreen.svelte";
@@ -19,11 +18,9 @@
 {#if $activeTheme}
   <SeasonalBackground themeId={$activeTheme} />
 
-  <RenderFor audience="all" device={["tablet-sm", "mobile"]}>
-    <SeasonalSplashscreen
-      hasSplashScreen={$hasSplashScreen}
-      themeId={$activeTheme}
-      {onDismiss}
-    />
-  </RenderFor>
+  <SeasonalSplashscreen
+    hasSplashScreen={$hasSplashScreen}
+    themeId={$activeTheme}
+    {onDismiss}
+  />
 {/if}
