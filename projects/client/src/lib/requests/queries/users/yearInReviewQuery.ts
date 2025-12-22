b65ecf3257e75812802b077c58ce7859138e4892
seferturan <seferturan85@gmail.com>
2025-12-22 12:10:14 +0100

    feat(requests): add request for yir

---
+++ projects/client/src/lib/requests/queries/users/yearInReviewQuery.ts
@@ -0,0 +1,34 @@
+import { defineQuery } from '$lib/features/query/defineQuery.ts';
+import { api, type ApiParams } from '$lib/requests/api.ts';
+import { time } from '$lib/utils/timing/time.ts';
+import { mapToUserReview, UserReviewSchema } from './monthInReviewQuery.ts';
+
+export type YearInReviewParams = {
+  slug: string;
+  year: number;
+} & ApiParams;
+
+const yearInReviewRequest = (
+  { fetch, year, slug }: YearInReviewParams,
+) =>
+  api({ fetch })
+    .users
+    .year_in_review({
+      params: {
+        id: slug,
+        year,
+      },
+      query: {
+        extended: 'images',
+      },
+    });
+
+export const yearInReviewQuery = defineQuery({
+  key: 'yearInReview',
+  request: yearInReviewRequest,
+  invalidations: [],
+  dependencies: (params) => [params.slug, params.year],
+  mapper: (response) => mapToUserReview(response.body),
+  schema: UserReviewSchema,
+  ttl: time.days(1),
+});
--- projects/client/src/mocks/data/users/mapped/UserMonthInReviewMappedMock.ts
+++ projects/client/src/mocks/data/users/mapped/UserMonthInReviewMappedMock.ts
@@ -1,10 +1,12 @@
-import type { UserMonthInReview } from '$lib/requests/queries/users/monthInReviewQuery.ts';
+import type { UserReview } from '$lib/requests/queries/users/monthInReviewQuery.ts';
 import { MovieHereticMappedMock } from '../../summary/movies/heretic/mapped/MovieHereticMappedMock.ts';
 
-export const UserMonthInReviewMappedMock: UserMonthInReview = {
+export const UserMonthInReviewMappedMock: UserReview = {
   'commentsCount': 0,
   'hoursWatched': 12,
   'playCount': 7,
   'ratingsCount': 0,
   'firstPlay': MovieHereticMappedMock,
+  'libraryCount': 1,
+  'listsCount': 0,
 };
