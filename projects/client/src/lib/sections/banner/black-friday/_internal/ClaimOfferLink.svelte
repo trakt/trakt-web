15466ff5497470e1e4b83e180dbc1a94f99e9064
seferturan <seferturan85@gmail.com>
2025-12-22 14:27:47 +0100

    refactor(banner): extract banner link

--- projects/client/src/lib/sections/banner/black-friday/_internal/ClaimOfferLink.svelte
+++ projects/client/src/lib/sections/banner/black-friday/_internal/ClaimOfferLink.svelte
@@ -1,77 +1,19 @@
 <script lang="ts">
-  import ExternalLinkIcon from "$lib/components/icons/ExternalLinkIcon.svelte";
-  import Link from "$lib/components/link/Link.svelte";
-  import { AnalyticsEvent } from "$lib/features/analytics/events/AnalyticsEvent";
-  import { useTrack } from "$lib/features/analytics/useTrack";
   import type { Promotion } from "$lib/features/promotions/models/Promotion";
   import RenderFor from "$lib/guards/RenderFor.svelte";
   import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import BannerLink from "../../_internal/BannerLink.svelte";
   import DealEnd from "./DealEnd.svelte";
 
   const { promotion }: { promotion: Promotion } = $props();
-  const { track } = useTrack(AnalyticsEvent.Link);
 </script>
 
-<div class="trakt-black-friday-claim-offer">
-  <RenderFor audience="all" device={["desktop", "tablet-lg"]}>
-    <DealEnd endDate={promotion.end} />
-  </RenderFor>
+<BannerLink href={UrlBuilder.vip()} target="_blank" source={promotion.id}>
+  {#snippet tag()}
+    <RenderFor audience="all" device={["desktop", "tablet-lg"]}>
+      <DealEnd endDate={promotion.end} />
+    </RenderFor>
+  {/snippet}
 
-  <Link
-    href={UrlBuilder.vip()}
-    target="_blank"
-    onclick={() => track({ source: promotion.id, target: UrlBuilder.vip() })}
-  >
-    <ExternalLinkIcon size="small" />
-    <span class="no-wrap">Claim offer</span>
-  </Link>
-</div>
-
-<style lang="scss">
-  @use "$style/scss/mixins/index" as *;
-
-  .trakt-black-friday-claim-offer {
-    display: flex;
-    align-items: center;
-    gap: var(--gap-m);
-
-    :global(svg) {
-      width: var(--ni-14);
-      height: var(--ni-14);
-    }
-
-    :global(.trakt-link) {
-      display: flex;
-      align-items: center;
-      gap: var(--gap-xs);
-
-      background-color: var(--purple-500);
-      border-radius: var(--border-radius-l);
-      padding: var(--ni-8) var(--ni-12);
-
-      text-decoration: none;
-
-      transition: padding var(--transition-increment) ease-in-out;
-
-      @include for-mouse() {
-        &:hover {
-          color: var(--purple-100);
-        }
-      }
-    }
-
-    @include for-tablet-lg {
-      flex-direction: column-reverse;
-      gap: var(--gap-micro);
-
-      :global(.trakt-link) {
-        padding: var(--ni-4) var(--ni-10);
-      }
-    }
-
-    @include for-tablet-sm-and-below {
-      flex-direction: row-reverse;
-      gap: var(--gap-xs);
-    }
-  }
-</style>
+  Claim offer
+</BannerLink>
