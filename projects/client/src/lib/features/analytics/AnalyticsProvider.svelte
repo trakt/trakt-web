3010e14dcfc17a44a2a4fe6a13372bf3f9d2bdad
Vlad Jerca <vlad.jerca@gmail.com>
2026-01-15 14:15:00 +0100

    feat(analytics): setup reddit pixel
    Adds Reddit pixel tracking, enabling it based on user consent.
    
    Updates the Content Security Policy to allow connections to and images from Reddit.

--- projects/client/src/lib/features/analytics/AnalyticsProvider.svelte
+++ projects/client/src/lib/features/analytics/AnalyticsProvider.svelte
@@ -7,6 +7,33 @@
   const { consent } = useCookieConsent();
 </script>
 
+<svelte:head>
+  {#if $consent === "all"}
+    <!-- Reddit Pixel -->
+    <script>
+      !(function (w, d) {
+        if (!w.rdt) {
+          var p = (w.rdt = function () {
+            p.sendEvent
+              ? p.sendEvent.apply(p, arguments)
+              : p.callQueue.push(arguments);
+          });
+          p.callQueue = [];
+          var t = d.createElement("script");
+          ((t.src = "https://www.redditstatic.com/ads/pixel.js"),
+            (t.async = !0));
+          var s = d.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(t, s);
+        }
+      })(window, document);
+      rdt("init", "a2_i1ea6gxvoudq");
+      rdt("track", "PageVisit");
+    </script>
+    <!-- DO NOT MODIFY UNLESS TO REPLACE A USER IDENTIFIER -->
+    <!-- End Reddit Pixel -->
+  {/if}
+</svelte:head>
+
 {#if $consent === "all"}
   <FirebaseSetup>
     {@render children()}
