214a91f9dd1d396ce68f31e5081fb2c06d316d1a
Vlad Jerca <vlad.jerca@gmail.com>
2026-01-15 13:37:40 +0100

    refactor(analytics): remove hotjar
    Updates the content security policy (CSP) to align with analytics changes and refactors the analytics provider component.
    Removes `https://*.hotjar.com` from `connect-src` and `frame-src`.
    Moves the Hotjar script to the conditional block based on cookie consent.

--- projects/client/src/lib/features/analytics/AnalyticsProvider.svelte
+++ projects/client/src/lib/features/analytics/AnalyticsProvider.svelte
@@ -7,33 +7,6 @@
   const { consent } = useCookieConsent();
 </script>
 
-<svelte:head>
-  {#if $consent === "all"}
-    <!-- HOTJAR -->
-    <script>
-      (function (c, s, q, u, a, r, e) {
-        c.hj =
-          c.hj ||
-          function () {
-            (c.hj.q = c.hj.q || []).push(arguments);
-          };
-        c._hjSettings = { hjid: a };
-        r = s.getElementsByTagName("head")[0];
-        e = s.createElement("script");
-        e.async = true;
-        e.src = q + c._hjSettings.hjid + u;
-        r.appendChild(e);
-      })(
-        window,
-        document,
-        "https://static.hj.contentsquare.net/c/csq-",
-        ".js",
-        6374269,
-      );
-    </script>
-  {/if}
-</svelte:head>
-
 {#if $consent === "all"}
   <FirebaseSetup>
     {@render children()}
