4de0ecdde785e4b282a4a9cf2c7034b27b5accd3
seferturan <seferturan85@gmail.com>
2026-01-15 16:49:04 +0100

    feat(summary): new design and placement for social media links
    Fixes #1542

--- projects/client/src/lib/sections/summary/components/people/v2/_internal/SocialMediaLinks.svelte
+++ projects/client/src/lib/sections/summary/components/people/v2/_internal/SocialMediaLinks.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
   import type { PersonSummary } from "$lib/requests/models/PersonSummary";
-  import SummaryActionsBar from "../../../_internal/SummaryActionsBar.svelte";
   import SocialMediaAction from "./SocialMediaAction.svelte";
 
-  const {
-    person,
-    variant = "default",
-  }: { person: PersonSummary; variant?: "default" | "compact" } = $props();
+  const { person }: { person: PersonSummary } = $props();
 
   const facebookUsername = $derived(person.socialMedia?.facebook);
   const xUsername = $derived(person.socialMedia?.x);
@@ -17,40 +13,16 @@
   );
 </script>
 
-<trakt-social-media-links data-variant={variant}>
-  {#if hasSocialMediaLinks}
-    <SummaryActionsBar>
-      {#if facebookUsername}
-        <SocialMediaAction username={facebookUsername} type="facebook" />
-      {/if}
-
-      {#if xUsername}
-        <SocialMediaAction username={xUsername} type="x" />
-      {/if}
-
-      {#if instagramUsername}
-        <SocialMediaAction username={instagramUsername} type="instagram" />
-      {/if}
-    </SummaryActionsBar>
+{#if hasSocialMediaLinks}
+  {#if facebookUsername}
+    <SocialMediaAction username={facebookUsername} type="facebook" />
   {/if}
-</trakt-social-media-links>
 
-<style>
-  trakt-social-media-links {
-    :global(.trakt-summary-actions) {
-      justify-content: center;
-      gap: var(--gap-l);
+  {#if xUsername}
+    <SocialMediaAction username={xUsername} type="x" />
+  {/if}
 
-      :global(svg) {
-        height: var(--ni-24);
-        width: auto;
-      }
-    }
-
-    &[data-variant="compact"] {
-      :global(.trakt-summary-actions) {
-        width: fit-content;
-      }
-    }
-  }
-</style>
+  {#if instagramUsername}
+    <SocialMediaAction username={instagramUsername} type="instagram" />
+  {/if}
+{/if}
