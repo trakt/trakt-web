659b9380c9f8c4ad4adcb76401e30f295eb6ebd9
seferturan <seferturan85@gmail.com>
2026-01-16 10:18:54 +0100

    test(query): mapping user likes

---
+++ projects/client/src/mocks/data/users/response/MinimalLikedListsResponseMock.ts
@@ -0,0 +1,18 @@
+import type { LikedListItemResponse } from '@trakt/api';
+
+export const MinimalLikedListsResponseMock: LikedListItemResponse[] = [
+  {
+    'liked_at': '2026-01-16T08:04:27.000Z',
+    'type': 'list',
+    'list': {
+      'id': 19503241,
+    },
+  },
+  {
+    'liked_at': '2026-01-16T07:27:25.000Z',
+    'type': 'list',
+    'list': {
+      'id': 30925754,
+    },
+  },
+];
