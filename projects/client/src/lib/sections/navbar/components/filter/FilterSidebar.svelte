d2091c9d7a61d35fd2897bb6e6b8af49d90f4b56
seferturan <seferturan85@gmail.com>
2026-01-22 05:04:27 +0100

    feat(filter): uses more compact design for filters
    Fixes https://github.com/trakt/trakt-web/issues/1484

--- projects/client/src/lib/sections/navbar/components/filter/FilterSidebar.svelte
+++ projects/client/src/lib/sections/navbar/components/filter/FilterSidebar.svelte
@@ -5,35 +5,55 @@
   import { DpadNavigationType } from "$lib/features/navigation/models/DpadNavigationType";
   import ResetAllButton from "./_internal/ResetAllButton.svelte";
   import SaveFiltersButton from "./_internal/SaveFiltersButton.svelte";
+  import FilterGroup from "./filters/_internal/FilterGroup.svelte";
   import ListFilter from "./filters/ListFilter.svelte";
   import RatingsFilter from "./filters/RatingsFilter.svelte";
   import ToggleFilter from "./filters/ToggleFilter.svelte";
 
   const { onClose }: { onClose: () => void } = $props();
   const { filters } = useFilter();
+
+  const listTypeFilters = $derived(
+    filters.filter((filter) => filter.type === "list"),
+  );
+  const ratingTypeFilters = $derived(
+    filters.filter((filter) => filter.type === "ratings"),
+  );
+  const toggleTypeFilters = $derived(
+    filters.filter((filter) => filter.type === "toggle"),
+  );
 </script>
 
-<Drawer {onClose} title={m.header_filters()} trapSelector=".trakt-filter">
-  {#each filters as filter}
-    {#if filter.type === "list"}
-      <ListFilter {filter} />
-    {/if}
+<Drawer
+  {onClose}
+  title={m.header_filters()}
+  trapSelector=".trakt-filter"
+  size="auto"
+>
+  <div class="trakt-filters-content">
+    <FilterGroup>
+      {#each listTypeFilters as filter}
+        <ListFilter {filter} />
+      {/each}
+    </FilterGroup>
 
-    {#if filter.type === "ratings"}
+    {#each ratingTypeFilters as filter}
       <RatingsFilter {filter} />
-    {/if}
+    {/each}
 
-    {#if filter.type === "toggle"}
-      <ToggleFilter {filter} />
-    {/if}
-  {/each}
+    <FilterGroup>
+      {#each toggleTypeFilters as filter}
+        <ToggleFilter {filter} />
+      {/each}
+    </FilterGroup>
 
-  <div
-    class="trakt-filter-actions"
-    data-dpad-navigation={DpadNavigationType.List}
-  >
-    <SaveFiltersButton onSave={onClose} />
-    <ResetAllButton />
+    <div
+      class="trakt-filter-actions"
+      data-dpad-navigation={DpadNavigationType.List}
+    >
+      <ResetAllButton />
+      <SaveFiltersButton onSave={onClose} />
+    </div>
   </div>
 </Drawer>
 
@@ -45,5 +65,11 @@
     :global(.trakt-button) {
       flex: 1;
     }
+  }
+
+  .trakt-filters-content {
+    display: flex;
+    flex-direction: column;
+    gap: var(--gap-xl);
   }
 </style>
---
+++ projects/client/src/lib/sections/navbar/components/filter/filters/_internal/FilterGroup.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  const { children }: ChildrenProps = $props();
+</script>
+
+<div class="trakt-filter-group">
+  {@render children()}
+</div>
+
+<style>
+  .trakt-filter-group {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: var(--gap-s);
+  }
+</style>
