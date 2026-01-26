f0e0801dbdd6df1f1e97cce9b0982171c18cc60c
seferturan <seferturan85@gmail.com>
2026-01-26 12:51:49 +0100

    feat(summary): adds popup menu for season list
    Fixes https://github.com/trakt/trakt-web/issues/1593

---
+++ projects/client/src/lib/sections/lists/season/_internal/SeasonPopupMenu.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import PopupMenu from "$lib/components/buttons/popup/PopupMenu.svelte";
+  import * as m from "$lib/features/i18n/messages.ts";
+  import type { EpisodeEntry } from "$lib/requests/models/EpisodeEntry";
+  import type { ShowEntry } from "$lib/requests/models/ShowEntry";
+  import MarkAsWatchedAction from "$lib/sections/media-actions/mark-as-watched/MarkAsWatchedAction.svelte";
+
+  type SeasonPopupMenuProps = {
+    title: string;
+    episodes: EpisodeEntry[];
+    show: ShowEntry;
+  };
+
+  const { title, episodes, show }: SeasonPopupMenuProps = $props();
+</script>
+
+<PopupMenu label={m.button_label_popup_menu({ title })} mode="standalone">
+  {#snippet items()}
+    <MarkAsWatchedAction
+      style="dropdown-item"
+      type="episode"
+      {title}
+      media={episodes}
+      {show}
+    />
+  {/snippet}
+</PopupMenu>
