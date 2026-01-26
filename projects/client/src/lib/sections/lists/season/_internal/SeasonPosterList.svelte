a806301245d1e882c87f45c3259a6242f4c311f9
seferturan <seferturan85@gmail.com>
2026-01-26 12:51:49 +0100

    feat(summary): adds popup menu for season list
    Fixes https://github.com/trakt/trakt-web/issues/1593

--- projects/client/src/lib/sections/lists/season/_internal/SeasonPosterList.svelte
+++ projects/client/src/lib/sections/lists/season/_internal/SeasonPosterList.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import SectionList from "$lib/components/lists/section-list/SectionList.svelte";
-  import RenderFor from "$lib/guards/RenderFor.svelte";
   import type { EpisodeEntry } from "$lib/requests/models/EpisodeEntry";
   import type { Season } from "$lib/requests/models/Season";
   import type { ShowEntry } from "$lib/requests/models/ShowEntry.ts";
   import SeasonItem from "$lib/sections/lists/components/SeasonItem.svelte";
   import { mediaListHeightResolver } from "$lib/sections/lists/utils/mediaListHeightResolver";
-  import MarkAsWatchedAction from "$lib/sections/media-actions/mark-as-watched/MarkAsWatchedAction.svelte";
   import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import SeasonPopupMenu from "./SeasonPopupMenu.svelte";
 
   type SeasonListProps = {
     show: ShowEntry;
@@ -44,25 +43,6 @@
     />
   {/snippet}
   {#snippet actions()}
-    <RenderFor audience="authenticated" device={["desktop", "tablet-lg"]}>
-      <MarkAsWatchedAction
-        style="normal"
-        type="episode"
-        size="small"
-        title={subtitle}
-        media={episodes}
-        {show}
-      />
-    </RenderFor>
-    <RenderFor audience="authenticated" device={["mobile", "tablet-sm"]}>
-      <MarkAsWatchedAction
-        style="action"
-        type="episode"
-        size="small"
-        title={subtitle}
-        media={episodes}
-        {show}
-      />
-    </RenderFor>
+    <SeasonPopupMenu title={subtitle} {episodes} {show} />
   {/snippet}
 </SectionList>
