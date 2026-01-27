b317283ecce32e2ed1f6cef746d6d4a1f6c6ee5c
seferturan <seferturan85@gmail.com>
2026-01-27 09:40:55 +0100

    wip(summary): hides watch again if user has watch only once enabled

--- projects/client/src/lib/sections/summary/components/_internal/WatchAgainAction.svelte
+++ projects/client/src/lib/sections/summary/components/_internal/WatchAgainAction.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { useUser } from "$lib/features/auth/stores/useUser";
   import MarkAsWatchedAction from "$lib/sections/media-actions/mark-as-watched/MarkAsWatchedAction.svelte";
   import { useIsWatched } from "$lib/sections/media-actions/mark-as-watched/useIsWatched";
   import type { MarkAsWatchedStoreProps } from "$lib/sections/media-actions/mark-as-watched/useMarkAsWatched";
@@ -10,8 +11,10 @@
   const { title, ...target }: WatchAgainProps = $props();
 
   const { isWatched } = $derived(useIsWatched(target));
+  const { user } = useUser();
+  // TODO: get rid of refactor, move logic to MarkAsWatchedAction; should be centralized there
 </script>
 
-{#if $isWatched}
+{#if $user.preferences.hasWatchAgain && $isWatched}
   <MarkAsWatchedAction style="dropdown-item" mode="ask" {title} {...target} />
 {/if}
