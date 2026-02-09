78ee9fd57018ef5afd7b17428d0cb59675be7877
seferturan <seferturan85@gmail.com>
2026-02-09 07:51:36 +0100

    fix(library): use media collection endpoint for library
    Fixes https://github.com/trakt/trakt-web/issues/1651

--- projects/client/src/lib/sections/lists/library/useLibraryList.ts
+++ projects/client/src/lib/sections/lists/library/useLibraryList.ts
@@ -1,6 +1,4 @@
 import type { PaginationParams } from '$lib/requests/models/PaginationParams.ts';
-import { libraryEpisodesQuery } from '../../../requests/queries/sync/libraryEpisodesQuery.ts';
-import { libraryMoviesQuery } from '../../../requests/queries/sync/libraryMoviesQuery.ts';
 import { libraryQuery } from '../../../requests/queries/sync/libraryQuery.ts';
 import { DEFAULT_PAGE_SIZE } from '../../../utils/constants.ts';
 import { usePaginatedListQuery } from '../stores/usePaginatedListQuery.ts';
@@ -8,26 +6,12 @@ import type { Library } from './models/Library.ts';
 
 type UseLibraryListProps = PaginationParams & {
   library: Library;
-  type?: 'movie' | 'episode';
 };
 
-function typeToQuery(props: UseLibraryListProps) {
-  const params = {
+export function useLibraryList(props: UseLibraryListProps) {
+  return usePaginatedListQuery(libraryQuery({
     page: props.page ?? 1,
     limit: props.limit ?? DEFAULT_PAGE_SIZE,
     availableOn: props.library,
-  };
-
-  switch (props.type) {
-    case 'movie':
-      return libraryMoviesQuery(params);
-    case 'episode':
-      return libraryEpisodesQuery(params);
-    default:
-      return libraryQuery(params);
-  }
-}
-
-export function useLibraryList(props: UseLibraryListProps) {
-  return usePaginatedListQuery(typeToQuery(props));
+  }));
 }
