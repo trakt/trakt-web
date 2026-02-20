3116e66b6f8848ef44a3cde0c195db2f1a0adf0b
seferturan <seferturan85@gmail.com>
2026-02-20 15:09:54 +0100

    fix: do not overwrite local search params

--- projects/client/src/lib/features/parameters/_internal/constants.ts
+++ projects/client/src/lib/features/parameters/_internal/constants.ts
@@ -1,6 +1,8 @@
 import { FilterKey } from '$lib/features/filters/models/Filter.ts';
 
-export const WHITE_LISTED_PARAMS = [
+export const WHITE_LISTED_PARAMS: readonly string[] = [
   'navigation',
   ...Object.values(FilterKey),
 ];
+
+export const LOCAL_PARAMS: readonly string[] = ['sort_by', 'sort_how'];
