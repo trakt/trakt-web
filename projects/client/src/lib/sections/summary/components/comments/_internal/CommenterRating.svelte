fe7e2490b27d7723ada0f8dc5821abc1f2dd5713
seferturan <seferturan85@gmail.com>
2026-02-24 17:02:06 +0100

    refactor: extract user rating component

--- projects/client/src/lib/sections/summary/components/comments/_internal/CommenterRating.svelte
+++ projects/client/src/lib/sections/summary/components/comments/_internal/CommenterRating.svelte
@@ -1,39 +1,16 @@
 <script lang="ts">
-  import RatingIcon from "$lib/components/icons/RatingIcon.svelte";
   import { useUser } from "$lib/features/auth/stores/useUser";
-  import { getLocale } from "$lib/features/i18n";
   import type { MediaComment } from "$lib/requests/models/MediaComment";
-  import { toUserRating } from "$lib/utils/formatting/number/toUserRating";
+  import UserRating from "$lib/sections/components/UserRating.svelte";
 
   const { comment }: { comment: MediaComment } = $props();
 
   const { user } = useUser();
   const isOwnComment = $derived(comment.user.id === $user.id);
 
-  const rating = $derived.by(() => {
-    const rating = comment.user.stats.rating;
-    if (!rating) return;
-
-    return toUserRating(rating, getLocale());
-  });
+  const rating = $derived(comment.user.stats.rating);
 </script>
 
 {#if !isOwnComment && rating}
-  <div class="trakt-commenter-rating">
-    <span>{rating}</span>
-    <RatingIcon style="rated" variant="user" />
-  </div>
+  <UserRating {rating} />
 {/if}
-
-<style>
-  .trakt-commenter-rating {
-    display: flex;
-    align-items: center;
-    gap: var(--gap-xxs);
-
-    :global(svg) {
-      width: var(--ni-16);
-      height: var(--ni-16);
-    }
-  }
-</style>
