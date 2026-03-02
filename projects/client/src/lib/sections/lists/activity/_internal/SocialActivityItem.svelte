4ab5afdb0123e2c94d81be0745e77b9032e6e01c
seferturan <seferturan85@gmail.com>
2026-03-02 17:31:10 +0100

    feat(activity): adds user ratings to activity items
    Fixes https://github.com/trakt/trakt-web/issues/1779

--- projects/client/src/lib/sections/lists/activity/_internal/SocialActivityItem.svelte
+++ projects/client/src/lib/sections/lists/activity/_internal/SocialActivityItem.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import * as m from "$lib/features/i18n/messages.ts";
   import type { SocialActivity } from "$lib/requests/models/SocialActivity";
+  import UserRating from "$lib/sections/components/UserRating.svelte";
   import ActivityItem from "../../components/ActivityItem.svelte";
   import ActivitySummaryCard from "../../components/ActivitySummaryCard.svelte";
   import UserAvatar from "../../components/UserAvatar.svelte";
@@ -22,7 +23,13 @@
   );
 </script>
 
-{#snippet badge()}
+{#snippet activityRating()}
+  {#if activity.rating}
+    <UserRating rating={activity.rating} />
+  {/if}
+{/snippet}
+
+{#snippet profileBadges()}
   <div class="user-profile-badges">
     {#each cappedUsers as user (user.slug)}
       <div class="user-profile-badge" class:has-background={!hasMultipleUsers}>
@@ -43,12 +50,20 @@
   </div>
 {/snippet}
 
+{#snippet summaryBadge()}
+  <div class="trakt-summary-badge">
+    {@render activityRating()}
+    {@render profileBadges()}
+  </div>
+{/snippet}
+
 {#if style === "cover"}
   <ActivityItem
     {activity}
     activityAt={activity.activityAt}
-    {badge}
+    badge={profileBadges}
     source="social-activity"
+    action={activityRating}
   />
 {/if}
 
@@ -56,7 +71,7 @@
   <ActivitySummaryCard
     {activity}
     activityAt={activity.activityAt}
-    {badge}
+    badge={summaryBadge}
     source="social-activity"
   />
 {/if}
@@ -102,5 +117,19 @@
 
   .user-count-label {
     padding: var(--ni-8);
+  }
+
+  .trakt-summary-badge {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: var(--gap-s);
+
+    width: 100%;
+
+    @include for-tablet-sm-and-below() {
+      flex-direction: row-reverse;
+      align-items: center;
+    }
   }
 </style>
