17ca5d906401b2f1dd010ae1223bfbcc78fc9814
seferturan <seferturan85@gmail.com>
2026-03-02 07:00:12 +0100

    refactor: extract shadow scroller

--- projects/client/src/lib/sections/components/ShadowScroller.svelte
+++ projects/client/src/lib/sections/components/ShadowScroller.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { setScrollInfo } from "./setScrollInfo";
+  import { setScrollInfo } from "./_internal/setScrollInfo";
 
   const { children }: ChildrenProps = $props();
 
