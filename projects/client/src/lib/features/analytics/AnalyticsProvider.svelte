517778bd12dd72c8ea9d79c8c0d3fc2b54ff4233
Vlad Jerca <vlad.jerca@gmail.com>
2026-03-05 11:27:43 +0100

    feat(analytics): integrate X conversion pixel

--- projects/client/src/lib/features/analytics/AnalyticsProvider.svelte
+++ projects/client/src/lib/features/analytics/AnalyticsProvider.svelte
@@ -2,6 +2,7 @@
   import { useCookieConsent } from "../cookie-consent/useCookieConsent";
   import FirebaseSetup from "./FirebaseSetup.svelte";
   import RedditPixel from "./_internal/RedditPixel.svelte";
+  import XPixel from "./_internal/XPixel.svelte";
 
   const { children }: ChildrenProps = $props();
 
@@ -10,6 +11,7 @@
 
 {#if $consent === "all"}
   <RedditPixel />
+  <XPixel />
 {/if}
 
 {#if $consent === "all"}
