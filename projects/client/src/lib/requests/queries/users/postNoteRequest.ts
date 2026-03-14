8e2d1c9585aa87748258224c3603eeab64c2e9a6
seferturan <seferturan85@gmail.com>
2026-03-14 16:29:47 +0100

    feat(requests): adds support for posting notes

---
+++ projects/client/src/lib/requests/queries/users/postNoteRequest.ts
@@ -0,0 +1,26 @@
+import { type ApiParams, rawApiFetch } from '$lib/requests/api.ts';
+import type { MediaType } from '$lib/requests/models/MediaType.ts';
+import type { NoteType } from '$lib/requests/models/NoteType.ts';
+
+type PostNoteParams = {
+  body: {
+    media: { type: MediaType; id: string | number };
+    type: NoteType;
+    notes: string;
+  };
+} & ApiParams;
+
+export async function postNoteRequest(
+  { fetch, body }: PostNoteParams,
+): Promise<boolean> {
+  const response = await rawApiFetch({
+    fetch,
+    path: '/v3/users/me/notes',
+    init: {
+      method: 'POST',
+      body: JSON.stringify(body),
+    },
+  });
+
+  return response.ok;
+}
