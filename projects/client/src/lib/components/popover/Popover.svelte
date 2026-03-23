ea9ca4d937b7c5af8a00c54d4202c471221b9daf
seferturan <seferturan85@gmail.com>
2026-03-23 14:21:11 +0100

    feat: adds basic popover component

---
+++ projects/client/src/lib/components/popover/Popover.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Popover } from "bits-ui";
+  import type { Snippet } from "svelte";
+
+  const { children, content }: { content: Snippet } & ChildrenProps = $props();
+</script>
+
+<Popover.Root>
+  <Popover.Trigger class="trakt-popover-trigger">
+    {@render children()}
+  </Popover.Trigger>
+  <Popover.Portal>
+    <Popover.Content sideOffset={8} side="top">
+      {@render content()}
+    </Popover.Content>
+  </Popover.Portal>
+</Popover.Root>
+
+<style>
+  :global(.trakt-popover-trigger) {
+    all: unset;
+    display: inline-flex;
+  }
+</style>
