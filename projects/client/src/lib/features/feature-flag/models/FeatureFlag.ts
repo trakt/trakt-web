a53b8ac6b9379f9cc12935acfa4e1f023b018a07
seferturan <seferturan85@gmail.com>
2026-04-08 22:50:04 +0200

    chore(settings): adds feature flag for data import

--- projects/client/src/lib/features/feature-flag/models/FeatureFlag.ts
+++ projects/client/src/lib/features/feature-flag/models/FeatureFlag.ts
@@ -1,3 +1,4 @@
 export enum FeatureFlag {
   ThisWeek = 'this-week',
+  DataImport = 'data-import',
 }
--- projects/client/src/lib/sections/settings/DataSettings.svelte
+++ projects/client/src/lib/sections/settings/DataSettings.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+  import { FeatureFlag } from "$lib/features/feature-flag/models/FeatureFlag";
+  import RenderForFeature from "$lib/guards/RenderForFeature.svelte";
   import RawExport from "./_internal/RawExport.svelte";
   import RawImport from "./_internal/RawImport.svelte";
 </script>
 
 <div class="trakt-data-settings">
-  <RawImport />
+  <RenderForFeature flag={FeatureFlag.DataImport}>
+    {#snippet enabled()}
+      <RawImport />
+    {/snippet}
+  </RenderForFeature>
   <RawExport />
 </div>
 
