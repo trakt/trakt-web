5a50c884dec4fbf820c0a6eef11d2beee824e52a
Marius <516291+Marius-TV@users.noreply.github.com>
2026-04-11 19:54:19 +0800

    fix(router): redirect loop guard

--- projects/client/src/lib/components/router/Redirect.svelte
+++ projects/client/src/lib/components/router/Redirect.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import { page } from "$app/state";
   import { onMount } from "svelte";
   const { to }: { to: string } = $props();
 
   onMount(() => {
+    if (page.url.pathname === to) return;
     goto(to, {
       replaceState: true,
     });
