2852b8b1cc69a955734ee308c96b6688214e2ac3
seferturan <seferturan85@gmail.com>
2026-04-20 15:03:46 +0200

    refactor: extract status indicators component

---
+++ projects/client/src/lib/sections/lists/components/StatusIndicators.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import BookmarkIcon from "$lib/components/icons/BookmarkIcon.svelte";
+  import IndicatorTag from "$lib/components/media/tags/IndicatorTag.svelte";
+  import TrackIcon from "$lib/components/TrackIcon.svelte";
+
+  type StatusIndicatorsProps = {
+    isWatched: boolean;
+    isWatchlisted: boolean;
+  };
+
+  const { isWatched, isWatchlisted }: StatusIndicatorsProps = $props();
+</script>
+
+{#if isWatched}
+  <IndicatorTag>
+    <TrackIcon />
+  </IndicatorTag>
+{:else if isWatchlisted}
+  <IndicatorTag>
+    <BookmarkIcon state="added" />
+  </IndicatorTag>
+{/if}
