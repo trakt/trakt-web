63a3de2b102a0a222562b064b35c4284cf403a1d
Marius-TV <marius@trakt.tv>
2026-04-22 11:58:03 +0800

    revert: "fix(service-worker): prevent service worker registration in Safari"
    This reverts commit 41942f57ca7a835c69bb2ab697a1b03ea4c8fc8d.

--- projects/client/static/register-install.js
+++ projects/client/static/register-install.js
@@ -1,20 +1,4 @@
-// Only register service worker if not Safari
-
-function isSafari() {
-  const ua = navigator.userAgent;
-  return /Safari/.test(ua) && !/Chrome|Chromium|Android/.test(ua);
-}
-
-// Unregister existing service workers in Safari
-if ('serviceWorker' in navigator && isSafari()) {
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    registrations.forEach(registration => {
-      registration.unregister();
-    });
-  });
-}
-
-if ('serviceWorker' in navigator && !isSafari()) {
+if ('serviceWorker' in navigator) {
   globalThis.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js', { type: 'module' });
 
