e3edf7e94c97a0d4150e7098d8293c6268387c0f
seferturan <seferturan85@gmail.com>
2026-04-23 14:31:25 +0200

    fix: always display list action

--- projects/client/src/lib/sections/components/lists-drawer/ListAction.svelte
+++ projects/client/src/lib/sections/components/lists-drawer/ListAction.svelte
@@ -5,7 +5,6 @@
   import ListIcon from "$lib/components/icons/mobile/ListIcon.svelte";
   import * as m from "$lib/features/i18n/messages";
   import type { MediaEntry } from "$lib/requests/models/MediaEntry";
-  import { useAllPersonalLists } from "$lib/stores/useAllPersonalLists";
   import { useIsListed } from "$lib/stores/useIsListed";
 
   type ListActionProps = {
@@ -31,54 +30,49 @@
   const { isLoading } = $derived(useIsListed(media));
 
   const isDisabled = $derived(disabled || $isLoading);
-
-  const { lists } = useAllPersonalLists();
-  const hasLists = $derived($lists.length > 0);
 </script>
 
-{#if hasLists}
-  {#if style === "normal"}
-    <Button
-      style="flat"
-      color="blue"
-      {size}
-      {variant}
-      label={m.dropdown_label_add_remove_from_lists({ title })}
-      disabled={isDisabled}
-      onclick={onClick}
-    >
-      {m.button_text_manage_lists()}
+{#if style === "normal"}
+  <Button
+    style="flat"
+    color="blue"
+    {size}
+    {variant}
+    label={m.dropdown_label_add_remove_from_lists({ title })}
+    disabled={isDisabled}
+    onclick={onClick}
+  >
+    {m.button_text_manage_lists()}
 
-      {#snippet icon()}
-        <ListIcon />
-      {/snippet}
-    </Button>
-  {/if}
-
-  {#if style === "action"}
-    <ActionButton
-      label={m.dropdown_label_add_remove_from_lists({ title })}
-      onclick={onClick}
-      style="ghost"
-      disabled={isDisabled}
-    >
+    {#snippet icon()}
       <ListIcon />
-    </ActionButton>
-  {/if}
+    {/snippet}
+  </Button>
+{/if}
 
-  {#if style === "dropdown-item"}
-    <DropdownItem
-      {variant}
-      onclick={onClick}
-      style="flat"
-      color="default"
-      label={m.dropdown_label_add_remove_from_lists({ title })}
-      disabled={isDisabled}
-    >
-      {`${m.button_text_manage_lists()}…`}
-      {#snippet icon()}
-        <ListIcon />
-      {/snippet}
-    </DropdownItem>
-  {/if}
+{#if style === "action"}
+  <ActionButton
+    label={m.dropdown_label_add_remove_from_lists({ title })}
+    onclick={onClick}
+    style="ghost"
+    disabled={isDisabled}
+  >
+    <ListIcon />
+  </ActionButton>
+{/if}
+
+{#if style === "dropdown-item"}
+  <DropdownItem
+    {variant}
+    onclick={onClick}
+    style="flat"
+    color="default"
+    label={m.dropdown_label_add_remove_from_lists({ title })}
+    disabled={isDisabled}
+  >
+    {`${m.button_text_manage_lists()}…`}
+    {#snippet icon()}
+      <ListIcon />
+    {/snippet}
+  </DropdownItem>
 {/if}
