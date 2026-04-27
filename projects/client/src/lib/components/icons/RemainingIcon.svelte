2adabc5bac300a26ef1d85c63eb42d2d9eb5912c
seferturan <seferturan85@gmail.com>
2026-04-27 15:52:07 +0200

    feat(up-next): sorting options for continue watching
    Fixes https://github.com/trakt/trakt-web/issues/2203

---
+++ projects/client/src/lib/components/icons/RemainingIcon.svelte
@@ -0,0 +1,11 @@
+<svg
+  width="24"
+  height="24"
+  viewBox="0 -960 960 960"
+  fill="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M160-240v-80h320v80H160Zm0-160v-80h320v80H160Zm0-160v-80h320v80H160ZM600-120v-480l320 240-320 240Z"
+  />
+</svg>
--- projects/client/src/lib/sections/lists/user/models/Sorting.ts
+++ projects/client/src/lib/sections/lists/user/models/Sorting.ts
@@ -1,7 +1,7 @@
 import type { SortBy } from './SortBy.ts';
 
-export type Sorting = {
+export type Sorting<T = SortBy> = {
   text: () => string;
   label: () => string;
-  value: SortBy | undefined;
+  value: T | undefined;
 };
