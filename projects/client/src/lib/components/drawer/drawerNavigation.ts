c8bdf1efc4fe7311b367d0a8029cbdae1a2f6f6c
seferturan <seferturan85@gmail.com>
2026-04-30 15:55:45 +0200

    feat(drawer): adds support for extra params

--- projects/client/src/lib/components/drawer/drawerNavigation.ts
+++ projects/client/src/lib/components/drawer/drawerNavigation.ts
@@ -2,10 +2,27 @@ import { goto } from '$app/navigation';
 import { page } from '$app/state';
 import { DRAWER_VIEW_PARAM } from '$lib/components/drawer/constants/index.ts';
 
-export function drawerNavigation<T extends string>() {
-  const buildDrawerLink = (drawer: T) => {
+export function drawerNavigation<
+  T extends string,
+  P extends Partial<Record<T, Record<string, string>>> = Record<never, never>,
+>(params?: P) {
+  const cleanupKeys = params
+    ? Object.values(params).flatMap((p) =>
+      Object.keys(p as Record<string, string>)
+    )
+    : [];
+
+  const buildDrawerLink = <D extends T>(
+    drawer: D,
+    ...[extraParams]: D extends keyof P ? [P[D]?] : []
+  ) => {
     const url = new URL(page.url);
     url.searchParams.set(DRAWER_VIEW_PARAM, drawer);
+    if (extraParams) {
+      for (const [key, value] of Object.entries(extraParams)) {
+        url.searchParams.set(key, value);
+      }
+    }
     return {
       href: url.toString(),
       noscroll: true,
@@ -16,6 +33,7 @@ export function drawerNavigation<T extends string>() {
   const close = () => {
     const url = new URL(page.url);
     url.searchParams.delete(DRAWER_VIEW_PARAM);
+    cleanupKeys.forEach((key) => url.searchParams.delete(key));
     goto(url, { noScroll: true, replaceState: true });
   };
 
