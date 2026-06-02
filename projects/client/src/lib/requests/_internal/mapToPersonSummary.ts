edb3023219bf3f283605c499d14874a2b3bf1347
Michal <michal.drabik0@gmail.com>
2026-06-02 13:15:04 +0200

    fix: use parseLocalDate for people birthday

--- projects/client/src/lib/requests/_internal/mapToPersonSummary.ts
+++ projects/client/src/lib/requests/_internal/mapToPersonSummary.ts
@@ -1,6 +1,7 @@
 import type { PersonResponse } from '@trakt/api';
 import { crewPositionSchema } from '../models/CrewPosition.ts';
 import type { PersonSummary } from '../models/PersonSummary.ts';
+import { parseLocalDate } from '$lib/utils/date/parseLocalDate.ts';
 import { mapToHeadshot } from './mapToHeadshot.ts';
 import { mapToSocialMedia } from './mapToSocialMedia.ts';
 
@@ -21,8 +22,8 @@ export const mapToPersonSummary = (
      * FIXME: @seferturan remove cast once @trakt/api is updated
      */
     height: (response as unknown as { height: number }).height,
-    birthday: response.birthday ? new Date(response.birthday) : null,
+    birthday: response.birthday ? parseLocalDate(response.birthday) : null,
     socialMedia: mapToSocialMedia(response),
-    deathDate: response.death ? new Date(response.death) : null,
+    deathDate: response.death ? parseLocalDate(response.death) : null,
   };
 };
