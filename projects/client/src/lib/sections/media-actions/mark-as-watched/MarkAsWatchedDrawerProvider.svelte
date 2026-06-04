ca206f5eb92dfd81a3ef48ac9f1876fe600a1183
Vlad Jerca <vlad.jerca@gmail.com>
2026-06-04 12:10:52 +0200

    refactor(watched): rename MarkAsWatchedDrawer to MarkAsWatchedDrawerHost
    The component owns useMarkAsWatched data fetching before rendering the
    <Drawer>; align with the host-pattern convention.

--- projects/client/src/lib/sections/media-actions/mark-as-watched/MarkAsWatchedDrawerProvider.svelte
+++ projects/client/src/lib/sections/media-actions/mark-as-watched/MarkAsWatchedDrawerProvider.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import MarkAsWatchedDrawer from "./_internal/MarkAsWatchedDrawer.svelte";
+  import MarkAsWatchedDrawerHost from "./_internal/MarkAsWatchedDrawerHost.svelte";
   import { markAsWatchedDrawerStore } from "./_internal/markAsWatchedDrawerStore";
 </script>
 
 {#if $markAsWatchedDrawerStore?.isOpen}
-  <MarkAsWatchedDrawer
+  <MarkAsWatchedDrawerHost
     onClose={markAsWatchedDrawerStore.close}
     title={$markAsWatchedDrawerStore.title}
     {...$markAsWatchedDrawerStore.mediaStore}
