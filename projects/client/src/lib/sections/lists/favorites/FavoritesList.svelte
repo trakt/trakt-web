66287ffb24403936196092fbf90aa0c3f170b121
Michal <michal.drabik0@gmail.com>
2026-06-11 14:29:26 +0200

    feat(profile): update favorites section title and current year limit

--- projects/client/src/lib/sections/lists/favorites/FavoritesList.svelte
+++ projects/client/src/lib/sections/lists/favorites/FavoritesList.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   import { useIsMe } from "$lib/features/auth/stores/useIsMe";
   import type { DiscoverMode } from "$lib/features/discover/models/DiscoverMode";
+  import { FeatureFlag } from "$lib/features/feature-flag/models/FeatureFlag";
+  import { useFeatureFlag } from "$lib/features/feature-flag/useFeatureFlag";
   import * as m from "$lib/features/i18n/messages";
+  import RenderForFeature from "$lib/guards/RenderForFeature.svelte";
   import CtaItem from "$lib/sections/lists/components/cta/CtaItem.svelte";
   import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+  import { map } from "rxjs";
   import DrillableMediaList from "../drilldown/DrillableMediaList.svelte";
-  import { useFavoritesList } from "../stores/useFavoritesList";
+  import {
+    useFavoritesList,
+    type UseFavoritesProps,
+  } from "../stores/useFavoritesList";
   import FavoriteMediaItem from "./_internal/FavoriteMediaItem.svelte";
 
   const {
@@ -30,6 +37,29 @@
     type: "favorites" as const,
     mediaType: mode === "media" ? undefined : mode,
   });
+
+  const currentYear = new Date().getFullYear();
+
+  const { isEnabled } = useFeatureFlag();
+  const isFlagEnabled = $derived(isEnabled(FeatureFlag.ScopedFavorites));
+
+  function useFavoritesListForYear(params: UseFavoritesProps) {
+    const result = useFavoritesList(params);
+    return {
+      ...result,
+      list: result.list.pipe(
+        map((entries) =>
+          entries.filter(
+            (entry) => entry.favoritedAt.getFullYear() === currentYear,
+          ),
+        ),
+      ),
+    };
+  }
+
+  const useFavorites = $derived(
+    $isFlagEnabled ? useFavoritesListForYear : useFavoritesList,
+  );
 </script>
 
 <DrillableMediaList
@@ -39,11 +69,21 @@
     key: `${mode}-${slug}`,
   }}
   type={mode}
-  useList={(params) => useFavoritesList({ ...params, slug })}
+  useList={(params) => useFavorites({ ...params, slug })}
   drilldownLabel={m.button_label_view_all_favorites()}
   source={{ id: "favorites", type: mode }}
   urlBuilder={() => UrlBuilder.profile.favorites(slug)}
 >
+  {#snippet metaInfo()}
+    <RenderForFeature flag={FeatureFlag.ScopedFavorites}>
+      {#snippet enabled()}
+        <p class="tag secondary">
+          {currentYear}
+        </p>
+      {/snippet}
+    </RenderForFeature>
+  {/snippet}
+
   {#snippet item(media)}
     <FavoriteMediaItem {media} {mode} isActionable={$isMe} />
   {/snippet}
