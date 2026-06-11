51f75b59820ad4ef3a9d77fea1e7e4483917c5e3
Felix Salazar <fxsalazar@gmail.com>
2026-06-11 22:47:16 +0200

    feat: show watched users as avatars with tooltips

--- projects/client/src/lib/sections/lists/PeopleWatchedList.svelte
+++ projects/client/src/lib/sections/lists/PeopleWatchedList.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import SectionList from "$lib/components/lists/section-list/SectionList.svelte";
+  import Tooltip from "$lib/components/tooltip/Tooltip.svelte";
   import * as m from "$lib/features/i18n/messages.ts";
   import type { UserProfile } from "$lib/requests/models/UserProfile.ts";
   import { toDisplayableName } from "$lib/utils/profile/toDisplayableName";
@@ -18,23 +19,16 @@
     id={{ scope: "people-watched", key: slug }}
     items={watchers}
     title={m.list_title_people_watched()}
-    --height-list="var(--ni-64)"
+    --height-list="var(--ni-48)"
   >
     {#snippet item(user)}
-      <div class="people-watched-item">
-        <UserAvatar {user} size="small" />
-        <p class="bold ellipsis">{toDisplayableName(user)}</p>
-      </div>
+      <Tooltip
+        content={toDisplayableName(user)}
+        variant="compact"
+        side="bottom"
+      >
+        <UserAvatar {user} size="large" />
+      </Tooltip>
     {/snippet}
   </SectionList>
 {/if}
-
-<style lang="scss">
-  .people-watched-item {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: var(--gap-xs);
-    width: var(--ni-56);
-  }
-</style>
