4c73d6de17f70fd3fd8f6046d289138e144f6c83
seferturan <seferturan85@gmail.com>
2026-06-12 09:40:42 +0200

    fix(sentiment): makes design consistent with trivia
    Fixes https://github.com/trakt/trakt-web/issues/2595

--- projects/client/src/lib/sections/summary/components/_internal/InfoCard.svelte
+++ projects/client/src/lib/sections/summary/components/_internal/InfoCard.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
   import Card from "$lib/components/card/Card.svelte";
 
-  const { children }: ChildrenProps = $props();
+  type InfoCardProps = {
+    variant?: "default" | "highlight";
+  } & ChildrenProps;
+
+  const { children, variant = "default" }: InfoCardProps = $props();
 </script>
 
 <Card variant="transparent" --width-card="100%" --height-card="fit-content">
-  <div class="trakt-info-container">
+  <div class="trakt-info-container" data-variant={variant}>
     {@render children()}
   </div>
 </Card>
 
 <style>
   .trakt-info-container {
+    --border-opacity-info: 10%;
+
     position: relative;
 
     display: flex;
@@ -22,8 +28,6 @@
     padding: var(--ni-20) var(--ni-24);
 
     background: color-mix(in srgb, var(--color-text-primary) 4%, transparent);
-    border: var(--border-thickness-xxs) solid
-      color-mix(in srgb, var(--color-text-primary) 24%, transparent);
     border-radius: var(--border-radius-m);
 
     :global(.trakt-spoiler) {
@@ -32,6 +36,32 @@
 
     :global(li) {
       font-size: var(--font-size-text);
+    }
+
+    &[data-variant="highlight"] {
+      --border-opacity-info: 25%;
+
+      overflow: hidden;
+
+      &::before {
+        content: "";
+
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+
+        background: linear-gradient(
+          90deg,
+          transparent 0%,
+          var(--color-sentiment-highlight-border) 50%,
+          transparent 100%
+        );
+
+        transform: translateX(-100%);
+        animation: slide calc(5 * var(--transition-increment)) ease-in-out;
+      }
     }
   }
 </style>
