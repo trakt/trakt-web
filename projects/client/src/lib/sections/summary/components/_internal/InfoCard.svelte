6122b72363da556bae7eeac7498813cb49cbec3f
seferturan <seferturan85@gmail.com>
2026-06-12 09:33:24 +0200

    refactor: extracts info card
    Co-authored-by: Vlad Jerca <vlad.jerca@gmail.com>

---
+++ projects/client/src/lib/sections/summary/components/_internal/InfoCard.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Card from "$lib/components/card/Card.svelte";
+
+  const { children }: ChildrenProps = $props();
+</script>
+
+<Card variant="transparent" --width-card="100%" --height-card="fit-content">
+  <div class="trakt-info-container">
+    {@render children()}
+  </div>
+</Card>
+
+<style>
+  .trakt-info-container {
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+    gap: var(--gap-s);
+
+    box-sizing: border-box;
+    padding: var(--ni-20) var(--ni-24);
+
+    background: color-mix(in srgb, var(--color-text-primary) 4%, transparent);
+    border: var(--border-thickness-xxs) solid
+      color-mix(in srgb, var(--color-text-primary) 24%, transparent);
+    border-radius: var(--border-radius-m);
+
+    :global(.trakt-spoiler) {
+      cursor: pointer;
+    }
+
+    :global(li) {
+      font-size: var(--font-size-text);
+    }
+  }
+</style>
--- projects/client/src/lib/sections/summary/components/trivia/_internal/TriviaCard.svelte
+++ projects/client/src/lib/sections/summary/components/trivia/_internal/TriviaCard.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import Card from "$lib/components/card/Card.svelte";
   import Spoiler from "$lib/features/spoilers/components/Spoiler.svelte";
   import type { MediaEntry } from "$lib/requests/models/MediaEntry";
   import type { MediaTrivia } from "$lib/requests/models/MediaTrivia";
   import { Marked } from "marked";
+  import InfoCard from "../../_internal/InfoCard.svelte";
 
   const {
     trivia,
@@ -20,40 +20,12 @@
   {@html marked.parse(trivia.text)}
 {/snippet}
 
-<Card variant="transparent" --width-card="100%" --height-card="fit-content">
-  <div class="trakt-trivia-container">
-    {#if !trivia.isSpoiler}
+<InfoCard>
+  {#if !trivia.isSpoiler}
+    {@render parsedContent()}
+  {:else}
+    <Spoiler {media} type={media.type}>
       {@render parsedContent()}
-    {:else}
-      <Spoiler {media} type={media.type}>
-        {@render parsedContent()}
-      </Spoiler>
-    {/if}
-  </div>
-</Card>
-
-<style>
-  .trakt-trivia-container {
-    position: relative;
-
-    display: flex;
-    flex-direction: column;
-    gap: var(--gap-s);
-
-    box-sizing: border-box;
-    padding: var(--ni-20) var(--ni-24);
-
-    background: color-mix(in srgb, var(--color-text-primary) 4%, transparent);
-    border: var(--border-thickness-xxs) solid
-      color-mix(in srgb, var(--color-text-primary) 24%, transparent);
-    border-radius: var(--border-radius-m);
-
-    :global(.trakt-spoiler) {
-      cursor: pointer;
-    }
-
-    :global(li) {
-      font-size: var(--font-size-text);
-    }
-  }
-</style>
+    </Spoiler>
+  {/if}
+</InfoCard>
