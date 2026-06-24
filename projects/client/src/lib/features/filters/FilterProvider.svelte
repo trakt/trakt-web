6fe9f4b320a592ea33705db8fd9ed9687ac30dec
seferturan <seferturan85@gmail.com>
2026-06-24 09:10:33 +0200

    fix(filter): restore filters after client-side navigation
    Fixes https://github.com/trakt/trakt-web/issues/2705

--- projects/client/src/lib/features/filters/FilterProvider.svelte
+++ projects/client/src/lib/features/filters/FilterProvider.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { useStoredFilters } from "./useStoredFilters";
 
   const { children }: ChildrenProps = $props();
 
   const { restoreFilters } = useStoredFilters();
-  restoreFilters();
+
+  onMount(() => {
+    restoreFilters();
+  });
 </script>
 
 {@render children()}
