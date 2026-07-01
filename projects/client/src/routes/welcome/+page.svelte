292197a668d54f1f7d0b26063315342be32e5feb
seferturan <seferturan85@gmail.com>
2026-07-01 15:21:20 +0200

    feat(welcome): redirect welcome route to home

---
+++ projects/client/src/routes/welcome/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Redirect from "$lib/components/router/Redirect.svelte";
+  import { UrlBuilder } from "$lib/utils/url/UrlBuilder";
+</script>
+
+<Redirect to={UrlBuilder.home()} />
