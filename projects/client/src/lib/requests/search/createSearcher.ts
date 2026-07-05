61c227cb4edbccc0021fc2c2c449e3f7372eebfd
Stefan Friptu <stefan.friptu@uipath.com>
2026-07-05 14:19:45 +0200

    fix: switch between local or remote typesense server

--- projects/client/src/lib/requests/search/createSearcher.ts
+++ projects/client/src/lib/requests/search/createSearcher.ts
@@ -7,12 +7,15 @@ export function createSearcher({
   key: string;
   server: string;
 }): Client {
+  const url = new URL(server.includes('://') ? server : `https://${server}`);
+  const protocol = url.protocol === 'http:' ? 'http' : 'https';
+
   return new Client({
     nodes: [
       {
-        host: server,
-        port: 443,
-        protocol: 'https',
+        host: url.hostname,
+        port: url.port ? Number(url.port) : protocol === 'http' ? 80 : 443,
+        protocol,
       },
     ],
     apiKey: key,
