02b609b0697d5df22f0e16caf18cd424c166800e
Salvador Domingues <82028566+salvadord18@users.noreply.github.com>
2026-08-06 11:13:46 +0100

    Remove text-transform from settings title
    Removed text transformation for settings title.

---
+++ projects/client/src/lib/sections/settings/_internal/SettingsBlock.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  const {
+    title,
+    description,
+    children,
+  }: ChildrenProps & { title: string; description: string } = $props();
+</script>
+
+<div class="trakt-settings-block">
+  <div class="trakt-settings-block-header">
+    <p class="settings-title">{title}</p>
+    <p class="secondary">{description}</p>
+  </div>
+  <div class="trakt-settings-block-content">
+    {@render children()}
+  </div>
+</div>
+
+<style lang="scss">
+  @use "$style/scss/mixins/index" as *;
+
+  .trakt-settings-block-header {
+    display: flex;
+    flex-direction: column;
+
+    gap: var(--gap-xs);
+
+    p.settings-title {
+      transition: font-size var(--transition-increment) ease-in-out;
+      font-size: var(--font-size-title);
+    }
+  }
+
+  .trakt-settings-block,
+  .trakt-settings-block-content {
+    display: flex;
+    flex-direction: column;
+    gap: var(--gap-l);
+  }
+</style>
