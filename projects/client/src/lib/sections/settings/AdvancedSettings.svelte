24cf5c9750fa5522d1aef03f7b02ee2e0e1a4493
seferturan <seferturan85@gmail.com>
2026-08-10 20:50:36 +0200

    feat(settings): adds export option to advanced settings
    Temporarily exposes the raw export functionality in the settings UI. Its placement will be reviewed and possibly removed once clearing or deduping actions automatically force an export.

--- projects/client/src/lib/sections/settings/AdvancedSettings.svelte
+++ projects/client/src/lib/sections/settings/AdvancedSettings.svelte
@@ -2,12 +2,16 @@
   import ClearData from "./_internal/ClearData.svelte";
   import DeleteAccount from "./_internal/DeleteAccount.svelte";
   import HistoryAnalysis from "./_internal/HistoryAnalysis.svelte";
+  import RawExport from "./_internal/RawExport.svelte";
 </script>
 
 <div class="trakt-advanced-settings-page">
   <HistoryAnalysis />
 
   <ClearData />
+
+  <!-- FIXME: remove this from here when clearing/deduping forces an export -->
+  <RawExport />
 
   <DeleteAccount />
 </div>
