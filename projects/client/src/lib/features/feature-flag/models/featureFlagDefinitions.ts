07f935c67cdea850ca5a4e538437c7c3fea36ad0
Stefan Friptu <stefan@trakt.tv>
2026-09-12 02:18:51 +0900

    chore(flags): feature flag gifs in comments

--- projects/client/src/lib/features/feature-flag/models/featureFlagDefinitions.ts
+++ projects/client/src/lib/features/feature-flag/models/featureFlagDefinitions.ts
@@ -3,6 +3,7 @@ import EditModeIcon from '$lib/components/icons/EditModeIcon.svelte';
 import EyeIcon from '$lib/components/icons/EyeIcon.svelte';
 import FastRewindIcon from '$lib/components/icons/FastRewindIcon.svelte';
 import FavoriteIcon from '$lib/components/icons/FavoriteIcon.svelte';
+import GifIcon from '$lib/components/icons/GifIcon.svelte';
 import ListIcon from '$lib/components/icons/mobile/ListIcon.svelte';
 import MusicNoteIcon from '$lib/components/icons/MusicNoteIcon.svelte';
 import NoSpoilerIcon from '$lib/components/icons/NoSpoilerIcon.svelte';
@@ -114,5 +115,12 @@ export const featureFlagDefinitions: FeatureFlagDefinitions = {
     title: () => m.preview_feature_title_action_confirmations(),
     addedAt: new Date('2026-08-25'),
     description: () => m.preview_feature_description_action_confirmations(),
+  },
+  [FeatureFlag.CommentGifs]: {
+    icon: GifIcon,
+    title: () => m.preview_feature_title_comment_gifs(),
+    addedAt: new Date('2026-09-12'),
+    description: () => m.preview_feature_description_comment_gifs(),
+    audience: 'director',
   },
 };
--- projects/client/src/lib/features/gif-picker/GifButton.svelte
+++ projects/client/src/lib/features/gif-picker/GifButton.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import ActionButton from "$lib/components/buttons/ActionButton.svelte";
   import GifIcon from "$lib/components/icons/GifIcon.svelte";
+  import { FeatureFlag } from "$lib/features/feature-flag/models/FeatureFlag.ts";
   import * as m from "$lib/features/i18n/messages.ts";
+  import RenderForFeature from "$lib/guards/RenderForFeature.svelte";
   import type { GifEntry } from "$lib/requests/models/GifEntry.ts";
   import GifPickerDrawerHost from "./GifPickerDrawerHost.svelte";
 
@@ -15,18 +17,22 @@
   let isPickerOpen = $state(false);
 </script>
 
-<ActionButton
-  onclick={() => (isPickerOpen = true)}
-  label={m.button_label_add_gif()}
-  style="ghost"
-  color="purple"
-  size="small"
-  variant="secondary"
-  {disabled}
->
-  <GifIcon />
-</ActionButton>
+<RenderForFeature flag={FeatureFlag.CommentGifs} audience="director">
+  {#snippet enabled()}
+    <ActionButton
+      onclick={() => (isPickerOpen = true)}
+      label={m.button_label_add_gif()}
+      style="ghost"
+      color="purple"
+      size="small"
+      variant="secondary"
+      {disabled}
+    >
+      <GifIcon />
+    </ActionButton>
 
-{#if isPickerOpen}
-  <GifPickerDrawerHost onClose={() => (isPickerOpen = false)} {onSelect} />
-{/if}
+    {#if isPickerOpen}
+      <GifPickerDrawerHost onClose={() => (isPickerOpen = false)} {onSelect} />
+    {/if}
+  {/snippet}
+</RenderForFeature>
